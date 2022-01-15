8d88ab7a9c35c38aa8e3a642d537fa0e3dbdd681
jeong98CH <jchul1998@naver.com>
2022-01-15 20:49:21 +0900

    cc

--- basis/Report_SyS/Report_SyS/Report_SYS.cpp
+++ basis/Report_SyS/Report_SyS/Report_SYS.cpp
@@ -8,20 +8,35 @@ using namespace std;
     // 한유저가 같은 유저를 여러번 신고한 경우는 1번으로 처리
     //신고한 내용 취합 후 마지막에 정지메일 발송.
 
-//레포트에서 신고를 한사람 추출하는 함수
-
-vector<string> GetReporterID(vector<string>& report)
+//레포트에서 신고를 받은id iterator
+auto GetReporterIterator(const vector<string>& report)
 {
-
+    for (auto it = report.begin(); it != report.end(); ++it)
+    {
+        // 공백찾음
+        if (*it == " ")
+        {
+            ++it;
+            return it;
+        }
+    }
+    return report.end(); //잘못된 문자열
 }
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    
+    //id_list의 순서대로 자신이 신고한 아이디중에 2회이상 신고당한 사람의 수를 push_back
     vector<int> answer;
+    vector<int> totalreported; // 아이디 리스트의 순서대로 신고받은 총 회수  
+
+
+
     //id_list하나씩 검사
     for (auto it = id_list.begin(); it != id_list.end(); ++it)
     {
-
-        if ()
+        int cnt = count(id_list.begin(), id_list.end(), *it);
+        totalreported.push_back(cnt);
     }
+
     return answer;
 }
