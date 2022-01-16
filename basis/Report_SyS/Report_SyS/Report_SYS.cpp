e390ec71817cdd985efff429a57fed8e61bb288f
jeong98CH <jchul1998@naver.com>
2022-01-16 14:37:59 +0900

    middel

--- basis/Report_SyS/Report_SyS/Report_SYS.cpp
+++ basis/Report_SyS/Report_SyS/Report_SYS.cpp
@@ -1,42 +1,102 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
-//처리결과를 받은 횟수를 리턴(k이상의 신고를 한 경우의 수)
+//자신의 신고결과에 대한  정치처리결과를 받은 횟수를 리턴(k이상의 신고를 한 경우의 수)
 
-//아이디리스트에서 각 한개와 피신고자를 리포트("신고자 신고한id" 형태)에서 비교해서 카운트
-    // 한유저가 같은 유저를 여러번 신고한 경우는 1번으로 처리
-    //신고한 내용 취합 후 마지막에 정지메일 발송.
+//신고자가 신고한 아이디를 알아야한다
+//각 아이디가 몇번 신고당했는지 알아햐한다.
+// 정치 처리 받은 아이디를 각 신고자가 신고한 아이디에서 검사해서 카운트 해야한다...
 
-//레포트에서 신고를 받은id iterator
-auto GetReporterIterator(const vector<string>& report)
+
+vector<int> MakeReportData(const vector<string>& id_list, const vector<string>& truereport, int k)
 {
-    for (auto it = report.begin(); it != report.end(); ++it)
+    vector<int> reportdata(id_list.size(), 0);
+    vector<int> answer;
+
+    //쪼개서 아이디별로 리포트 시스템 관리
+    for (auto it = truereport.begin(); it != truereport.end(); ++it)
     {
-        // 공백찾음
-        if (*it == " ")
+        string ownerid = "";
+        string ownerreport = "";
+
+        string tmp = *it;
+
+        size_t offset = tmp.find(" ");
+        ownerid = tmp.substr(0, offset-1);
+        ownerreport = tmp.substr(offset+1);
+
+        for (auto i = 0 ;  i<id_list.size();  i++)
         {
-            ++it;
-            return it;
+            if( id_list[i] == ownerreport)
+            {  
+                //id_list의 순서에 맞춰 id가 신고받은 횟수를 저장
+                reportdata[i]++;
+            }
         }
     }
-    return report.end(); //잘못된 문자열
+
+    for(int i = 0 ;  i<reportdata.size(); i++)
+    {
+        if(reportdata[i]>=k) //정지메일대상
+        {
+            for (auto it = truereport.begin(); it != truereport.end(); ++it)
+            {
+                string ownerid = "";
+                string ownerreport = "";
+
+                string tmp = *it;
+
+                size_t offset = tmp.find(" ");
+                ownerreport = tmp.substr(offset + 1);
+                ownerid = tmp.substr(0, offset - 1);
+
+                if (ownerreport == id_list[i])
+                {
+
+                }
+            }
+
+        }
+    }
+
+
+    return answer; 
+}
+
+//중복 제거 한 report반환
+vector<string> GetTrueReportVector(const vector<string>& report)
+{
+    vector<string> truereport;
+    for (auto it = report.begin(); it != report.end(); ++it)
+    {
+       //중복이 아니면
+        if(count(it, report.end(),*it) < 2)
+        {
+            truereport.push_back(*it);
+        }
+    }
+    return truereport;;
 }
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     
-    //id_list의 순서대로 자신이 신고한 아이디중에 2회이상 신고당한 사람의 수를 push_back
     vector<int> answer;
-    vector<int> totalreported; // 아이디 리스트의 순서대로 신고받은 총 회수  
-
-
-
-    //id_list하나씩 검사
-    for (auto it = id_list.begin(); it != id_list.end(); ++it)
-    {
-        int cnt = count(id_list.begin(), id_list.end(), *it);
-        totalreported.push_back(cnt);
-    }
-
+    
+    vector<string> truereport = GetTrueReportVector(report);
+    answer = MakeReportData(id_list, truereport, k);
     return answer;
 }
+
+#if 1
+int main()
+{
+    vector<string> id_list = { "muzi", "frodo", "apeach", "neo" };
+    vector<string> report = { "muzi frodo","apeach frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" };
+    solution(id_list, report, 2);
+
+    return 0;
+
+}
+#endif // 0
