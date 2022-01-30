3f117d47dc34d357e6bb174cd7d47f5f75e25722
jeong98CH <jchul1998@naver.com>
2022-01-30 19:45:53 +0900

    ddd

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -3,37 +3,53 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
+
+map<char, int> dial;
+vector<char> flag;
+
+int GetFlag(const char c)
+{
+	for (int i = 0; i < flag.size()-1; i++)
+	{
+		if (c < flag[i+1])
+			return i;
+	}
+	return flag.size()-1;
+}
+int FindNUM(const char& c)
+{
+	int idx = GetFlag(c);
+
+	char tmp = flag[idx];
+	int ret = dial.at(tmp);
+	return ret;
+}
+
+//숫자+2초 걸린다.
 int main()
 {
-	string s;
-	vector<string> verb;
+	flag = vector<char>{ 'A','D','G','J','M','P','T','W' };
 
-	getline(cin, s);
-
-	s = ' ' + s + ' ';
-
-	int start = 0, end = 0;
-
-	for (int i = 0; i < s.length(); i++)
+	for (int i = 0; i < flag.size(); i++)
 	{
-		if (s[i] == ' ')
-		{
-			end = i;
-			string tmp = s.substr(start, end - start);
-			if (start < s.size())
-				start = i + 1;
-
-			if (tmp.length() != 0)
-				verb.push_back(tmp);
-		}
+		dial.emplace(make_pair(flag[i], i + 2));
 	}
 
-	cout << verb.size();
+	string s;
+	getline(cin, s);
 
-	return 0;
+	int sum = 0;
+	
+	for (char c : s)
+	{
+		sum += FindNUM(c) + 1;
+	}
+
+	cout << sum << endl;
 }
 
 #endif // 1
