b006aa2a87fa872104c6190596dfef21a735adba
jeong98CH <jchul1998@naver.com>
2022-02-05 15:19:34 +0900

    backjoon 2839

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -4,44 +4,33 @@
 #include <vector>
 #include <algorithm>
 
-
 using namespace std;
 
-int cache[15][15];
-
-int GetAns(const int& k, const int& n)
-{
-	if (k < 0 || n < 0)
-		return 0;
-	int& ret = cache[k][n - 1];
-
-	if (ret != 0)
-		return ret;
-
-	if (k == 0)
-		return ret = n;
-
-	return ret = GetAns(k - 1, n) + GetAns(k, n - 1);
-}
-
+ 
 int main()
 {
-	
-	vector<int> ans;
-	int T;
-	cin >> T;
-	for (int i = 0; i < T; i++)
-	{
-		int k, n;
-		cin >> k >> n;
+	int N;
+	cin >> N;
 
-		ans.push_back(GetAns(k, n));
-	}
+	int n5 = N / 5;
 
-	for(int a :  ans)
+	while (n5 >= 0)
 	{
-		cout << a << endl;
+		int loss = N - n5 * 5;
+
+		if (loss % 3 != 0)
+		{
+			n5--;
+			continue;
+		}
+		else
+		{
+			cout << n5 + loss / 3 << endl;
+			return 0;
+		}
 	}
+	cout << -1 << endl;
+	return 0;
 }
 
 #endif // 1
