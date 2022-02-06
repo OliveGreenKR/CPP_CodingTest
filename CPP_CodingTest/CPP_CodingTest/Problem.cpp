a8f41c3bfecc48da9d8621c2ed89ef5fd73c1259
jeong98CH <jchul1998@naver.com>
2022-02-06 18:22:58 +0900

    10757

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -7,29 +7,60 @@
 #include <iostream>
 using namespace std;
 
-void AddList(const string& str, list<int>& ans)
+void Add(const string& str, vector<int>& ans)
 {
+	int current = 0;
+
 	for (char c : str)
 	{
-		ans.push_front(c - '0');
+		int& here = ans[current];
+		if (here == -1)
+			here = 0;
+
+		ans[current] += c - '0';
+		current++;
 	}
 }
 
+void Answering(vector<int>& ans)
+{
+
+	for (int i= 0; i < ans.size() ; i ++)
+	{
+		int& now = ans[i];
+
+		if (now > 9)
+		{
+			int& next = ans[i + 1];
+			next == -1 ? next = now / 10 : next += now / 10;
+			now = now % 10;
+		}
+
+	}
+}
 
 int main()
 {
 	string A, B;
 	cin >> A >> B;
 	
-	list<int> ans;
-
-	AddList(A, ans);
-	AddList(B, ans);
+	vector<int> ans(7,-1);
 	
-	for (auto it = ans.begin(); it != ans.end(); ++it)
+	::reverse(A.begin(), A.end());
+	::reverse(B.begin(), B.end());
+
+	Add(A, ans);
+	Add(B, ans);
+
+	Answering(ans);
+
+	for (int i = ans.size()-1; i >= 0; --i)
 	{
-		cout << *it;
+		const int& c = ans[i];
+		if(c!=-1)
+			cout << c;
 	}
+
 	return 0;
 }
 #endif
