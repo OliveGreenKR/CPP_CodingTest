73bbf4304a3f2a88e3c746878e282fa2628838c2
jeong98CH <jchul1998@naver.com>
2022-02-15 01:00:05 +0900

    find changed bit, solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -1,50 +1,26 @@
 #include "pch.h"
 #ifdef BACK
 
-
 #include <cstdio>
 using namespace std;
 
-
-enum
-{
-	MAX_SLOT = 7
-};
-
-int GetNUM(int n)
-{	
-	int sum = n;
-	for (int i = 1; i <= MAX_SLOT; i++)
-	{
-		int tmp = n % 10;
-		sum += tmp;
-		n /= 10;
-		if (n <= 0)
-			break;
-	}
-	return sum;
-}
-void GetAns(int N)
-{
-	int sum;
-	for (int i = N/2; i <= N; i++)
-	{
-		sum = GetNUM(i);
-		if (sum == N)
-		{
-			printf("%d\n", i);
-			return;
-		}
-	}
-	printf("%d\n", 0);
-
-}
 int main()
 {
-	
 	int N;
 	scanf_s("%d", &N);
-	GetAns(N);
+
+	//비트반전
+	int comple = (N ^ -1) + 1 ;
+
+	int cnt = 0;
+	for (int i = 0; i < 32; i++)
+	{
+		int mask = 1 << i;
+		if( (N &mask) ==  (comple&mask))
+			continue;
+		cnt++;
+	}
+	printf("%d", cnt);
 }
 
 #endif
