30d048e5e5837f50d2b6d0f59ef729285ca3447d
jeong98CH <jchul1998@naver.com>
2022-02-15 21:47:36 +0900

    chessboard solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -1,48 +1,97 @@
 #include "pch.h"
 #ifdef BACK
-
+#include <iostream>
 #include <cstdio>
 #include<vector>
+#include <string>
 using namespace std;
+
+enum
+{
+	WHITE = 0,
+	BLACK = 1
+};
+
+int Check(const vector<vector<int>>&arr, int y, int x)
+{
+	int ret=0;
+
+	int CorrectColor = arr[y][x];
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (arr[i+y][j+x] ^ CorrectColor)
+				ret++;
+			CorrectColor = CorrectColor ^ 1;
+		} 
+		CorrectColor = CorrectColor ^ 1;
+	}
+	return ret;
+}
+
+
+int Check2(const vector<vector<int>>& arr, int y, int x)
+{
+	int ret = 0;
+
+	int CorrectColor = arr[y][x];
+	CorrectColor = CorrectColor ^ 1;
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (arr[i + y][j + x] ^ CorrectColor)
+				ret++;
+			CorrectColor = CorrectColor ^ 1;
+		}
+		CorrectColor = CorrectColor ^ 1;
+	}
+	return ret;
+}
 
 int main()
 {
-	struct Size {
-		int _weight;
-		int _height;
+	int M, N;
+	scanf_s("%d%d", &N, &M);
+	vector<vector<int>> arr(N,vector<int>(M,-1));
 
-		bool operator>(const Size& other) 
-		{ return (_weight > other._weight && _height > other._height); }
-	};
-	vector<Size> datas;
-	
-	int N;
-	scanf_s("%d", &N);
-
-	vector<int> ranks(N,1);
-
-	for (int i = 0; i < N; i++)
+	for (int j = 0; j < N; j++)
 	{
-		int x, y;
-		scanf_s("%d%d", &x, &y);
-		datas.push_back( Size{x,y} );
-	}
-
-	for (Size now : datas)
-	{
-		for (int i = 0; i < N; i++)
+		string str;
+		cin >> str;
+		for (int i = 0 ;  i < M ; i++)
 		{
-			Size other = datas[i];
-			if (now > other)
-				ranks[i]++;
+			char c = str[i];
+			if (c == 'W')
+				arr[j][i] = WHITE;
+			if (c == 'B')
+				arr[j][i] = BLACK;
 		}
 	}
 	
-	for (int rank : ranks)
+	int cangoRight = M - 8;
+	int cangoDown = N - 8;
+	int ret = INT32_MAX;
+
+	for (int i = 0; i <= cangoDown;i++)
 	{
-		printf("%d ", rank);
+		for (int j = 0; j <= cangoRight; j++)
+		{
+			int cnt = min(Check(arr, i, j), Check2(arr, i, j));
+			if (ret > cnt)
+			{
+				ret = cnt;
+				if (ret == 0)
+				{
+					printf("%d\n", ret);
+					return 0;
+				}
+			}
+				
+		}
 	}
-	
+	printf("%d\n", ret);
 }
 
 #endif
