19b67a817c04cbc6e9b901cb86643d942e8ad933
jeong98CH <jchul1998@naver.com>
2022-02-18 19:14:51 +0900

    COunting Sort

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -1,58 +1,37 @@
 #include "pch.h"
 #ifdef BACK
 #include <iostream>
-#include <cstdio>
-#include<vector>
 
 using namespace std;
-#include <stdio.h>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
 
-int main(void)
+
+//최대숫자 범위는 1~ 10'000
+enum
 {
-	int N, cnt = 0, i = 0, NUM;
-	scanf_s("%d", &N);
-	while (1)
-	{
-		if (cnt == N) break;
-		if (i % 1000 == 666)
-		{
-			for (int j = 0; j < 1000; j++)
-			{
-				NUM = i * 1000 + j;
-				cnt++;
-				if (cnt == N) break;
-			}
-			i++;
-		}
-		else if (i % 100 == 66)
-		{
-			for (int j = 0; j < 100; j++)
-			{
-				NUM = i * 1000 + 600 + j;
-				cnt++;
-				if (cnt == N) break;
-			}
-			i++;
-		}
-		else if (i % 10 == 6)
-		{
-			for (int j = 0; j < 10; j++)
-			{
-				NUM = i * 1000 + 660 + j;
-				cnt++;
-				if (cnt == N) break;
-			}
-			i++;
-		}
-		else
-		{
-			NUM = i * 1000 + 666;
-			cnt++;
-			i++;
-		}
+	MAX = 10'001
+};
+
+int cSum[MAX];
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int index = 0;
+		scanf("%d", &index);
+		cSum[index]++;
 	}
-	printf("%d", NUM);
+	for (int i = 1; i < MAX; i++) {
+		for (int j = 0; j < cSum[i]; j++)
+			printf("%d\n", i);
+	}
 	return 0;
 }
+
 
 #endif
