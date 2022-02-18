c6badc0e6a064fe60aae3876777ceac1153c9f03
jeong98CH <jchul1998@naver.com>
2022-02-18 21:24:05 +0900

    statictionary solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -1,37 +1,69 @@
 #include "pch.h"
 #ifdef BACK
-#include <iostream>
 
-using namespace std;
+#include <iostream>
 #include <cstdio>
 #include <vector>
 #include <algorithm>
-
-
-//최대숫자 범위는 1~ 10'000
 enum
 {
-	MAX = 10'001
+	_MAX = 8001,
+	_HALF = (_MAX) / 2
 };
-
-int cSum[MAX];
+using namespace std;
+vector<int> InputCount(_MAX, 0); //횟수
+vector<int> Vsort;
 
 int main()
 {
-	int N;
+	int MAX = INT32_MIN, MIN = INT32_MAX; //최대값 최소값
+
+	int N;//원소개수
 	cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		int index = 0;
-		scanf("%d", &index);
-		cSum[index]++;
+	int SUM = 0;//합계
+	int MaxCnt = 0; // 최대 회수
+	for (int i = 0; i < N; i++)
+	{
+		int input;
+		scanf_s("%d", &input);
+		SUM += input;
+		if (input > MAX)
+			MAX = input;
+		if (input < MIN)
+			MIN = input;
+		int cnt = ++InputCount[input + _HALF];
+		if (cnt > MaxCnt)
+			MaxCnt = cnt;
 	}
-	for (int i = 1; i < MAX; i++) {
-		for (int j = 0; j < cSum[i]; j++)
-			printf("%d\n", i);
+	//벡터 정렬
+	int Freq = INT32_MAX; //최빈값
+	for (int idx = 0; idx < _MAX; idx++)
+	{
+		int cnt = InputCount[idx];
+		int tmp = idx - _HALF;
+		if (cnt != 0)
+		{
+			for (int i = 0; i < cnt; i++)
+				Vsort.push_back(tmp);
+		}
+		if (cnt == MaxCnt)
+		{
+			if (Freq != INT32_MAX)
+			{
+				MaxCnt++;
+			}
+			Freq = tmp;
+		}
+
 	}
-	return 0;
+	//산술평균
+	printf("%.0lf\n", static_cast<double>(SUM) / N);
+	//중앙값
+	printf("%d\n", Vsort[N / 2]);
+	//최빈값
+	printf("%d\n", Freq);
+	//범위
+	printf("%d\n", MAX - MIN);
 }
-
-
 #endif
