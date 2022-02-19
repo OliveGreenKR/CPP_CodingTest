2176ad550a7f97e63808aa70bf12b9b9dd4ddfe4
jeong98CH <jchul1998@naver.com>
2022-02-19 21:54:45 +0900

    dictionary

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,28 +2,33 @@
 #ifdef BACK
 
 #include <iostream>
-#include <cstdio>
 #include <vector>
 using namespace std;
-
 #include <algorithm>
 #include <string>
-#include <queue>
 
-int main()
-{
-	priority_queue<int, vector<int>, less<int>> ans;
-	string str;
-	cin >> str;
-	for (char c : str)
-	{
-		ans.emplace(static_cast<int>(c - '0'));
-	}
-	while (!ans.empty())
-	{
-		printf("%d", ans.top());
-		ans.pop();
-	}
-	printf("\n");
+#define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
+#define loop(i,n) for(int i=0;i<n;++i)
+
+using namespace std;
+
+const int mxN = 2e4;
+
+string s[mxN];
+
+int main() {
+	FASTIO;
+
+	int N; cin >> N;
+	loop(i, N) cin >> s[i];
+
+	sort(s, s + N, [&](string& a, string& b){
+		return a.length() == b.length() ? a < b : a.length() < b.length();
+		});
+
+	N = ::unique(s, s + N) - s;
+	loop(i, N)
+		cout << s[i] << "\n";
+	return 0;
 }
 #endif
