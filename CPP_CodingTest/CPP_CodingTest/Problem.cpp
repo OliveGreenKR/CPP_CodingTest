2a96366561c46e31fc27ce34d60a24eceaaf8579
jeong98CH <jchul1998@naver.com>
2022-02-19 22:58:48 +0900

    10814 solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -8,27 +8,26 @@ using namespace std;
 #include <string>
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
-#define loop(i,n) for(int i=0;i<n;++i)
+#define M_Loop(i,n) for(int i=0;i<n;++i)
 
 using namespace std;
 
-const int mxN = 2e4;
-
-string s[mxN];
-
+const int MAX_IN = 100'000;
+pair<int, string*> ans[MAX_IN]; 
+string ids[MAX_IN];
 int main() {
 	FASTIO;
-
 	int N; cin >> N;
-	loop(i, N) cin >> s[i];
-
-	sort(s, s + N, [&](string& a, string& b){
-		return a.length() == b.length() ? a < b : a.length() < b.length();
-		});
-
-	N = ::unique(s, s + N) - s;
-	loop(i, N)
-		cout << s[i] << "\n";
-	return 0;
+	M_Loop(i, N)
+	{
+		int age;
+		string id;
+		cin >> age >> id;
+		ids[i] = id;
+		ans[i] = make_pair(age, &ids[i]);
+	}
+	::sort(ans, ans + N);
+	M_Loop(i, N)
+		cout << ans[i].first << " " << *(ans[i].second) << "\n";
 }
 #endif
