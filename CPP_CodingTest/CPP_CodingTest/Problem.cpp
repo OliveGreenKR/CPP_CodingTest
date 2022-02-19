d7e8dd571fe8700802853cf7c087ccbc627aee3b
jeong98CH <jchul1998@naver.com>
2022-02-19 18:41:55 +0900

    1427 soved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -4,66 +4,26 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
-#include <algorithm>
-enum
-{
-	_MAX = 8001,
-	_HALF = (_MAX) / 2
-};
 using namespace std;
-vector<int> InputCount(_MAX, 0); //횟수
-vector<int> Vsort;
+
+#include <algorithm>
+#include <string>
+#include <queue>
 
 int main()
 {
-	int MAX = INT32_MIN, MIN = INT32_MAX; //최대값 최소값
-
-	int N;//원소개수
-	cin >> N;
-
-	int SUM = 0;//합계
-	int MaxCnt = 0; // 최대 회수
-	for (int i = 0; i < N; i++)
+	priority_queue<int, vector<int>, less<int>> ans;
+	string str;
+	cin >> str;
+	for (char c : str)
 	{
-		int input;
-		scanf_s("%d", &input);
-		SUM += input;
-		if (input > MAX)
-			MAX = input;
-		if (input < MIN)
-			MIN = input;
-		int cnt = ++InputCount[input + _HALF];
-		if (cnt > MaxCnt)
-			MaxCnt = cnt;
+		ans.emplace(static_cast<int>(c - '0'));
 	}
-	//벡터 정렬
-	int Freq = INT32_MAX; //최빈값
-	for (int idx = 0; idx < _MAX; idx++)
+	while (!ans.empty())
 	{
-		int cnt = InputCount[idx];
-		int tmp = idx - _HALF;
-		if (cnt != 0)
-		{
-			for (int i = 0; i < cnt; i++)
-				Vsort.push_back(tmp);
-		}
-		if (cnt == MaxCnt)
-		{
-			if (Freq != INT32_MAX)
-			{
-				MaxCnt++;
-			}
-			Freq = tmp;
-		}
-
+		printf("%d", ans.top());
+		ans.pop();
 	}
-	//산술평균
-	printf("%.0lf\n", static_cast<double>(SUM) / N);
-	//중앙값
-	printf("%d\n", Vsort[N / 2]);
-	//최빈값
-	printf("%d\n", Freq);
-	//범위
-	printf("%d\n", MAX - MIN);
+	printf("\n");
 }
 #endif
