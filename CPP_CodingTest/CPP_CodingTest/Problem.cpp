a856f2370c3af1d3a56b96844cf5f853ee5351cf
jeong98CH <jchul1998@naver.com>
2022-02-20 12:21:24 +0900

    solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -1,40 +1,39 @@
 #include "pch.h"
 #ifdef BACK
-
 #include <iostream>
 #include <vector>
 using namespace std;
 #include <algorithm>
-#include <string>
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,n) for(int i=0;i<n;++i)
 
 using namespace std;
-
-const int MAX_IN = 1'000'000;
-
-int Input[MAX_IN] = { 0, };
-int Input_Unique[MAX_IN] = { 0, };
-
+vector<pair<int, int>> Input; //{ val, 입력순서}
 int main() {
 	FASTIO;
-
 	int N;
 	cin >> N;
-
-	M_Loop(i, N) 
-	{
-		cin >> Input[i];
-		Input_Unique[i]= Input[i];
-	}
-	::sort(Input_Unique, Input_Unique + N);
-	::unique(Input_Unique, Input_Unique + N);
 	M_Loop(i, N)
 	{
-		auto findit = ::find(Input_Unique, Input_Unique + N, Input[i]);
-		cout << findit - Input_Unique << " ";
+		int in;
+		cin >> in;
+		Input.push_back(pair<int,int>{ in,i });
 	}
+	vector<int> ans(N);
 
+	::sort(Input.begin(),Input.end());
+
+	int comp = 0, prev_val = Input[0].first;
+	for (auto& pair : Input)
+	{
+		int now_val = pair.first;
+		int idx = pair.second;
+		if (prev_val != now_val) ++comp;
+		prev_val = now_val;
+		ans[idx] = comp;
+	}
+	for (int c : ans)
+		cout << c << " ";
 }
 #endif 
