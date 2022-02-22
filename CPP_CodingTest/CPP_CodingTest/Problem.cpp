aef440ece4dd3f7445b51fd0185beae6d71d182a
jeong98CH <jchul1998@naver.com>
2022-02-22 11:58:00 +0900

    operator insert solved!

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,83 +2,96 @@
 #ifdef BACK
 #include <iostream>
 #include <vector>
-#include <map>
+
 using namespace std;
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=st;i<n;i++)
 #define M_Loop_sub(i,st,n) for(int i=n;i>st;i--)
 
-int Board[9][9] = {};
-vector<pair<int, int>> GBlanks;
+vector<int> Input;
+vector<int> Operators(4, 0);
 
-bool Check(int num, const int y, const int x)
+enum
 {
-	M_Loop(i, 0, 9)
+	PLUS = 0,
+	SUB = 1,
+	MUL = 2,
+	DIV = 3
+};
+
+int MIN = INT32_MAX;
+int MAX = INT32_MIN;
+int OperCNT = 0;
+
+void Calcul(int cnt)
+{
+	if (cnt == OperCNT)
 	{
-		if (Board[y][i] == num || Board[i][x] == num)
-			return false;
+		int result = Input[Input.size() - 1];
+		if (result > MAX)
+			MAX = result;
+		if (MIN > result)
+			MIN = result;
+		/*cout << Input[Input.size() - 1] << "\n";*/
+		return;
 	}
 
-	int Ry = (y / 3) * 3;
-	int Rx = (x / 3) * 3;
-
-	M_Loop(j, Ry, (Ry + 3))
+	const int& left = Input[cnt];
+	int& right = Input[cnt+1];
+	int tmp = right;
+	M_Loop(j, 0, Operators.size())
 	{
-		M_Loop(i, Rx, (Rx + 3))
+		if (Operators[j] != 0)
 		{
-			if (num == Board[j][i])
-				return false;
-		}
-	}
-	return true;
-}
-
-void Sdo(int cnt)
-{
-	if (cnt == GBlanks.size())
-	{
-		M_Loop(y, 0, 9)
-		{
-			M_Loop(x, 0, 9)
+			Operators[j]--;
+			switch (j)
 			{
-				cout << Board[y][x] << " ";
+			case PLUS:
+				right = left + right;
+				Calcul(cnt + 1);
+				break;
+			case SUB:
+				right = left - right;
+				Calcul(cnt + 1);
+				break;
+			case MUL:
+				right = left * right;
+				Calcul(cnt + 1);
+				break;
+			case DIV:
+				if (right == 0)
+					return;
+				right = left / right;
+				Calcul(cnt + 1);
+				break;
 			}
-			cout << "\n";
+			Operators[j]++;
+			right = tmp;
 		}
-		exit(0);
-	}
-
-	auto& c = GBlanks[cnt];
-	int y = c.first;
-	int x = c.second;
-
-	M_Loop(i, 1, 10)
-	{
-		if (Check(i, y, x))
-		{
-			Board[y][x] = i;
-			Sdo(cnt + 1);
-			Board[y][x] = 0;
-		}
-
 	}
 }
 
 int main()
 {
-	FASTIO;
-
-	M_Loop(y, 0, 9)
+	int N;
+	cin >> N;
+	M_Loop(i, 0, N)
 	{
-		M_Loop(x, 0, 9)
-		{
-			int N;
-			cin >> Board[y][x];
-			if (!Board[y][x])
-				GBlanks.push_back(make_pair(y, x));
-		}
+		int k;
+		cin >> k;
+		Input.push_back(k);
 	}
-	Sdo(0);
+	M_Loop(i, 0, Operators.size())
+	{
+		cin >> Operators[i];
+		OperCNT += Operators[i];
+	}
+
+	Calcul(0);
+
+	cout << MAX << "\n" << MIN;
+
 }
+
 #endif 
