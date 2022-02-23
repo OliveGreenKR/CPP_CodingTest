9fc398bc86f6d1053c9e9d3991c7f0ecdc50b5e3
jeong98CH <jchul1998@naver.com>
2022-02-23 22:07:47 +0900

    solving...

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -11,40 +11,47 @@ using namespace std;
 
 enum
 {
-	MAX_IN = 51
+	MAX_IN = 1'000'000
 };
 
-int Buff[MAX_IN][MAX_IN][MAX_IN] ={1,};
+int Buff[MAX_IN];
 
-int& W(int a, int b, int c)
+int Combi(int n, int k)
 {
-	if (a <= 0 || b <= 0 || c <= 0)
-	{
-		return Buff[0][0][0];
-	}
-	int& ret = Buff[a][b][c];
+	if (k == 1)
+		return n;
+	if (k > n || k<=0 || n<=0)
+		return 0;
+	if (k == n)
+		return 1;
+	return Combi(n - 1, k-1) + Combi(n-1, k);
+}
+
+int& GetAns(const int& N)
+{
+	int& ret = Buff[N];
 
 	if (ret != 0)
 		return ret;
-	else if (a > 20 || b > 20 || c > 20)
-		ret =  W(20, 20, 20);
-	else if (a < b && b < c)
-		ret =  W(a, b, c - 1) + W(a, b - 1, c - 1) - W(a, b - 1, c);
+	if (N % 2 == 0)
+	{
+
+	}
 	else
-		ret =  W(a-1, b, c) + W(a-1, b- 1, c) + W(a-1, b, c-1) - W(a - 1, b-1, c - 1);
-	return ret;
+	{
+		ret = GetAns(N - 1) + 1;
+	}
+
 }
+
 int main()
 {
 	FASTIO;
-	int a, b, c;
-	while (true)
-	{
-		cin >> a >> b >> c;
-		if (a == -1 && b == -1 && c == -1)
-			break;
-		printf("w(%d, %d, %d) = %d\n", a, b, c, W(a, b, c));
-	}
+
+	int N, K;
+	cin >> N >> K;
+	//GetAns(N);
+	cout << Combi(N, K);
 }
 
 #endif 
