51bbbfc6fccf9d9e8cb2d18438b106bf33af8f04
jeong98CH <jchul1998@naver.com>
2022-02-23 20:08:45 +0900

    14889 Link and start team select

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,96 +2,85 @@
 #ifdef BACK
 #include <iostream>
 #include <vector>
-
+#include <map>
+#include <math.h>
 using namespace std;
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=st;i<n;i++)
 #define M_Loop_sub(i,st,n) for(int i=n;i>st;i--)
 
-vector<int> Input;
-vector<int> Operators(4, 0);
-
 enum
 {
-	PLUS = 0,
-	SUB = 1,
-	MUL = 2,
-	DIV = 3
+	MAX_IN = 20
 };
+map<int, vector<int>> Board;
+vector<bool> visited(MAX_IN, false);
+int ANS = INT32_MAX;
+int N;
 
-int MIN = INT32_MAX;
-int MAX = INT32_MIN;
-int OperCNT = 0;
 
-void Calcul(int cnt)
+void GetSUB()
 {
-	if (cnt == OperCNT)
+	int T1 = 0, T2 = 0;
+	M_Loop(now, 0, N)
 	{
-		int result = Input[Input.size() - 1];
-		if (result > MAX)
-			MAX = result;
-		if (MIN > result)
-			MIN = result;
-		/*cout << Input[Input.size() - 1] << "\n";*/
-		return;
+		M_Loop(mate, now + 1, N)
+		{
+			if (visited[now] && visited[mate])
+			{
+				T1 += Board[now][mate];
+				T1 += Board[mate][now];
+			}
+			else if (!visited[now] && !visited[mate])
+			{
+				T2 += Board[now][mate];
+				T2 += Board[mate][now];
+			}
+		}
 	}
 
-	const int& left = Input[cnt];
-	int& right = Input[cnt+1];
-	int tmp = right;
-	M_Loop(j, 0, Operators.size())
+	ANS = ::min(ANS, abs(T1 - T2));
+	return;
+}
+
+void ChoiceTeam(int cnt, int now = 0)
+{
+	if (cnt == N / 2)
 	{
-		if (Operators[j] != 0)
-		{
-			Operators[j]--;
-			switch (j)
-			{
-			case PLUS:
-				right = left + right;
-				Calcul(cnt + 1);
-				break;
-			case SUB:
-				right = left - right;
-				Calcul(cnt + 1);
-				break;
-			case MUL:
-				right = left * right;
-				Calcul(cnt + 1);
-				break;
-			case DIV:
-				if (right == 0)
-					return;
-				right = left / right;
-				Calcul(cnt + 1);
-				break;
-			}
-			Operators[j]++;
-			right = tmp;
-		}
+		GetSUB();
+		return;
+	}
+	if (now >= N)
+		return;
+
+	if (visited[now] == false)
+	{
+		visited[now] = true;
+		ChoiceTeam(cnt + 1, now + 1);
+		visited[now] = false;
+		ChoiceTeam(cnt, now + 1);
 	}
 }
 
 int main()
 {
-	int N;
+	FASTIO;
+
 	cin >> N;
 	M_Loop(i, 0, N)
 	{
-		int k;
-		cin >> k;
-		Input.push_back(k);
-	}
-	M_Loop(i, 0, Operators.size())
-	{
-		cin >> Operators[i];
-		OperCNT += Operators[i];
+		vector<int> tmp(N);
+		M_Loop(j, 0, N)
+		{
+			cin >> tmp[j];
+		}
+		Board.emplace(i, tmp);
 	}
 
-	Calcul(0);
+	ChoiceTeam(0, 0);
 
-	cout << MAX << "\n" << MIN;
+	cout << ANS << "\n";
 
 }
-
 #endif 
