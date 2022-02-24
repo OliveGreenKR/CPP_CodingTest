718b30aba433c80354200e5e40870c0e5e07080d
jeong98CH <jchul1998@naver.com>
2022-02-24 13:33:51 +0900

    BJ 1149 , time out

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,47 +2,61 @@
 #ifdef BACK
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=st;i<n;i++)
 #define M_Loop_sub(i,st,n) for(int i=n;i>st;i--)
-#define null_pair make_pair(-1,-1)
+using int64 = long long;
 
 enum
 {
-	MAX_IN = 100+1
+	Red = 0,
+	Green = 1,
+	Blue = 2,
+	MAX_IN = 1000+1
 };
+int N;
+int64 MIN = INT64_MAX;
+int64 sel = 0;
+vector<pair<int, vector<int>>> Buff(MAX_IN, make_pair(0,vector<int>(3, 0)));
 
-long long Buff[MAX_IN] = {0,1,1,1,2,2,3,4,5,7,9};
-
-
-long long GetAns(const int& N)
+void GetAns(const int& cnt,int ex =-1)
 {
-	if (N < 1)
-		return 0;
-
-	long long& ret = Buff[N];
-	
-	if (ret != 0)
-		return ret;
-	else
-		ret = GetAns(N - 5)+ GetAns(N - 1);
-
-	return ret;
+	if (cnt == N)
+	{
+		MIN = min(MIN, sel);
+		return;
+	}
+	M_Loop(i, 0, 3)
+	{
+		if (i == ex)
+			continue;
+		else
+		{
+			sel += Buff[cnt+1].second[i];
+			GetAns(cnt + 1, i);
+			sel -= Buff[cnt+1].second[i];
+		}
+	}
+	return;
 }
 
 int main()
 {
 	FASTIO;
-	int T;
-	cin >> T;
-	M_Loop(i, 0, T)
+	cin >> N;
+	M_Loop(i, 1, N+1)
 	{
-		int N;
-		cin >> N;
-		cout << GetAns(N) << "\n";
+		int R, G, B;
+		cin >> R >> G >> B;
+		Buff[i].second = vector<int>{R,G,B};
 	}
+	/*cout << GetAns(N) << "\n";*/
+	GetAns(0);
+	cout << MIN << "\n";
+	return 0;
 }
 
 #endif 
