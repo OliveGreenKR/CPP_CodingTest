810693c36dcc77cdcc9f4ae6779eb08f15ed6c10
jeong98CH <jchul1998@naver.com>
2022-02-25 22:17:26 +0900

    wind selection solved, but memory over

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -14,7 +14,7 @@ enum
 	MAX_IN = 10'000+1
 };
 
-int64 Ans[MAX_IN] = { 0, };
+int64 Ans[MAX_IN][MAX_IN] = {  }; //[처음으로 N고름][그뒤]
 int64 Cost[MAX_IN] = { 0, };
 
 int main()
@@ -23,18 +23,24 @@ int main()
 	int N;
 	cin >> N;
 
+	int64 MAX = 0;
+
 	M_Loop(i, 1, N+1)
 	{
 		cin >> Cost[i];
 	}
-	Ans[1] = Cost[1];
-	Ans[2] = Cost[2]+Cost[1];
-
-	M_Loop(i, 3, N + 1)
+	M_Loop(j, 1, N + 1)
 	{
-		Ans[i] = ::max(Ans[i - 2] + Cost[i], Ans[i - 3] + Cost[i - 1] + Cost[i]);
+		Ans[j][j] = Cost[j];
+		Ans[j][j+1] = Cost[j] + Cost[j + 1];
+		M_Loop(i, j+2, N + 1)
+		{
+			Ans[j][i] = ::max(Ans[j][i - 2] + Cost[i], Ans[j][i - 3] + Cost[i - 1] + Cost[i]);
+			MAX = ::max(MAX, Ans[j][i]);
+		}
 	}
-	cout << *(::max_element(Ans, Ans + N)) << "\n";
+	cout << MAX << "\n";
 }
 
 #endif 
+//이거는 순서대로일때, 문제는 조합으로 해야한다.(순서없음)
