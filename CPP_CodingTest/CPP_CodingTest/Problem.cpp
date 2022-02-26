690506105f25e45537dfe8d617208096ad781f3c
jeong98CH <jchul1998@naver.com>
2022-02-26 17:32:23 +0900

    LIS solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=st;i<n;i++)
-#define M_Loop_sub(i,st,n) for(int i=n;i>st;i--)
+#define M_Loop_sub(i,n,st) for(int i=n;i>st;i--)
 using int64 = long long;
 
 enum
 {
-	MAX_IN = 10'000+1
+	MAX_IN = 1'000+1
 };
 
-int64 Ans[MAX_IN] = {  };
-int64 Cost[MAX_IN] = { 0, };
+int Ans[MAX_IN] = { 0,1,}; //N까지 일때 가장 긴 증가부분수열의 길이
+int Cost[MAX_IN] = { 0, };
 
 int main()
 {
@@ -23,21 +23,22 @@ int main()
 	int N;
 	cin >> N;
 
-	int64 MAX = 0;
-
 	M_Loop(i, 1, N+1)
 	{
 		cin >> Cost[i];
 	}
-	Ans[1] = Cost[1];
-	Ans[2] = Cost[1] + Cost[2];
-	M_Loop(j, 3, N + 1)
-	{
-		Ans[j] = ::max(Ans[j - 2] + Cost[j], Ans[j - 3] + Cost[j - 1] + Cost[j]);
-		Ans[j] = ::max(Ans[j - 1], Ans[j]);
-	}
 
-	cout << Ans[N] << "\n";
+	M_Loop(j, 2, N + 1)
+	{
+		int mine = Cost[j];
+		Ans[j] = 1;
+		M_Loop_sub(k, j, 0)
+		{
+			if (Cost[k] < mine)
+				Ans[j] = ::max(Ans[k] + 1, Ans[j]);
+		}
+	}
+	cout << *(::max_element(Ans, Ans + N+1)) << "\n";
 }
 
 #endif 
