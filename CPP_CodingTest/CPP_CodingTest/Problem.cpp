2c7fbfa9d3761568cd4c4ce393762bf8f85cc6ed
jeong98CH <jchul1998@naver.com>
2022-02-28 19:02:31 +0900

    LIS solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -12,31 +12,34 @@ using int64 = long long;
 
 enum
 {
-	MAX_IN = 1'000+1
+	MAX_IN = 100'000+1
 };
-string s1;
-string s2;
 
-int Ans[MAX_IN][MAX_IN] = {}; //[i][j] '이전'까지의 LCS의 길이
-
+int64 Arr[MAX_IN] = {};
+int64 Ans[MAX_IN] = {}; //i까지 최대 연속합
 int main()
 {
 	FASTIO;
-	cin >> s1 >> s2; 
-	int len1 = s1.length();
-	int len2 = s2.length();
+	int N;
+	cin >> N;
 
-	M_Loop(i, 1, len1+1)
+	M_Loop(i, 1, N + 1)
 	{
-		M_Loop(j, 1, len2+1)
+		cin >> Arr[i];
+	}
+
+	Ans[1] = Arr[1];
+	M_Loop(i, 2, N + 1)
+	{
+		if(Ans[i-1]<0)//이제 더이상 더하면 손해, 새로시작
+			Ans[i] = Arr[i];
+		else//더하는게 이득이므로 더한다
 		{
-			if (s1[i - 1] == s2[j - 1])
-				Ans[i][j] = Ans[i - 1][j - 1] + 1;
-			else
-				Ans[i][j] = ::max(Ans[i - 1][j],Ans[i][j-1]);
+			Ans[i] = Ans[i - 1] + Arr[i];
 		}
 	}
-	cout << Ans[len1][len2] << "\n";
+
+	cout << *(::max_element(Ans + 1, Ans + N + 1)) << "\n";
 }
 
 #endif 
