14714849a257c025fbe96456bd32a9a05f364d00
jeong98CH <jchul1998@naver.com>
2022-02-28 21:10:10 +0900

    DP NAP SACK solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,44 +2,30 @@
 #ifdef BACK
 #include <iostream>
 #include <algorithm>
-#include <string>
 using namespace std;
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
-#define M_Loop(i,st,n) for(int i=st;i<n;i++)
-#define M_Loop_sub(i,n,st) for(int i=n;i>st;i--)
+#define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
+#define M_Loop_sub(i,n,st) for(int i=(n);i>(st);i--)
 using int64 = long long;
 
-enum
-{
-	MAX_IN = 100'000+1
-};
 
-int64 Arr[MAX_IN] = {};
-int64 Ans[MAX_IN] = {}; //i까지 최대 연속합
+int64 N, K, W, V, D[100001] = { 0, };
+#include<stdio.h>
 int main()
 {
 	FASTIO;
-	int N;
-	cin >> N;
-
-	M_Loop(i, 1, N + 1)
+	cin >> N >> K;
+	M_Loop(i, 0, N)
 	{
-		cin >> Arr[i];
-	}
-
-	Ans[1] = Arr[1];
-	M_Loop(i, 2, N + 1)
-	{
-		if(Ans[i-1]<0)//이제 더이상 더하면 손해, 새로시작
-			Ans[i] = Arr[i];
-		else//더하는게 이득이므로 더한다
+		cin >> W >> V;
+		M_Loop_sub(j, K, W-1)
 		{
-			Ans[i] = Ans[i - 1] + Arr[i];
+			D[j] = ::max(D[j], D[j - W] + V);
 		}
 	}
-
-	cout << *(::max_element(Ans + 1, Ans + N + 1)) << "\n";
+	cout << D[K] << "\n";
+	return 0;
 }
 
 #endif 
