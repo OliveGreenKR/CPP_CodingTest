473fcb7369e23a8ff8cdeb095bb945252cbed881
jeong98CH <jchul1998@naver.com>
2022-03-01 15:08:13 +0900

    Fibonacci with over int64

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,38 +2,60 @@
 #ifdef BACK
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
 #define M_Loop_sub(i,n,st) for(int i=(n);i>(st);i--)
+
 using int64 = long long;
+
 
 enum
 {
-	MAX_IN = 1000 +1,
-	SK =1,
-	CY =2
+	MAX_IN = 10000 +1,
 };
 
-int64 DP[MAX_IN] = { 0 , CY,SK,CY};
+inline int Ctoi(char ch) { return ch - '0'; }
 
+string DP[MAX_IN] = { "0","1","1", };
+string Sum(string s1, string s2) //s2>s1
+{
+	::reverse(s1.begin(), s1.end());
+	::reverse(s2.begin(), s2.end());
+
+	while (s1.length() != s2.length())
+		s1.append("0");
+
+	string tmp;
+	tmp.resize(s2.length()+1,'0');
+
+	M_Loop(i, 0, s2.length())
+	{	
+		int sum = Ctoi(tmp[i]) + Ctoi(s1[i]) + Ctoi(s2[i]);
+		tmp[i] = (sum % 10) + '0';
+		tmp[i+1] = (sum / 10) + '0';
+	}
+
+	if (tmp[tmp.length()-1] == '0')
+		tmp = tmp.substr(0, tmp.length() - 1);
+	reverse(tmp.begin(), tmp.end());
+	return tmp;
+}
 int main()
 {
 	FASTIO;
-	
+
 	int N;
 	cin >> N;
 
-	M_Loop(i, 4, N + 1)
+	M_Loop(i, 3, N + 1)
 	{
-		DP[i] = DP[i - 3] ^ 1;
+		DP[i] = Sum(DP[i - 2], DP[i - 1]);
 	}
 
-	if (DP[N] == CY)
-		cout << "CY\n";
-	else
-		cout << "SK\n";
+	cout << DP[N] << "\n";
 }
 
 #endif 
