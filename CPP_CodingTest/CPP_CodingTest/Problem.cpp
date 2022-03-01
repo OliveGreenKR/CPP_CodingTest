cb3dcd72c9e67d1e21f27e52850e6989768dc3bb
jeong98CH <jchul1998@naver.com>
2022-03-01 15:37:03 +0900

    최대곱

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,14 +2,13 @@
 #ifdef BACK
 #include <iostream>
 #include <algorithm>
-#include <string>
 using namespace std;
 
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
 #define M_Loop_sub(i,n,st) for(int i=(n);i>(st);i--)
-
 using int64 = long long;
+using float64 = long double;
 
 
 enum
@@ -17,45 +16,27 @@ enum
 	MAX_IN = 10000 +1,
 };
 
-inline int Ctoi(char ch) { return ch - '0'; }
+float64 DP[MAX_IN] = {};
+float64 Arr[MAX_IN] = {};
 
-string DP[MAX_IN] = { "0","1","1", };
-string Sum(string s1, string s2) //s2>s1
-{
-	::reverse(s1.begin(), s1.end());
-	::reverse(s2.begin(), s2.end());
-
-	while (s1.length() != s2.length())
-		s1.append("0");
-
-	string tmp;
-	tmp.resize(s2.length()+1,'0');
-
-	M_Loop(i, 0, s2.length())
-	{	
-		int sum = Ctoi(tmp[i]) + Ctoi(s1[i]) + Ctoi(s2[i]);
-		tmp[i] = (sum % 10) + '0';
-		tmp[i+1] = (sum / 10) + '0';
-	}
-
-	if (tmp[tmp.length()-1] == '0')
-		tmp = tmp.substr(0, tmp.length() - 1);
-	reverse(tmp.begin(), tmp.end());
-	return tmp;
-}
 int main()
 {
-	FASTIO;
-
 	int N;
 	cin >> N;
-
-	M_Loop(i, 3, N + 1)
+	M_Loop(i, 1, N+1)
 	{
-		DP[i] = Sum(DP[i - 2], DP[i - 1]);
+		cin >> Arr[i];
 	}
 
-	cout << DP[N] << "\n";
+	DP[1] = Arr[1];
+	M_Loop(i, 2, N + 1)
+	{
+		DP[i] = ::max(DP[i - 1] * Arr[i], Arr[i]);	//곱하다가 1미만이 되면 자기자신 선택
+	}
+	cout.precision(3);
+	cout.setf(ios::fixed, ios::floatfield);
+	
+	cout << *(::max_element(DP + 1, DP + N + 1)) << "\n";
 }
 
 #endif 
