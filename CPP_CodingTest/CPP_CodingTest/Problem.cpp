85a6a19817a4a87a2b9dcc96ebdf10873638cb31
jeong98CH <jchul1998@naver.com>
2022-03-01 17:36:59 +0900

    Pixxa solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -12,55 +12,19 @@ using float64 = long double;
 
 enum
 {
-	MAX_IN = 6 +1
+	MAX_IN = 10 +1
 };
-int N, M;
 
-int Cost[MAX_IN][MAX_IN];
-int Cache[MAX_IN][MAX_IN][4]; // [y][x][dir]]
-int Ans = INT32_MAX;
-
-
-int Path(int y, int x, int dir=0)
-{
-	if (y > N)
-		return 0;
-
-	if (x<=0 || x>M)
-		return INT32_MAX;
-	
-	int& now = Cache[y][x][dir];
-
-	if (now != 0)
-		return now;
-
-	if (dir == 1) now = ::min(Path(y + 1,x,2),			Path(y + 1, x + 1, 3))			+ Cost[y][x];
-	else if (dir == 2) now = ::min(Path(y + 1, x + 1, 3),	Path(y + 1, x - 1, 1))		+ Cost[y][x];
-	else if (dir == 3) now = ::min(Path(y + 1, x, 2), Path(y + 1, x - 1, 1))			+ Cost[y][x];
-	else now = ::min({Path(y + 1,x,2), Path(y + 1, x + 1, 3), Path(y + 1, x - 1, 1) })	+ Cost[y][x];
-
-	return now;
-}
-
+int DP[MAX_IN] = { 0,0,1,0 };  //N 일때 얻는 즐거움
 int main()
 {
+	int N;
+	cin >> N;
 
-	cin >> N >> M;
+	M_Loop(i, 3, N + 1)
+		DP[i] = ((i + 1) / 2* (i/2)) + DP[(i + 1)/2] + DP[i / 2];
 
-	M_Loop(y, 1, N+1)
-	{
-		M_Loop(x, 1, M + 1)
-		{
-			cin >> Cost[y][x];
-		}
-	}
-
-	M_Loop(x, 1, M + 1)
-	{
-		Ans = ::min(Path(1, x, 0),Ans);
-	}
-
-	cout << Ans << "\n";
+	cout << DP[N] << "\n";
 }
 
 #endif 
