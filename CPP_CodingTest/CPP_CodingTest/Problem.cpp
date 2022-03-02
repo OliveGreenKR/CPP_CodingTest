200deb81d3286a1725d459bc2e9e12bd9c4753b5
jeong98CH <jchul1998@naver.com>
2022-03-02 15:20:13 +0900

    tmp save

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -12,19 +12,62 @@ using float64 = long double;
 
 enum
 {
-	MAX_IN = 10 +1
+	MAX_IN = 100'000
 };
-
-int DP[MAX_IN] = { 0,0,1,0 };  //N 일때 얻는 즐거움
+int N;
+int MAX_CNT = 0;
+pair<int, int> Meets[MAX_IN];
+int cache[MAX_IN] = {};
 int main()
 {
-	int N;
+	FASTIO;
+
 	cin >> N;
 
-	M_Loop(i, 3, N + 1)
-		DP[i] = ((i + 1) / 2* (i/2)) + DP[(i + 1)/2] + DP[i / 2];
+	M_Loop(i, 0, N)
+	{
+		int f, s;
+		cin >> f >> s;
+		Meets[i] = pair<int, int>{ f,s };
+	}
+	
+	::sort(Meets, Meets + N, [&](auto& A, auto& B) {
+		int left = (A.second - A.first);
+		int right = (B.second - B.first);
+		if (A.first == B.first)
+			return left < right;
+		return A.first < B.first;
+		 });
 
-	cout << DP[N] << "\n";
+	int end = 0;
+	int cnt = 0;
+	M_Loop(i,0,N)
+	{
+		cnt = 1;
+		const auto& now = Meets[i];
+
+		int& record = cache[i];
+		if (record != 0)
+		{
+			cnt += record;
+			continue;
+		}
+		end = now.second;
+		for (auto& meet : Meets)
+		{
+			if (meet.first < end)
+				continue;
+			cout << meet.first << "~" << meet.second << "\n";
+			cnt++;
+			end = meet.second;
+		}
+		record = cnt;
+
+		cout << "cnt : " << cnt << "\n-----------------------------\n";
+		MAX_CNT = ::max(cnt, MAX_CNT);
+	}
+
+	cout << MAX_CNT;
 }
 
 #endif 
