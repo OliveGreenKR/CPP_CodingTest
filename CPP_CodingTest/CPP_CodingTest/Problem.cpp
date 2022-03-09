e58a5837e2a705ecd18d397dee2ae43c5fea7136
jeong98CH <jchul1998@naver.com>
2022-03-09 18:49:01 +0900

    my queue solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 #ifdef BACK
 #include <iostream>
-#include <stack>
 using namespace std;
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
@@ -9,12 +8,112 @@ using namespace std;
 using int64 = long long;
 using float64 = long double;
 
-enum
+struct Node
 {
-	MAX_IN = 1'000'000
+	Node(int data) : _data(data)
+	{}
+	int _data = 0;
+	Node* _next = nullptr;
+	Node* _pre = nullptr;
 };
-stack<int> cache;
-int inputs[MAX_IN] = {};
+class MyQueue 
+{
+public:
+	MyQueue()
+	{
+		_back = nullptr;
+		_front = nullptr;
+	}
+	~MyQueue()
+	{
+		delete _back, _front;
+	}
+
+	void Push(int elm)
+	{
+		if (Empty() == true)
+		{
+			_front = new Node(elm);
+			_back = _front;
+			_back->_next = _front;
+			_front->_pre = _back;
+		}
+		else
+		{
+			Node* Other = new Node(elm);
+			_back->_next = Other;
+			Other->_pre = _back;
+			_back = Other;
+		}
+		_size++;
+	}
+	int Pop()//back이 제일 최신
+	{
+		if (Empty() != true)
+		{
+			int top = _front->_data;
+			Node* tmp = _front;
+			_front = _front->_next;
+			if (_front)
+				_front->_pre = nullptr;
+			else
+				_back = _front;//nullptr
+			delete tmp;
+			_size--;
+			return top;
+		}
+		return -1;
+	}
+	int Size()
+	{
+		return _size;
+	}
+	bool Empty()
+	{
+		if (_size == 0)
+			return true;
+		return false;
+	}
+	int Front()
+	{
+		if (Empty())
+			return -1;
+		return _front->_data;
+	}
+	int Back()
+	{
+		if (Empty())
+			return -1;
+		return _back->_data;
+	}
+
+private:
+	Node* _back;
+	Node* _front;
+	int _size = 0;
+}_GQueue;
+
+
+void Ans(const string & cmd)
+{
+	if (cmd == "push")
+	{
+		int elm;
+		cin >> elm;
+		_GQueue.Push(elm);
+	}
+
+	else if (cmd == "pop")
+		cout << _GQueue.Pop() << "\n";
+	else if (cmd == "size")
+		cout << _GQueue.Size() << "\n";
+	else if (cmd == "empty")
+		cout << static_cast<int>(_GQueue.Empty()) << "\n";
+	else if (cmd == "front")
+		cout << _GQueue.Front() << "\n";
+	else if (cmd == "back")
+		cout << _GQueue.Back() << "\n";
+}
 int main()
 {
 	FASTIO;
@@ -22,27 +121,13 @@ int main()
 	cin >> N;
 
 	M_Loop(i, 0, N)
-		cin >> inputs[i];
-	cache.push(inputs[N-1]);
-	inputs[N - 1] = -1;
-	M_Loop_sub(i, N - 2, -1)
 	{
-		int& now = inputs[i];
-		int tmp = now;
-		while (now >= cache.top())
-		{
-			cache.pop();
-			if (cache.empty())
-			{
-				now = -1;
-				break;
-			}
-		}
-		if (now != -1)
-			now = cache.top();
-		cache.push(tmp);
+		string cmd;
+		cin.width(5);
+		cin >> cmd;
+		Ans(cmd);
 	}
-	M_Loop(i, 0, N)
-		cout << inputs[i] << " ";
+	return 0;
 }
+
 #endif 
