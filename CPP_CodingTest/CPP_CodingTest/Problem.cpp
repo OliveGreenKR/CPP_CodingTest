4a8945d103fed199a8e9fc10175dac234608063a
jeong98CH <jchul1998@naver.com>
2022-03-11 20:02:22 +0900

    solved.... q intiallize!

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -13,36 +13,37 @@ enum
 {
 	MAX_INPUT = 100
 };
-queue<pair<int, int>> q;//[중요도][초반위치]
-int cache[MAX_INPUT]; //[중요도]
-
+//queue<pair<int, int>> q;//[중요도][표시]
 int main()
 {
 	FASTIO;
 	int T;
 	cin >> T;
-
 	M_Loop(i, 0, T)
-	{
+	{	
+		queue<pair<int, int>> q;//[중요도][표시]
 		int N, M;
-		//현재 M번째인 원소가 재배열되었을 때 탈출순서
+		int cache[MAX_INPUT] = {};
 		cin >> N >> M;
 		M_Loop(j, 0, N)
 		{
 			int cost;
 			cin >> cost;
-			q.push({ cost, j });
+			if (j == M)
+				q.push({ cost, 1 });
+			else
+				q.push({ cost, 0 });
 			cache[j] = cost;
 		}
 		::sort(cache, cache + N, ::greater<int>());
 		int idx = 0;
-		while (idx <= N)
+		while (!q.empty())
 		{
 			auto tmp = q.front();
-			if (tmp.first == cache[idx])//탈출
+			if (tmp.first == cache[idx])//탈출순서
 			{
 				idx++;
-				if (q.front().second == M)
+				if (tmp.second == 1)
 				{
 					cout << idx << "\n";
 					break;
@@ -54,6 +55,7 @@ int main()
 				q.pop();
 				q.push(tmp);
 			}
+
 		}
 	}
 }
