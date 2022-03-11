7eff427da25121747da2c227fe0181f4d920c420
jeong98CH <jchul1998@naver.com>
2022-03-11 19:26:13 +0900

    1669 print queue, solved but time over

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -4,33 +4,59 @@
 #ifdef BACK
 #include <iostream>
 #include <queue>
+#include <algorithm>
 using namespace std;
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
 #define M_Loop_sub(i,st,n) for(int i=(st);i>(n);i--)
-using int64 = long long;
-using float64 = long double;
+enum
+{
+	MAX_INPUT = 100
+};
+queue<pair<int, int>> q;//[중요도][초반위치]
+int cache[MAX_INPUT]; //[중요도]
 
-queue<int> Ans;
 int main()
 {
 	FASTIO;
-	int N;
-	cin >> N;
+	int T;
+	cin >> T;
 
-	M_Loop(i, 1, N + 1)
-		Ans.push(i);
-
-	while (Ans.size() != 1)
+	M_Loop(i, 0, T)
 	{
-		Ans.pop();
-		if (Ans.size() == 1)
-			break;
-		int tmp = Ans.front();
-		Ans.pop();
-		Ans.push(tmp);
+		int N, M;
+		//현재 M번째인 원소가 재배열되었을 때 탈출순서
+		cin >> N >> M;
+		M_Loop(j, 0, N)
+		{
+			int cost;
+			cin >> cost;
+			q.push({ cost, j });
+			cache[j] = cost;
+		}
+		::sort(cache, cache + N, ::greater<int>());
+		int idx = 0;
+		while (idx <= N)
+		{
+			auto tmp = q.front();
+			if (tmp.first == cache[idx])//탈출
+			{
+				idx++;
+				if (q.front().second == M)
+				{
+					cout << idx << "\n";
+					break;
+				}
+				q.pop();
+			}
+			else
+			{
+				q.pop();
+				q.push(tmp);
+			}
+		}
 	}
-	cout << Ans.front() << "\n";
 }
+
 
 #endif 
