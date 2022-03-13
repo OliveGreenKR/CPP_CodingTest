f3d4d2d2f2cbe87e62c84a150c60709a4d0010ee
jeong98CH <jchul1998@naver.com>
2022-03-13 21:57:47 +0900

    arr ver

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -4,52 +4,61 @@
 
 #ifdef BACK
 #include <iostream>
-#include <deque>
-#include <vector>
 using namespace std;
-deque<int> dq;
-int cnt = 0;
-
-inline void Func1() { dq.pop_front();}
-inline void Func2(int n) {
-    for (int i = 0; i < n; i++)
-    {
-        int tmp = dq.front();
-        dq.pop_front();
-        dq.push_back(tmp);
-        cnt++;
-    } 
+#define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
+#define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
+#define M_Loop_sub(i,st,n) for(int i=(st);i>(n);i--)
+enum
+{
+	MAX_IN = 1000000
+};
+int Arr[MAX_IN];
+int front, back, cnt = 0;
+inline void Func1() { front++; }
+inline void Func2(int n)
+{
+	M_Loop(i, 0, n)
+	{
+		Arr[back++] = Arr[front++];
+		cnt++;
+	}
 }
-inline void Func3(int n) {
-    for (int i = 0; i < n; i++)
-    {
-        int tmp = dq.back();
-        dq.pop_back();
-        dq.push_front(tmp);
-        cnt++;
-    }
+inline void Func3(int n)
+{
+	M_Loop(i, 0, n)
+	{
+		Arr[--front] = Arr[--back];
+		cnt++;
+	}
 }
-int main() {
-    int N, M;
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++) //초기화
-        dq.push_back(i);
+int main()
+{
+	FASTIO;
+	int N, M;
+	cin >> N >> M;
 
-    for (int i = 0; i < M; i++) {
-        int num;
-        cin >> num;
-        int index=0;
-        if (dq.front() == num)
-        {
-            Func1();
-            continue;
-        }
-        while (dq[index] != num)
-            index++;
-        (index) <= (dq.size() - index) ? Func2(index) : Func3(dq.size() - index);
-        Func1();
-    }
-    cout << cnt;
+	//초기값 설정
+	front = MAX_IN/2; back = MAX_IN / 2;
+	M_Loop(i, 1, N + 1)
+		Arr[back++] = i;
+	//타겟
+	M_Loop(i, 0, M)
+	{
+		int num;
+		cin >> num;
+		if (Arr[front] == num)
+		{
+			Func1();
+			continue;
+		}
+		int index = front;
+		while (Arr[index] != num)
+			index++;
+		(index - front) <= (back - index) ? Func2(index - front) : Func3(back - index);
+		Func1();
+	}
+	cout << cnt;
+	return 0;
 }
 #endif 
 
