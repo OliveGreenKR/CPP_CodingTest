5cf07c71863aa46b02ff2511278c02541e8f6893
jeong98CH <jchul1998@naver.com>
2022-03-16 18:53:34 +0900

    divide and conquere base solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,8 +2,11 @@
 #include <cstdio>
 #pragma warning(disable: 4996)
 
+
 #ifdef BACK
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
 #define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
@@ -11,18 +14,17 @@ using namespace std;
 
 enum
 {
-	MAX_IN = 128,
-	Color_white = false,
-	Color_blue = true
+	MAX_IN = 64,
+	Color_white = false,//0
+	Color_black = true//1
 };
 int N;
 bool Table[MAX_IN][MAX_IN]; 
+string ans="";
 
-int White = 0;
-int Blue = 0;
-
-void DivCon(int nowX,int nowY, int endX, int endY)
+void DivCon(int nowX,int nowY, int endX, int endY,int cnt=0)
 {
+
 	int middleX = (nowX + endX) / 2;
 	int middleY = (nowY + endY) / 2;
 	bool mark = Table[nowY][nowX];
@@ -33,28 +35,34 @@ void DivCon(int nowX,int nowY, int endX, int endY)
 		{
 			if (mark != Table[j][i])
 			{
-				DivCon(nowX, nowY, middleX, middleY); //1
-				DivCon(middleX, nowY, endX, middleY); //2
-				DivCon(nowX, middleY, middleX, endY); //3
-				DivCon(middleX, middleY, endX, endY); //4
+				ans += "(";
+				DivCon(nowX, nowY, middleX, middleY,1); //1
+				DivCon(middleX, nowY, endX, middleY,2); //2
+				DivCon(nowX, middleY, middleX, endY,3); //3
+				DivCon(middleX, middleY, endX, endY,4); //4
+				ans += ")";
 				return;
 			}
 		}
 	}
-	mark == Color_white ? White++ : Blue++;
+	ans +=::to_string(mark);
 }
 int main()
 {
 	FASTIO
-	cin >> N;
+		cin >> N;
+	
 	M_Loop(j, 0, N)
 	{
 		M_Loop(i, 0, N)
-			cin >> Table[j][i];
+		{
+			char c;
+			cin >> c;
+			Table[j][i] = c - '0';
+		}
 	}
-
-	DivCon(0, 0, N, N);
-	cout << White << "\n" << Blue << "\n";
+	DivCon(0, 0, N, N,0);
+	cout << ans;
 }
 #endif 
 
