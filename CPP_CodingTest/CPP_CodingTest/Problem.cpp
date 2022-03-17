8a633306868e1bd9fb3d9595e3dadc3db8c8f31c
jeong98CH <jchul1998@naver.com>
2022-03-17 15:22:14 +0900

    solving.. stack overflow

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -13,39 +13,61 @@ using namespace std;
 #define M_Loop_sub(i,st,n) for(int i=(st);i>(n);i--)
 
 enum
+{ MAX_IN = 2187,}; //3^7
+
+enum State
 {
-	MAX_IN = 64,
-	Color_white = false,//0
-	Color_black = true//1
+	High	= 1,
+	Middle	= 0,
+	Low		= -1
 };
+
 int N;
 bool Table[MAX_IN][MAX_IN]; 
-string ans="";
+int Ans[3] = {};
 
-void DivCon(int nowX,int nowY, int endX, int endY,int cnt=0)
+void DivCon(int nowX,int nowY, int endX, int endY,int row=0)
 {
+	int middleX1 = (nowX + endX) / 3;
+	int middleX2 = (nowX + endX) / 3 * 2;
 
-	int middleX = (nowX + endX) / 2;
-	int middleY = (nowY + endY) / 2;
+	int middleY1 = (nowY + endY) / 3;
+	int middleY2 = (nowY + endY) / 3 * 2;
+
 	bool mark = Table[nowY][nowX];
 
-	M_Loop(j,nowY,endY)
+	M_Loop(j, nowY, endY)
 	{
-		M_Loop(i,nowX,endX)
+		M_Loop(i, nowX, endX)
 		{
 			if (mark != Table[j][i])
 			{
-				ans += "(";
-				DivCon(nowX, nowY, middleX, middleY,1); //1
-				DivCon(middleX, nowY, endX, middleY,2); //2
-				DivCon(nowX, middleY, middleX, endY,3); //3
-				DivCon(middleX, middleY, endX, endY,4); //4
-				ans += ")";
+
+				DivCon(nowX, nowY, middleX1, middleY1);			//1
+				DivCon(middleX1,nowY,middleX2,middleY1);		//2
+				DivCon(middleX2, nowY, endX, middleY1);			//3
+
+				DivCon(nowX, middleY1, middleX1, middleY2);		//4
+				DivCon(middleX1, middleY1, middleX2, middleY2);	//5
+				DivCon(middleX2, middleY1, endX, middleY2);		//6
+
+				DivCon(nowX, middleY2, middleX1, endY);			//7
+				DivCon(middleX1, middleY2, middleX2, endY);		//8
+				DivCon(middleX2, middleY2, endX, endY);			//9
+
 				return;
 			}
 		}
 	}
-	ans +=::to_string(mark);
+	switch(mark)
+	{
+	case State::High:
+		Ans[2]++; break;
+	case State::Middle:
+		Ans[1]++; break;
+	case State::Low:
+		Ans[0]++; break;
+	}
 }
 int main()
 {
@@ -56,13 +78,10 @@ int main()
 	{
 		M_Loop(i, 0, N)
 		{
-			char c;
-			cin >> c;
-			Table[j][i] = c - '0';
+			cin >> Table[j][i];
 		}
 	}
-	DivCon(0, 0, N, N,0);
-	cout << ans;
+	DivCon(0, 0, N, N);
 }
 #endif 
 
