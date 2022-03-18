3f5c8c680ec0336b8d4b422a2d6c52c897ebbe52
jeong98CH <jchul1998@naver.com>
2022-03-18 14:46:32 +0900

    ddd

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -5,62 +5,36 @@
 
 #ifdef BACK
 #include <iostream>
+#include <vector>
+#include <deque>
 using namespace std;
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
 #define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
 #define M_Loop_sub(i,st,n) for(int i=(st);i>(n);i--)
-
-using namespace std;
-int N;
-int Table[3000][3000];
-int Ans[3] = {};
-
-bool check(int nowX, int nowY, int size) { // 전부 같은 색깔인지 검사
-    int mark = Table[nowY][nowX];
-    M_Loop(j,nowY,nowY+size) {
-        M_Loop(i,nowX,nowX+size) {
-            if (mark != Table[j][i]) 
-                return false;
-        }
-    }
-    return true;
+using int64 = long long;
+int64 A, B, C;
+int MOD = 0;
+vector<int> Table;
+void GetAns()
+{
 }
+int main()
+{
+	FASTIO
+	cin >> A >> B >> C;
+	
+	if (A % C == 0)
+	{
+		cout << 0;
+		return 0;
+	}
+	Table.resize(B + 1, 1);
+	M_Loop(i, 0, B)
+		Table[i] = A;
 
-void DivC(int nowX, int nowY, int len) {
-   
-    if (check(nowX, nowY, len)) 
-    {
-        Ans[Table[nowY][nowX]]++;
-        return;
-    }
+	GetAns();
 
-   int div_len = len / 3; 
-   if (div_len)
-   {
-       M_Loop(j, 0, 3)
-       {
-           M_Loop(i, 0, 3)
-               DivC(nowX + i*div_len, nowY + j*div_len, div_len);
-       }
-   }
-}
-
-int main(void) {
-    FASTIO
-    cin >> N;
-    M_Loop(i,0,N)
-    {
-        M_Loop(j, 0, N)
-        {
-            cin >> Table[i][j]; 
-            Table[i][j]++; // 0, 1, 2 로 변환
-        }      
-    }
-    DivC(0, 0, N);
-
-    for (auto c : Ans) {
-        cout << c << '\n';
-    }
+	cout << MOD;
 }
 #endif 
 
