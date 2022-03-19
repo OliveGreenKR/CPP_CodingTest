64dcfdba2f2f34fde836a1ae2cedf8af2fd381a2
jeong98CH <jchul1998@naver.com>
2022-03-19 21:28:15 +0900

    Matrix Pow calculator solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -3,59 +3,79 @@
 #pragma warning(disable: 4996)
 
 
+
 #ifdef BACK
 #include <iostream>
+#include <vector>
 using namespace std;
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
 #define M_Loop_sub(i,st,n) for(int i=(st);i>(n);i--)
-enum
+using int64 = long long;
+using Matrix = vector<vector<int64>>;
+enum : int64
 {
-	MAX_IN = 100
+	MAX_IN = 100'000'000'000,
+	MAX_N = 5,
+	MOD = 1000
 };
-int A[MAX_IN][MAX_IN], B[MAX_IN][MAX_IN], C[MAX_IN][MAX_IN] = {};
-int N, M, K;
+int64 N, B;
+Matrix half;
+Matrix nullmat(MAX_N, vector<int64>(MAX_N, 0));
+Matrix unitmat(MAX_N, vector<int64>(MAX_N, 0));
+Matrix A(MAX_N, vector<int64>(MAX_N, 0));
 
-void MMul()
-{
+Matrix Mul(Matrix a, Matrix b)
+{ 
+	Matrix ret = nullmat;
 	M_Loop(k, 0, N)
 	{
-		M_Loop(j, 0, K)
+		M_Loop(j, 0, N)
 		{
-			M_Loop(i, 0, M)
+			M_Loop(i, 0, N)
 			{
-				C[k][j] += A[k][i] * B[i][j];
+				ret[k][j] += (a[k][i] * b[i][j])%MOD;
+				ret[k][j] %= MOD;
 			}
 		}
 	}
+	return ret;
 }
+
+Matrix Pow(int64 b)
+{
+	if (b == 0)
+		return unitmat;
+	if (b % 2)
+		return Mul(A, Pow(b - 1));
+	half = Pow(b / 2);
+	return Mul(half, half);
+}
+
 int main()
 {
 	FASTIO;
-	cin >> N >> M;
-	M_Loop(j, 0, N) 
-	{
-		M_Loop(i, 0, M)
-			cin >> A[j][i];
-	}
-		
-	cin >> M >> K;
-	M_Loop(j, 0, M)
-	{
-		M_Loop(i, 0, K)
-			cin >> B[j][i];
-	}
 
-	MMul();
 
+	cin >> N >> B;
 	M_Loop(j, 0, N)
 	{
-		M_Loop(i, 0, K)
-		{
-			cout << C[j][i] << " ";
-		}
-		cout << "\n";	
+		M_Loop(i, 0, N)
+			cin >> A[j][i];
 	}
+	M_Loop(i, 0, N)
+	{
+		unitmat[i][i] = 1;
+	}
+	A = Pow(B);
+	
+	M_Loop(j, 0, N)
+	{
+		M_Loop(i, 0, N)
+			cout << A[j][i] << " ";
+		cout << "\n";
+	}
+
 	return 0;
 }
 
