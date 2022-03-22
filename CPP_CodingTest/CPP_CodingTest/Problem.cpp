74bddab19543e2b70773e64485ce00e8e0ba1acb
jeong98CH <jchul1998@naver.com>
2022-03-22 22:48:44 +0900

    card counter solving with binary search.

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -4,41 +4,66 @@
 
 #ifdef BACK
 #include <iostream>
-#include <map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,n) for(int i=(st);i<(n);i++)
 #define M_Loop_sub(i,st,n) for(int i=(st);i>(n);i--)
-map<int, int> Card;
+vector<int> Arr;
+
+int get_lower(vector<int>& arr, int m) 
+{
+    int mid, start, end;
+    start = 0, end = arr.size()-1;
+
+    while (end > start) {
+        mid = (start + end) / 2;
+        if (arr[mid] >= m)//
+            end = mid;
+        else start = mid + 1;
+    }
+    return end;
+}
+
+int get_upper(vector<int>& arr, int m) 
+{
+    int mid, start, end;
+    start = 0, end = arr.size() - 1;
+
+    while (end > start) {
+        mid = (start + end) / 2;
+        if (arr[mid] > m)//
+            end = mid;
+        else start = mid + 1; ////
+    }
+    return end;
+}
 int main()
 {
 	FASTIO;
+    int N;
+    cin >> N;
+    Arr.resize(N, 0);
+    M_Loop(i, 0, N)
+        cin >> Arr[i];
 
-	int N;
-	cin >> N;
+    ::sort(Arr.begin(), Arr.end());
 
-	M_Loop(i, 0, N)
-	{
-		int n;
-		cin >> n;
-		if (Card.find(n) != Card.end())
-			Card.at(n)++;
-		else
-			Card.emplace(n, 1);
-	}
+    int M;
+    cin >> M;
+    M_Loop(i, 0, M)
+    {
+        int m;
+        cin >> m;
 
-	int M;
-	cin >> M;
-
-	M_Loop(i, 0, M)
-	{
-		int m;
-		cin >> m;
-		if (Card.find(m) != Card.end())
-			cout << Card.at(m) << " ";
-		else
-			cout << 0 << " ";
-	}
+        //못찾으면 둘은 같은 값을 반환한다.
+        int lower = get_lower(Arr, m);
+        int upper = get_upper(Arr, m);
+        if (upper == Arr.size() - 1 && Arr[upper] == m)
+            upper++;
+        cout << upper - lower << " ";
+    }
 
 	return 0;
 }
