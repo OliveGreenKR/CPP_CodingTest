8491ba2d12b88877820f7132832ce9e039a4a63e
jeong98CH <jchul1998@naver.com>
2022-03-24 17:16:33 +0900

    max slice solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -8,9 +8,15 @@
 #include <algorithm>
 using namespace std;
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
-#define M_Loop(i,st,K) for(int i=(st);i<(K);i++)
-#define M_Loop_sub(i,st,K) for(int i=(st);i>(K);i--)
+#define M_Loop(i,st,M) for(int i=(st);i<(M);i++)
+#define M_Loop_sub(i,st,M) for(int i=(st);i>(M);i--)
 using int64 = long long;
+
+enum
+{
+    MAX_IN = 1'000'000,
+    MAX_M = 2'000'000'000
+};
 vector<int> Arr;
 int64 GetCnt(vector<int>& arr, int slice)  //O-n
 {
@@ -18,27 +24,27 @@ int64 GetCnt(vector<int>& arr, int slice)  //O-n
     int size = arr.size();
     M_Loop(i, 0, size)
     {
-        sum += arr[i] / slice;
+        int64 res = (arr[i] - slice);
+        res > 0 ? sum += res : 0;
     }
-
     return sum;
 }
-int GetMaxSlice(vector<int>& arr, int N)
+int GetMaxSlice(vector<int>& arr, int M)
 {
     int64 mid, left, right;
 
-    left = 1; right = arr[arr.size()-1];
+    left = 0; right = arr[arr.size()-1];
     mid = (right + left) / 2;
 
     while (left < right)
     {
         mid = (right + left) / 2;
-        if (GetCnt(arr, mid) >= N) //최대
+        if (GetCnt(arr, mid) >= M) //자르는 높이를 높여도된다.
             left = mid + 1;
         else
             right = mid;
     }
-    if (left == arr[arr.size() - 1]&& GetCnt(arr, left) >= N)
+    if (left == arr[arr.size() - 1]&& GetCnt(arr, left) >= M)
         left++;
     return left-1;
 }
@@ -46,14 +52,14 @@ int GetMaxSlice(vector<int>& arr, int N)
 int main()
 {
 	FASTIO;
-    int K,N;
-    cin >> K >> N;
-    Arr.resize(K, 0);
-    M_Loop(i, 0, K)
+    int M, N;
+    cin >> N >> M;
+    Arr.resize(N, 0);
+    M_Loop(i, 0, N)
         cin >> Arr[i];
     ::sort(Arr.begin(), Arr.end());
 
-    cout << GetMaxSlice(Arr, N);
+    cout << GetMaxSlice(Arr, M);
 	return 0;
 }
 #endif 
