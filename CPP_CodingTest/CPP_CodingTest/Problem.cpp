319b49e78b782f61857164415a5ddbb10f84f90e
jeong98CH <jchul1998@naver.com>
2022-03-29 21:13:02 +0900

    mid val return solved with priority queueu

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,57 +6,59 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
-#include <vector>
 using namespace std;
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ)
 #define M_Loop(i,st,M) for(int i=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int i=(st);i>(M);i--)
 
-template <class _Ty = void>
-struct Func
-{
-
-	constexpr bool operator()(const _Ty& _Left, const _Ty& _Right) const
-	{
-		_Ty left, right;
-		_Left < 0 ? left = -_Left : left = _Left;
-		_Right < 0 ? right = -_Right : right = _Right;
-		if (left < right)
-			return false;
-		else if (left > right)
-			return true;
-		else
-			return _Left > _Right;
-	}
-};
-
-priority_queue< int, vector<int>, Func<int>> Gq;
+priority_queue< int, vector<int>, greater<int>> Bigger; //min Heap
+priority_queue< int, vector<int>, less<int>> Smaller; //max Heap
+int middle;
 
 int main()
 {
 	FASTIO;
     int N;
     cin >> N;
-    M_Loop(i, 0, N)
+    M_Loop(i, 1, N+1)
     {
         int x;
         cin >> x;
-        switch (x)
+        if (i == 1)
         {
-        case 0:
-            if (Gq.empty())
-            {
-                cout << 0 << "\n";
-                break;
-            }
-            cout << Gq.top() << "\n";
-            Gq.pop();
-            break;
-        default:
-            Gq.push(x);
+            middle = x;
+			goto END;
         }
-    }
+        
+        if (x > middle)
+            Bigger.push(x);
+        else if (x <= middle)
+            Smaller.push(x);
 
+
+        if (i % 2)//odd
+		{
+			if (Bigger.size() > Smaller.size())
+			{
+				Smaller.push(middle);
+				middle = Bigger.top();
+				Bigger.pop();
+			}
+			else if (Bigger.size() < Smaller.size())
+			{
+				Bigger.push(middle);
+				middle = Smaller.top();
+				Smaller.pop();
+			}
+		}
+		else if (Smaller.size() > Bigger.size()) //even
+		{
+			Bigger.push(middle);
+			middle = Smaller.top();
+			Smaller.pop();
+		}
+	END: cout << middle << "\n";
+	}
 	return 0;
 }
 #endif 
