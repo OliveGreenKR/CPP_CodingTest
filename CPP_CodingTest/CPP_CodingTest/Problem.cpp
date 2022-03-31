21beb72980986f0cc01f75c10039bbb860f42fe1
jeong98CH <jchul1998@naver.com>
2022-03-31 21:17:10 +0900

    mat mul min cnt solved with dp

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -15,18 +15,30 @@ enum
 {
     MAX_IN = 500+1,
 };
-
-int64 dp[MAX_IN]; //[val]
-pair<int, int> table[MAX_IN]; //[r][c]
-
-inline int64 Mul(int r, int c, int k)
+int64 dp[MAX_IN][MAX_IN] = {}; //dp[i][j] = i~j ÃÖ¼Ò°ö
+struct Mat
 {
-    return r*c*k;
-}
-inline int64 Mulidx(int idx1, int idx2)
+    int64 _r, _c;
+};
+Mat table[MAX_IN]; //[r][c]
+
+inline int64 GetAns(int l, int r)
 {
-    return table[idx1].first * table[idx1].second * table[idx2].second;
+    if (l == r - 1) return table[l]._r * table[l]._c * table[r]._c;
+    if (l == r) return 0;
+
+    int64& cost = dp[l][r];
+    if (cost) return dp[l][r];
+
+    cost = INT64_MAX;
+
+    M_Loop(i, l, r) //[l~i] * [i+1~r] 
+    {
+        cost = ::min(GetAns(l, i) + GetAns(i + 1, r) + table[l]._r * table[i]._c * table[r]._c, cost);
+    }
+    return cost;
 }
+
 int main()
 {
 	FASTIO;
@@ -38,21 +50,10 @@ int main()
         cin >> r >> c;
         table[i] = { r,c };
 	}
-    const int R = table[1].first;
-    if (N > 1)
-    {
-        dp[2] = table[1].first * table[1].second * table[2].second;
-    }
-
-    M_Loop(i, 3, N + 1)
-    {
-        int64 cost1 = dp[i-2] + Mulidx(i-1,i) + Mul(R,table[i-2].second,table[i].second);  // A*BC
-        int64 cost2 = dp[i-1] + Mul(R, table[i - 1].second, table[i].second);  // AB*C
-        dp[i] = ::min(cost1, cost2);
-    }
-    cout << dp[N];
-
+    GetAns(1, N);
+    cout << dp[1][N] << "\n";
 	return 0;
 }
 #endif 
+
 
