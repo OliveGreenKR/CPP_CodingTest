d6bdf24652e4e764d509e85f425980e18c9e2765
jeong98CH <jchul1998@naver.com>
2022-04-04 17:19:58 +0900

    dp solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -5,97 +5,56 @@
 #ifdef BACK
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 using namespace std;
-#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
 enum
 {
-    MAX_IN = 500+2,
+    MAX_IN = 2'000+1,
 };
-int map[MAX_IN][MAX_IN];
-int dp[MAX_IN][MAX_IN]; 
-int N, M;
+int arr[MAX_IN];
+int dp[MAX_IN][MAX_IN];  //[i]~[j]까지 팰런드럼 여부
+int ans[MAX_IN];
 
-int dir[][2] = {
-    {-1,0}, //up
-    {0,1},  //right
-    {1,0},  //down
-    {0,-1}  //left
-};
-
-inline int dfs(int y, int x) //(y,x)에서 목적지까지가는 경우의수
+int GetAns(int l, int r)
 {
-    if (y == M && x == N)
-        return 1;
-    if (y > M || x > N || y < 1 || x < 1)
-        return 0;
-    int& now = dp[y][x];
-    if (now != -1)
-        return now;
-    now = 0;
-    int h = map[y][x];
-    M_Loop(i, 0, 4)
-    {
-        int* dr = dir[i];
-        int nexty = y + dr[0];
-        int nextx = x + dr[1];
-        int nexth = map[nexty][nextx];
-        if ( nexth && (h > nexth))
-        {
-            now += dfs(nexty, nextx);
-        }
-    }
-    return now;
+	int& now = dp[l][r];
+	if (l >= r)
+		now = true;
+	if (now != -1)
+		return now;
+
+	if (arr[r] != arr[l])
+		now = false;
+	else
+		now = GetAns(l + 1,r - 1);
+
+	return now;
 }
 int main()
 {
+	::memset(dp, -1, MAX_IN * MAX_IN * sizeof(int));
+
+    int N;
 	FASTIO;
-    cin >> M >> N;
-    ::memset(dp, -1, MAX_IN * MAX_IN*sizeof(int));
+    cin >> N;
 
-    M_Loop(i, 1, M+1)
-    {
-        M_Loop(j, 1, N+1)
-        {
-            cin >> map[i][j];
-        }
-    }
-    cout << dfs(1, 1);
-
+	M_Loop(i, 1, N + 1)
+		cin >> arr[i];
+	int M;
+	cin >> M;
+	M_Loop(i, 0, M)
+	{
+		int S, E;
+		cin >> S >> E;
+		cout << GetAns(S, E) << "\n";
+	}
 	return 0;
 }
 #endif 
 
-/*
-#include <iostream>
-#include <algorithm>
-using namespace std;
-#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
-#define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
-enum
-{
-    MAX_IN = 9
-};
-int main()
-{
-    FASTIO;
 
-    int arr[9];
-    int MAX = INT32_MIN;
-    int idx = 0;
-    M_Loop(i, 1, MAX_IN+1)
-    {
-        int tmp;
-        cin >> tmp;
-        if (MAX < tmp)
-        {
-            MAX = tmp;
-            idx = i;
-        }
-    }
-    cout << MAX << "\n" << idx;
-}*/
 
 
