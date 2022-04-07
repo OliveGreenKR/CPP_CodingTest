4ac54e443baaba43aa052d85beec4d4a47561db3
jeong98CH <jchul1998@naver.com>
2022-04-07 18:24:16 +0900

    array base solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -3,36 +3,54 @@
 
 #ifdef BACK
 #include <iostream>
+#include <string>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
 using int64 = long long;
-enum
-{
-    MAX_IN = 100+1,
-	MAX_K = 10'000+1
-};
-int N,K;
-int arr[MAX_IN]; // 돈의가치
-int64 dp[MAX_K];  // j가치의 경우의수
+
+int cnt[10] = {};
 
 int main()
 {
 	FASTIO;
-    cin >> N >> K;
-	M_Loop(i, 1, N+1)
-		cin >> arr[i];
+	int A, B, C;
+	cin >> A >> B >> C;
+	int64 ret = A * B * C;
+	string ans = ::to_string(ret);
 
-	dp[0] = 1;
-	M_Loop(i, 1, N + 1) 
-		M_Loop(j, arr[i], K + 1)
-			dp[j] += dp[j - arr[i]];
-	cout << dp[K] << "\n";
-	return 0;
+	for (char c : ans)
+		cnt[c - '0']++;
+	for (int x : cnt)
+		cout << x << "\n";
 }
 #endif 
 
 
 
 
+//using int64 = long long;
+//enum
+//{
+//    MAX_IN = 100+1,
+//	MAX_K = 10'000+1
+//};
+//int N,K;
+//int arr[MAX_IN]; // 돈의가치
+//int64 dp[MAX_K];  // j가치의 경우의수
+//
+//int main()
+//{
+//	FASTIO;
+//    cin >> N >> K;
+//	M_Loop(i, 1, N+1)
+//		cin >> arr[i];
+//
+//	dp[0] = 1;
+//	M_Loop(i, 1, N + 1) 
+//		M_Loop(j, arr[i], K + 1)
+//			dp[j] += dp[j - arr[i]];
+//	cout << dp[K] << "\n";
+//	return 0;
+//}
