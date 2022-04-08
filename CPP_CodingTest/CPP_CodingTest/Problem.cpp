5065be41d5977574fd6a6d18aa5a6d8d58912d29
jeong98CH <jchul1998@naver.com>
2022-04-08 22:42:24 +0900

    app napsack solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -3,54 +3,50 @@
 
 #ifdef BACK
 #include <iostream>
-#include <string>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
-using int64 = long long;
-
-int cnt[10] = {};
+enum
+{
+	MAX_IN = 100 + 1,
+	MAX_C = 10'000+ 1
+};
+int N, M;
+int C[MAX_IN]; // 재실행비용
+int W[MAX_IN]; // 메모리
+int dp[MAX_IN][MAX_C];  // i선택시 j의 비용으로 얻을 수 있는 최대 메모리
 
 int main()
 {
 	FASTIO;
-	int A, B, C;
-	cin >> A >> B >> C;
-	int64 ret = A * B * C;
-	string ans = ::to_string(ret);
+	cin >> N >> M;
 
-	for (char c : ans)
-		cnt[c - '0']++;
-	for (int x : cnt)
-		cout << x << "\n";
+	M_Loop(i, 1, N + 1) cin >> W[i];
+	M_Loop(i, 1, N + 1) cin >> C[i];
+
+	M_Loop(i, 1, N + 1)
+	{
+		M_Loop(j, 0, MAX_C)
+		{
+			if (j - C[i] >= 0) //추가선택가능
+				dp[i][j] = ::max(dp[i][j], dp[i - 1][j - C[i]] + W[i]);
+
+			dp[i][j] = ::max(dp[i][j], dp[i - 1][j]);
+		}
+	}
+
+	M_Loop(j, 0, MAX_C)
+	{
+		if (dp[N][j] >= M)
+		{
+			cout << j << "\n";
+			break;
+		}
+	}
+
+	return 0;
 }
 #endif 
 
 
-
-
-//using int64 = long long;
-//enum
-//{
-//    MAX_IN = 100+1,
-//	MAX_K = 10'000+1
-//};
-//int N,K;
-//int arr[MAX_IN]; // 돈의가치
-//int64 dp[MAX_K];  // j가치의 경우의수
-//
-//int main()
-//{
-//	FASTIO;
-//    cin >> N >> K;
-//	M_Loop(i, 1, N+1)
-//		cin >> arr[i];
-//
-//	dp[0] = 1;
-//	M_Loop(i, 1, N + 1) 
-//		M_Loop(j, arr[i], K + 1)
-//			dp[j] += dp[j - arr[i]];
-//	cout << dp[K] << "\n";
-//	return 0;
-//}
