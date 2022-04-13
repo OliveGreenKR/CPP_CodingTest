49cd9ca24d84d1b5401fb0edb5f88fd7033346bd
jeong98CH <jchul1998@naver.com>
2022-04-13 21:39:19 +0900

    shortest distacne with bfs

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -13,11 +13,12 @@ using namespace std;
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
 enum
 {
-	MAX_IN = 50 + 2,
+	MAX_IN = 100 + 2,
 };
 bool visited[MAX_IN][MAX_IN] = {};
 bool map[MAX_IN][MAX_IN];
-int M,N, CNT = 0;
+int Gdistance[MAX_IN][MAX_IN] = {};
+int M, N;
 
 using Pos = pair<int, int>;
 Pos dir[4] = {
@@ -45,61 +46,50 @@ void BFS(int y, int x)
 	visited[y][x] = true;
 
 	queue<Pos> q;
-
+	Pos parent[MAX_IN][MAX_IN] = {};
 	Pos now = { y,x };
 	q.push(now);
-	int cnt = 1; //집 개수
+	parent[y][x] = now;
+	Gdistance[y][x] = 1;
 
 	while (!q.empty())
 	{
 		now = q.front();
+		if (now == Pos{ N,M }) return;
 		q.pop();
-
+	
 		M_Loop(i, 0, 4)
 		{
 			Pos next = now + dir[i];
 			if (IsRight(next.first, next.second))
 			{
 				q.push(next);
-				cnt++;
 				visited[next.first][next.second] = true;
+				parent[next.first][next.second] = now;
+				Gdistance[next.first][next.second] = Gdistance[now.first][now.second] + 1;
 			}
 		}
 	}
-	CNT++; //단지개수++
 	return;
-
 }
 int main()
 {
 	FASTIO;
-	int T;
-	cin >> T;
-	M_Loop(i, 0, T)
+	::memset(map, false, sizeof(map));
+	::memset(visited, false, sizeof(visited));
+	cin >> N >> M;
+
+	M_Loop(i, 1, N + 1)
 	{
-		CNT = 0;
-		int K;
-		::memset(map, false, sizeof(map));
-		::memset(visited, false, sizeof(visited));
-		cin >> M >> N >> K;
-		M_Loop(i, 0, K)
+		M_Loop(j, 1, M + 1)
 		{
-			int y, x;
-			cin >> x >> y;
-			map[y + 1][x + 1] = true;
+			char c;cin >> c;
+			map[i][j] = c - '0';
 		}
-		
-		M_Loop(i, 1, N+1)
-		{
-			M_Loop(j, 1, M+1)
-			{
-				if (IsRight(i, j))
-					BFS(i, j);
-			}
-		}
-		cout << CNT << "\n";
 	}
-	
+	BFS(1, 1);
+	cout << Gdistance[N][M] << "\n";
+
 	return 0;
 }
 #endif 
