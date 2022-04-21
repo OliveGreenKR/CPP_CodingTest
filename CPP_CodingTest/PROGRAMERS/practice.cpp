c64f7bc0a0aa0b8903d3c07c0b4872c7f7cd02be
jeong98CH <jchul1998@naver.com>
2022-04-21 16:23:02 +0900

    practice solved

--- CPP_CodingTest/PROGRAMERS/practice.cpp
+++ CPP_CodingTest/PROGRAMERS/practice.cpp
@@ -1,38 +1,30 @@
+#pragma warning(disable: 4996)
 #include <iostream>
-#include <string>
+#include <algorithm>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
 
-//o=79 x = 88
-enum
-{
-	MAX_IN = 80,
-};
-int score[MAX_IN];
-
 int main()
 {
-	FASTIO;
 	int T;
-	cin >> T;
+	scanf("%d", &T);
 
 	M_Loop(i, 0, T)
 	{
-		string str;
-		cin >> str;
-		int sum = 0;
-		int partsum = 0;
-		for (char c : str)
+		int N;
+		scanf("%d", &N);
+		double sum=0;
+		int arr[1000];
+		M_Loop(i, 0, N)
 		{
-			if (c == 'O')
-				partsum++;
-			else
-				partsum = 0;
-			sum += partsum;
+			scanf("%d", &arr[i]);
+			sum += arr[i];
 		}
-		cout << sum << "\n";
+		sum = sum / N; //now, sum is avg;
+		::sort(arr, arr + N);
+		printf("%0.3lf%c\n", ((arr + N) - ::find_if(arr, arr + N, [sum](double A) {return A > sum; })) / (double)N * 100,'%');
 	}
 	return 0;
 }
