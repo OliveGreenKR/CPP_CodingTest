566cac5b3073c8d3d29f45ceb8dcdcb74cfd3211
jeong98CH <jchul1998@naver.com>
2022-04-22 21:42:53 +0900

    knight moving solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -4,15 +4,114 @@
 #ifdef BACK
 #include <iostream>
 #include <queue>
+#include <algorithm>
+#include <string.h>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
 struct Pos
 {
-	int y; int x; int state = 0; int dir = 4; int dist = 0;
+	int y; int x; int dist = 0;
 };
 
+enum
+{
+	MAX_IN = 400,
+	DIR = 8
+};
+int I;
+bool map[MAX_IN][MAX_IN];
+
+Pos dir[DIR] =
+{
+	{-2,1,1},
+	{-1,2,1},
+	{2,1,1},
+	{1,2,1},
+	{-2,-1,1},
+	{-1,-2,1},
+	{2,-1,1},
+	{1,-2,1},
+};
+Pos operator+(const Pos& A, const Pos& B)
+{
+	return { A.y + B.y , A.x + B.x, A.dist + B.dist};
+}
+bool operator==(const Pos& A, const Pos& B)
+{
+	return A.y == B.y && A.x == B.x;
+}
+inline bool IsRight(Pos x)
+{
+	if (x.x<0||x.y<0||x.x>=I||x.y>=I||map[x.y][x.x])
+		return false;
+	return true;
+}
+
+int BFS(Pos now, Pos dest)
+{
+	if (now == dest)
+		return 0;
+
+	int result = -1;
+	queue<Pos>q;
+	q.push(now);
+	map[now.y][now.x] = true;
+
+	while (!q.empty())
+	{
+		now = q.front();
+		q.pop();
+		M_Loop(i, 0, DIR)
+		{
+			Pos next = now + dir[i];
+			if (IsRight(next))
+			{
+				q.push(next);
+				map[next.y][next.x] = true;
+				if (next == dest)
+					result = result > 0 ? ::min(result, next.dist) : next.dist;
+			}
+		}
+		
+	}
+	return result;
+}
+
+int main()
+{
+	FASTIO;
+	int T;
+	cin >> T;
+	M_Loop(i, 0, T)
+	{
+		::memset(map, 0, sizeof(map));
+		int x1, y1, x2, y2; //2:dest
+		cin >> I >> y1 >> x1 >> y2 >> x2;
+		cout << BFS(Pos{ y1,x1 }, Pos{ y2,x2 }) << "\n";
+	}
+
+	return 0;
+}
+
+#endif 
+
+
+
+
+
+
+
+
+
+//갔던 길 다시 갈수 잇어야함. --> 그럼 중단은 뭘로?
+// 
+//그와중에 한번갔던 dest는 다시 안가도록?
+
+//w경로선택에 전에 갔던 방향을 가나? 말도안됨 디버깅
+
+/*
 enum
 {
 	MAX_IN = 50+2,  //50+@,
@@ -20,7 +119,7 @@ enum
 	MAX_DIR = 4+1,
 };
 int map[MAX_IN][MAX_IN] = {};
-int visited[MAX_K][MAX_IN][MAX_IN] = {}; 
+int visited[MAX_K][MAX_IN][MAX_IN] = {};
 //int Gdistance[MAX_K][MAX_IN][MAX_IN] = {};
 Pos dests[3];
 int N, M , Max_dist = INT32_MAX;
@@ -45,7 +144,7 @@ inline int IsRight(Pos x)
 
 inline int IsDest(Pos x)
 {
-	
+
 	M_Loop(i, 1, 3)
 	{
 		Pos& dest = dests[i];
@@ -132,11 +231,4 @@ int main()
 	cout << Max_dist << "\n";
 	return 0;
 }
-
-#endif 
-
-//갔던 길 다시 갈수 잇어야함. --> 그럼 중단은 뭘로?
-// 
-//그와중에 한번갔던 dest는 다시 안가도록?
-
-//w경로선택에 전에 갔던 방향을 가나? 말도안됨 디버깅
+*/
