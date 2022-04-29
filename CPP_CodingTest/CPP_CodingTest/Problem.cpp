b3df15cd50088a767b02a6efad3eaa984aa817e2
jeong98CH <jchul1998@naver.com>
2022-04-29 21:48:14 +0900

    shortest path with layovers

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,68 +6,73 @@
 #include <queue>
 #include <algorithm>
 #include <vector>
-#include <map>
 #include <string.h>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
-using  Node = pair<int, int>;
-enum
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
+#define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
+using int64 = long long;
+enum : int64
 {
-	MAX_IN = 20'000 + 1,
-	MAX_E = 30'000 + 1,
+	MAX_IN = 800 + 1,
+	MAX_E = 20'000 + 1,
 	INF = INT32_MAX
 };
+int64 map[MAX_IN][MAX_IN]; //i~j까지 최단거리
+int N, E, V1, V2;
 
-int V, E, K;
-vector<Node> Edge[MAX_E];
-int Gdist[MAX_IN]; //K부터 i까지 최단거리
-
-void Dijikstra(int st)
-{
-	priority_queue<Node, vector<Node>, greater<Node>> pq;
-	pq.push({ 0,st });
-	Gdist[st] = 0;
-	while (!pq.empty())
-	{
-		Node now = pq.top();
-		pq.pop();
-		for (auto next : Edge[now.second])
-		{
-			if (next.first > Gdist[next.second]) continue;
-
-			int nextdist = Gdist[now.second] + next.first;
-			if (nextdist < Gdist[next.second])
-			{
-				Gdist[next.second] = nextdist;
-				pq.push({nextdist,next.second});
-			}
-			
-		}
-	}
-}
 int main()
 {
 	FASTIO;
-	cin >> V >> E >> K;
-	M_Loop(i, 1, V + 1) Gdist[i] = INF;
+	cin >> N >> E;
+
+	M_Loop(i, 1, N + 1)
+	{
+		M_Loop(j, 1, N + 1)
+		{
+			if (i != j) map[i][j] = INF;
+		}
+	}
 	M_Loop(i, 0, E)
 	{
-		int u, v, w;
-		cin >> u >> v >> w;
-		Edge[u].push_back({w,v});
+		int u, v, w; cin >> u >> v;
+		cin >> map[u][v]; map[v][u] = map[u][v];
 	}
 
-	Dijikstra(K);
-	M_Loop(i, 1, V + 1)
+	M_Loop(k, 1, N + 1)  //중간 경유점에 대한 모든 노드들의 경우의수를 구한다.
 	{
-		int ret = Gdist[i];
-		if (ret != INF)
-			cout << ret << "\n";
-		else
-			cout << "INF\n";
+		M_Loop(i, 1, N + 1)
+		{
+			M_Loop(j, 1, N + 1)
+			{
+				int64& now = map[i][j];
+				int64 newone = map[i][k] + map[k][j];
+				now = now > newone ? newone : now;
+			}
+		}
 	}
+
+	cin >> V1 >> V2;
+	if (map[1][V1] >= INF || map[V1][V2] >= INF || map[V2][N] >= INF)
+	{
+		int64 ans1 = map[1][V1] + map[V1][V2] + map[V2][N];
+		int64 ans2 = map[1][V2] + map[V2][V1] + map[V1][N];
+		ans1 = ans1 > ans2 ? ans2 : ans1;
+		cout << ans1 << "\n";
+	}
+	else
+		cout << -1 << "\n";
+
 	return 0;
 }
 
--- CPP_CodingTest/PROGRAMERS/practice.cpp
+++ CPP_CodingTest/PROGRAMERS/practice.cpp
@@ -6,35 +6,14 @@ using namespace std;
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
 
-
-inline void swap(char* x, char* y)
-{
-	char temp = *x;
-	*x = *y;
-	*y = temp;
-
-}
-
-void permute(char* a, int l, int r)
-{
-	if (l == r)
-		printf("%s\n", a);
-	else
-	{
-		for (int i = l; i <= r; i++)
-		{
-			swap(a + l, a + i);
-			permute(a, l + 1, r);
-			swap(a + l, a + i);
-		}
-	}
-}
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	
-	char str[] = "ABC";
-	permute(str, 0, 2);
-	return 0;
+	FASTIO;
+	int T; cin >> T;
+	M_Loop(i, 0, T)
+	{
+		int A, B;
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
 }
