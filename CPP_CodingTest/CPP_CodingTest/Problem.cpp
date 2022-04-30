2262c7acf578f847ddcf22383c44fc5b4a305182
jeong98CH <jchul1998@naver.com>
2022-04-30 18:13:01 +0900

    priority pathced

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -11,16 +11,6 @@ using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
-
-#include <iostream>
-#include <queue>
-#include <algorithm>
-#include <vector>
-#include <string.h>
-using namespace std;
-#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
-#define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
 int N, K;
 enum { MAX_IN = 100'000+1,};
 int Dist[MAX_IN];
@@ -32,7 +22,7 @@ inline bool IsRight(int x)
 }
 void BFS(int now)
 {
-	int dir[2] = { -1,+1 };
+	int dir[3] = {0, -1,+1 };
 	queue<int> q;
 	Dist[now] = 0;
 	q.push(now);
@@ -42,12 +32,12 @@ void BFS(int now)
 		M_Loop(i, 0, 3)
 		{
 			int next=now;
-			if (i < 2) next += dir[i];
+			if (i > 0) next += dir[i];
 			else next *= 2;
 
 			if (IsRight(next))
 			{
-				if (i < 2)
+				if (i > 0)
 					Dist[next] = Dist[now] + 1;
 				else
 					Dist[next] = Dist[now];
