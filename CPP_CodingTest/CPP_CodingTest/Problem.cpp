b7f37bef2ebc230e59b7bd0b2cba1e120d0d3d6c
jeong98CH <jchul1998@naver.com>
2022-05-03 00:07:05 +0900

    solving....

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -11,37 +11,52 @@ using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
-int N, K;
-enum { MAX_IN = 100'000+1,};
+int n,m,t,g,h;
+enum { MAX_IN = 2000+1,};
+int map[MAX_IN][MAX_IN];
 int Dist[MAX_IN];
-inline bool IsRight(int x)
+pair<int,bool> Dest[100];
+inline bool IsRight(int x, int y)
 {
-	if(x>=MAX_IN|| x<0||Dist[x]!=-1)
+	if(x> n|| x<1 || y > n || y < 1 || !map[x][y])
 		return false;
 	return true;
 }
-void BFS(int now)
+struct Node
 {
-	int dir[3] = {0, -1,+1 };
-	queue<int> q;
-	Dist[now] = 0;
+	int dist = 0; int idx; bool ck = false;
+};
+bool operator>(const Node& A, const Node& B) 
+{
+	return A.dist > B.dist;
+}
+void BFS(int st)
+{
+	priority_queue<Node,vector<Node>,greater<Node>> q;
+	Node now = { 0,st,0 };
 	q.push(now);
+	Dist[now.idx] = 0;
 	while (!q.empty())
 	{
-		now = q.front(); q.pop();
-		M_Loop(i, 0, 3)
+		//탐색 중 목적지체크
+		//목적지이면 상태가 g랑 h를 지난 상태면 기록 아니면 폐기.
+		now = q.top();
+		q.pop();
+		M_Loop(i, 1, n + 1)
 		{
-			int next=now;
-			if (i > 0) next += dir[i];
-			else next *= 2;
-
-			if (IsRight(next))
+			if (IsRight(now.idx, i))
 			{
-				if (i > 0)
-					Dist[next] = Dist[now] + 1;
-				else
-					Dist[next] = Dist[now];
-				q.push(next);
+				Node next = { 0,i,now.ck }; int newdist = now.dist + map[now.idx][next.idx];
+				if (Dist[next.idx]==-1 || Dist[next.idx] >= newdist)
+				{
+					if ((now.idx == g && next.idx == h) || (now.idx == h && next.idx == g)) next.ck = true;
+					Dist[next.idx] = newdist;
+					next.dist = newdist;
+					M_Loop(j,0,t)
+						if (next.idx == Dest[j].first && next.ck) Dest[j].second = true; //목적지 체크
+					q.push(next);
+				}
+				
 			}
 		}
 	}
@@ -50,12 +65,44 @@ void BFS(int now)
 int main()
 {
 	FASTIO;
-	cin >> N >> K;
-	::memset(Dist, -1, sizeof(Dist));
-	BFS(N);
+	int T;
+	cin >> T;
+	M_Loop(i, 0, T)
+	{
+		::memset(Dist, -1, sizeof(Dist)); //-1로 초기화
+		::memset(map, 0, sizeof(map)); //-1로 초기화
+		cin >> n >> m >> t;
+		M_Loop(i, 0, t) Dest[i] = { 0,0 };
+		int s;
+		cin >> s >> g >> h;
+		M_Loop(i, 0, m)
+		{
+			int a, b, d;
+			cin >> a >> b >> d;
+			map[a][b] = d;
+			map[b][a] = d;
+		}
+		M_Loop(i, 0, t)
+		{
+			cin >> Dest[i].first;
+		}
 
-	cout << Dist[K] << "\n";
+		BFS(s);
 
+		::sort(Dest,Dest+t);
+		M_Loop(i, 0, t)
+		{
+			auto ret = Dest[i];
+			if (ret.first)
+			{
+				if (ret.second) cout << ret.first << " ";
+			}
+			else
+				break;
+		}
+		cout << "\n";
+	}
+	return 0;
 }
 #endif 
 
