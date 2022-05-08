0b9a320d765a027d678db256271b8e55e67b6038
jeong98CH <jchul1998@naver.com>
2022-05-08 14:35:03 +0900

    two ptr

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -2,66 +2,30 @@
 #pragma warning(disable: 4996)
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 #ifdef BACK
 #include <iostream>
-#include <algorithm>
+#include <bitset>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
-enum 
+
+enum
 {
-	MAX_N = 100000,
+    Max = 1000000,
 };
-int arr[MAX_N]; int X,N;
-
-int FindPair()
-{
-	int cnt = 0;
-	int left = 0, right = N - 1;
-	while (left != right)
-	{
-		int now = arr[left] + arr[right];
-		if (now == X) { cnt++; right--; }
-		else if (now > X) right--;
-		else left++;
-	}
-	return cnt;
-}
+bitset<1000001> bits;
+int n, m, x;
 int main()
 {
-	FASTIO;
-	cin >> N;
-	M_Loop(i, 0, N)
-	{
-		cin >> arr[i];
-	}
-	::sort(arr, arr + N);
-	cin >> X;
-
-	cout << FindPair() << "\n";
-}
-#endif 
-
-#include <iostream>
-#include <bitset>
-using namespace std;
-const int Max = 1000000;
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    bitset<1000001> a;
-    int n, m, x;
+    FASTIO;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         cin >> m;
-        a[m] = 1;
+        bits[m] = 1;
     }
     cin >> x;
 
@@ -69,8 +33,14 @@ int main()
     for (int i = 1; i < x / 2; i++)
     {
         if (i > Max || x - i > Max) continue;
-        else if (a[i] && a[x - i]) ans++;
+        else if (bits[i] && bits[x - i]) ans++;
     }
-    if ((x % 2 == 1) && a[x / 2] && a[x / 2 + 1]) ans++;
+    if ((x % 2 == 1) && bits[x / 2] && bits[x / 2 + 1]) ans++;
     cout << ans;
+
+    return 0;
 }
+
+
+
+#endif 
