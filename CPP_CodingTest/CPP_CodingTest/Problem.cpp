edb82372fd35f1d0f8a770242100b619770e5529
jeong98CH <jchul1998@naver.com>
2022-05-13 15:52:29 +0900

    모든 mod =0 순서쌍 찾기

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,35 +6,32 @@
 
 #ifdef BACK
 #include <iostream>
-#include <algorithm>
-#include <memory.h>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
-int N, K;
-enum
+using int64 = long long;
+int N, M;
+enum 
 {
-    MAX_IN = 100'000+1,
+    MAX_IN = 1'000'000+1,
+    MAX_M = 1'000
 };
-int arr[MAX_IN];
-int main()
-{
+int64 Arr[MAX_IN];
+int mod[MAX_M];
+int main() {
     FASTIO;
-    cin >> N >> K;
-    M_Loop(i, 1, N + 1)
+    cin >> N >> M;
+    M_Loop(i, 1, N + 1) { cin >> Arr[i]; Arr[i] = (Arr[i] + Arr[i - 1]) % M; mod[Arr[i]]++; }
+
+    int cnt = mod[0];
+
+    M_Loop(i, 0, M)
     {
-        int tmp; cin >> tmp;
-        arr[i] = arr[i - 1] + tmp;
-    }
-    int max = INT32_MIN;
-    M_Loop(i, 1, N-K+2)
-    {
-        int sum = arr[i + K-1] - arr[i-1];
-        max = ::max(max, sum);
-    }
-    cout << max;
+        int tmp = mod[i];
+        if (tmp > 1) cnt += tmp * (tmp - 1) / 2;
+    } 
+    cout << cnt << "\n";
     return 0;
 }
-
 #endif 
