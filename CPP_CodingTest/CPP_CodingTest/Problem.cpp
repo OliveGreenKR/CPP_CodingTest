7e846e0df2f2b619a608fbef06a7b8cce7a3cad3
jeong98CH <jchul1998@naver.com>
2022-05-18 18:30:39 +0900

    prime 부분합  solve. ver1

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -1,45 +1,58 @@
 #include "pch.h"
 #pragma warning(disable: 4996)
 
+#define NULL (0)
 #ifdef BACK
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <memory.h>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
 #define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
 enum
 {
-    MAX_IN = 100'000+1,
+    MAX_IN = 4'000'000+1
 };
-int N, S;
+int N;
 int arr[MAX_IN];
 int main() 
 {
     FASTIO;
-    cin >> N >> S;
-    M_Loop(i, 1, N+1)
+    ::memset(arr, -1, sizeof(arr));
+    vector<long long> sumarr = { 0 };
+    cin >> N;
+    for (int i = 2; i < MAX_IN; i++)
     {
-        cin >> arr[i];
-        arr[i] += arr[i-1];
-    }
-
-    if (arr[N] < S) { cout << 0 << "\n"; return 0; }
-
-    int left = 1, right = 1, len = MAX_IN;
-    while (left <= N && right<= N )
-    {
-        int sum = arr[right]-arr[left - 1];
-        if (sum < S) right++;
-        else
+        if (arr[i] == -1)
         {
-            len = ::min(len, right - left + 1);
-            if (left < right)
-                left++;
-            else right++;
+            for (int j = 2; i * j < MAX_IN; j++)
+            {
+                arr[i * j] = 0;//¼Ò¼ö¾Æ´Ô
+            }
+            sumarr.push_back(i+sumarr[sumarr.size()-1]);
         }
     }
-    cout << len << "\n";
+
+    int cnt = 0; int left=1,right = 1;
+
+    while (left <= right && right < sumarr.size())
+    {
+        int ck = sumarr[right] - sumarr[left - 1];
+       
+        if (ck <= N) 
+        {
+            if (ck == N) cnt++;
+            right++; 
+        }
+        else
+        {
+           left++;
+        }
+        
+    }
+    cout << cnt << "\n";
     return 0;
 }
 #endif 
