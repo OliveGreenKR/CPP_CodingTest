0574895b0b7f0f02ae64873dba9e2e64aa5539dc
jeong98CH <jchul1998@naver.com>
2022-05-19 19:06:58 +0900

    napsack with two ptr..

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,48 +6,52 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <memory.h>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define M_Loop(i,st,M) for(int (i)=(st);i<(M);i++)
-#define M_Loop_sub(i,st,M) for(int (i)=(st);i>(M);i--)
+#define M_Loop(i,st,M) for(int (i)=(st);i<(M);(i)++)
+#define M_Loop_sub(i,st,M) for(int (i)=(st);(i)>(M);(i)--)
+using int64 = long long;
 enum
 {
-    MAX_IN = 4'000'000+1
+    MAX_IN = 30+1,
 };
-#include <cstdio>
-bool p[MAX_IN];
-int prime[290000];
-int N;
-int main() {
-    FASTIO;
-    cin >> N;
-    //N까지 소수 아닌거 구하기
-    for (int i = 3; i * i <= N; i += 2)
-        if (!p[i])
-            for (int j = i * i; j <= N; j += i + i)
-                p[j] = true;
-    int pdx=0, cnt=0, left=0, right=0, sum=0;
-    prime[pdx++] = 2;
-    for (int i = 3; i <= N; i += 2) if (!p[i]) prime[pdx++] = i;
-    while (right < pdx) {
-        if (sum < N) {
-            sum += prime[right++];
-            if (sum == N) {
-                cnt++;
-                sum -= prime[left++];
-            }
-        }
-        if (sum > N) {
-            sum -= prime[left++];
-            if (sum == N) {
-                cnt++;
-                sum -= prime[left++];
-            }
-        }
-    }
-    cout << cnt << "\n";
-    return 0;
+int64 N, C, W[MAX_IN], ans;
+vector<int64> group1;
+vector<int64> group2;
+
+void dfs(int left, int right, vector<int64>& save, int64 sum) //부분합 조합구하기
+{
+	if (left > right)
+	{
+		save.push_back(sum);
+		return;
+	}
+	else
+	{
+		dfs(left + 1, right, save, sum);//포함x
+		dfs(left + 1, right, save, sum + W[left]);//포함
+	}
+}
+
+int main()
+{
+	FASTIO;
+	cin >> N >> C;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> W[i];
+	}
+	// 반으로 나눠서 경우의수 저장  
+	dfs(0, N / 2, group1, 0);
+	dfs(N / 2 + 1, N - 1, group2, 0);
+	sort(group2.begin(), group2.end());
+
+	M_Loop(i,0,group1.size())
+	{
+		ans += upper_bound(group2.begin(), group2.end(), C - group1[i]) - group2.begin();
+	}
+	cout << ans << endl;
 }
 #endif 
 
