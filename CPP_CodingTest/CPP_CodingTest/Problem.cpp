0cbe4d793d6d33426506c73b1a318fab73ed304b
jeong98CH <jchul1998@naver.com>
2022-06-22 16:58:03 +0900

    tree solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <vector>
 #include <memory.h>
+#include <queue>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
@@ -17,34 +18,55 @@ enum
 };
 struct Node
 {
-    Node(int k) : key(k)
-    {}
+    Node(int k) : key(k){}
     int key;
     vector<Node*> adj;
 };
 vector<Node*> nodes(MAX_IN, nullptr);
-vector<pair<int, int>> inputs(MAX_IN);
+vector<int> parents(MAX_IN);
+
+void BFS(Node* root, int n)
+{
+    vector<bool> ck(n+1, false);
+    queue<Node*> q;
+    ck[root->key] = true;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node* now = q.front();
+        q.pop();
+
+        for (Node* next : now->adj)
+        {
+            if (!ck[next->key])
+            {
+                ck[next->key] = true;
+                q.push(next);
+                parents[next->key] = now->key;
+            }
+        }
+    }
+}
 int main()
 {
     FASTIO;
     cin >> N;
     for (int i = 1; i <= N; i++) { nodes[i] = new Node(i); }
-    for (int i = 0; i < N; i++)
+    //서로 adj에 넣기
+    for (int i = 1; i < N; i++)
     {
         int u, v;
         cin >> u >> v;
-        if (u > v) ::swap(u, v);  //u<v
-        inputs[i] = { u,v };
+        nodes[u]->adj.push_back(nodes[v]);
+        nodes[v]->adj.push_back(nodes[u]);
     }
-
-    //서로 adj에 넣기
     //1부터 Bfs등의 탐색
-    //탐색하면서 부모기록
+    BFS(nodes[1], N);
     //부모 출력
-
+    for (int i = 2; i <= N; i++)
+        cout << parents[i] << "\n";
     return 0;
 }
-
-
 #endif 
 
--- CPP_CodingTest/PROGRAMERS/11Practice.cpp
+++ CPP_CodingTest/PROGRAMERS/11Practice.cpp
@@ -3,58 +3,8 @@
 #include <string>
 #include <stack>
 using namespace std;
-using int64 = long long;
 
-int64 Stoi(string& str)
-{
-    int64 ans = 0;
-    int64 now = 1;
-    for (int i = str.size()-1; i>=0; i--)
-    {
-        ans += (str[i]-'0')*now;
-        now *= 10;
-    }
-    return ans;
-}
-
-bool IsPrime(int64 n)
-{
-    if (n<=1) return false;
-    if(n%2==0)
-        return n==2? true: false;
-    for (int64 i = 3; i*i<= n; i += 2)
-    {
-        if (n%i==0) return false;
-    }
-    return true;
-}
-
-string GetK(int& n, const int& k) //k<= 10
-{
-    string str;
-   
-    while (n)
-    {
-        str.push_back(n% k + '0');
-        n /= k;
-    }
-    ::reverse(str.begin(), str.end());
-    return str;
-}
-
-int solution(int n, int k) {
-
-    int answer = 0;
-    string str = GetK(n,k);
-    
-    for (auto left = str.begin(); left != str.end();)
-    {
-        auto right = ::find(left, str.end(), '0');
-        string sub = str.substr(left-str.begin(), right-left);
-        if(IsPrime( Stoi(sub)))
-            answer++;
-        left = right == str.end() ? right : right+1;
-    }
-
+vector<int> solution(int n, vector<int> info) {
+    vector<int> answer;
     return answer;
 }
