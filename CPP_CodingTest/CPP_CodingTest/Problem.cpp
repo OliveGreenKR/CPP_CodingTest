ee7b1abaf6abea0b5f03a2921ed8688ff03a9b5f
jeong98CH <jchul1998@naver.com>
2022-06-23 12:19:04 +0900

    get tree width with Dijikstra : time over

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <memory.h>
 #include <queue>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
@@ -15,57 +14,68 @@ int N;
 enum
 {
     MAX_IN = 100'000 + 1,
+    INF = 10'0000
 };
 struct Node
 {
     Node(int k) : key(k){}
     int key;
-    vector<Node*> adj;
+    vector<pair<Node*,int>> adj; //adj,dist
 };
 vector<Node*> nodes(MAX_IN, nullptr);
-vector<int> parents(MAX_IN);
 
-void BFS(Node* root, int n)
+int Dijk(Node* now, int n)
 {
-    vector<bool> ck(n+1, false);
-    queue<Node*> q;
-    ck[root->key] = true;
-    q.push(root);
+    int maxret = 0;
+    priority_queue<pair<int, Node*>> q;
+    vector<int> dist(n + 1, INF);
 
-    while (!q.empty())
+    dist[now->key] = 0;
+    q.push({ 0, now });
+
+    while (!q.empty()) // 가장 최소 선택 후 거리 업데이트.
     {
-        Node* now = q.front();
+        Node* now = q.top().second;
         q.pop();
-
-        for (Node* next : now->adj)
+        for (pair<Node*,int> next : now->adj)
         {
-            if (!ck[next->key])
+            if (dist[next.first->key] > dist[now->key] + next.second)
             {
-                ck[next->key] = true;
-                q.push(next);
-                parents[next->key] = now->key;
+                dist[next.first->key] = dist[now->key] + next.second;
+                 q.push({ dist[next.first->key], next.first });
+                maxret = ::max(maxret, dist[next.first->key]);
             }
         }
     }
+    return maxret;
 }
+
 int main()
 {
     FASTIO;
     cin >> N;
-    for (int i = 1; i <= N; i++) { nodes[i] = new Node(i); }
-    //서로 adj에 넣기
-    for (int i = 1; i < N; i++)
+    for (int i = 1; i <= N; i++) nodes[i] = new Node(i);
+
+    for (int i = 0; i < N; i++)
     {
-        int u, v;
-        cin >> u >> v;
-        nodes[u]->adj.push_back(nodes[v]);
-        nodes[v]->adj.push_back(nodes[u]);
+        int st; cin >> st;
+        while (true)
+        {
+            int v;
+            cin >> v;
+            if (v < 0) break;
+            int d;
+            cin >> d;
+            nodes[st]->adj.push_back({nodes[v], d});
+        }
     }
-    //1부터 Bfs등의 탐색
-    BFS(nodes[1], N);
-    //부모 출력
-    for (int i = 2; i <= N; i++)
-        cout << parents[i] << "\n";
+
+    int MaxD = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        MaxD = ::max(MaxD,Dijk(nodes[i], N));
+    }
+    cout << MaxD;
     return 0;
 }
 #endif 
