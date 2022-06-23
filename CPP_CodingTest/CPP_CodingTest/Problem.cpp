35a6fa62d0595b533e4efacaf78fceeae2911696
UOS_MIE <UOS@DESKTOP-5LPL2RE>
2022-06-23 14:31:29 +0900

    another tree diameter

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int N;
 enum
 {
-    MAX_IN = 100'000 + 1,
+    MAX_IN = 1'0000 + 1,
 };
 struct Node
 {
@@ -22,11 +22,11 @@ struct Node
     vector<pair<Node*,int>> adj; //adj,dist
 };
 vector<Node*> nodes(MAX_IN);
-pair<Node*, int> DFS(Node* st, int n) //최장경로의 노드를 리턴
+pair<Node*, int> DFS(Node* st, int n) 
 {
     stack<Node*> s;
     vector<bool> visited(n + 1, false);
-    vector<int> dist(MAX_IN, 0);
+    vector<int> dist(n+1, 0);
     int maxD = 0, idx = 0;
     s.push(st);
     visited[st->key] = true;
@@ -59,18 +59,12 @@ int main()
     cin >> N;
     for (int i = 1; i <= N; i++) nodes[i] = new Node(i);
 
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i < N; i++)
     {
-        int st; cin >> st;
-        while (true)
-        {
-            int v;
-            cin >> v;
-            if (v < 0) break;
-            int d;
-            cin >> d;
-            nodes[st]->adj.push_back({nodes[v], d});
-        }
+        int u,v,d;
+        cin >> u >> v >> d;
+		nodes[u]->adj.push_back({ nodes[v], d });
+		nodes[v]->adj.push_back({ nodes[u], d });
     }
 
     cout << DFS(DFS(nodes[1], N).first, N).second << "\n";
