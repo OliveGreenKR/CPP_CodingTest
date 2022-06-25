4e959f627fea0cc96ffb8f4ee975dd3d640a3487
jeong98CH <jchul1998@naver.com>
2022-06-25 19:13:21 +0900

    pre to post, in binary Search tree

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,59 +6,38 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-int N;
-enum
+
+vector<int> pre;
+vector<int> post;
+
+void getPost(vector<int>::iterator left, vector<int>::iterator right)
 {
-    MAX_IN = 100'000+1,
-};
-vector<vector<int>> order(2); //in,post,pre
+    if (right <= left) return;
+    int root = *left;
 
-void DivQ(int left, int right, int p) {
-    if (left > right || p<0) return;
-    
-    vector<int>& post = order[1];
-    vector<int>& in = order[0];
-    int root = post[p];
-  
-    cout << root << " "; //root
+    auto it = upper_bound(left+1, right, root);
 
-    auto r = ::find(in.begin() + left, in.begin() + right + 1, root);
-    
-    int llen = r - in.begin() - left;
-    int rlen = in.begin() + right - r;
-    int ridx = r - in.begin();
-
-    DivQ(left,ridx-1, p-rlen-1);
-    DivQ(ridx+1,ridx+rlen,p-1);
+    getPost(left + 1, it);
+    getPost(it, right);
+    post.push_back(root);
+   
 }
+
 int main()
 {
     FASTIO;
-    cin >> N;
-    
-    for (int i = 0; i < 2; i++)
-        order[i].resize(N);
-
-    for(int j = 0; j < 2;j++)
-        for (int i = 0; i < N; i++)
-        {
-            cin >> order[j][i];
-        }
-    DivQ(0, N - 1,N-1);
+    int t;
+    while (cin >> t)
+        pre.push_back(t);
+    getPost(pre.begin(), pre.end());
+  
+    for (int c : post)
+        cout << c << "\n";
     return 0;
 }
 
 #endif 
 
-//struct Node
-//{
-//    Node(int k) : key(k){}
-//    int key;
-//    Node* left = nullptr;
-//    Node* right = nullptr;
-//};
-//vector<Node*> nodes(MAX_IN, nullptr);
-//const Node* NullNode = nodes[0];
+
