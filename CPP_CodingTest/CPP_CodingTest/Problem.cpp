87264161d73c3a29cee3126243ee37a20cd5a562
jeong98CH <jchul1998@naver.com>
2022-07-01 13:02:58 +0900

    code refactoring

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 enum
@@ -16,7 +15,6 @@ enum
 int N, M;
 vector<int> parent(MAX_IN);
 vector<vector<int>> adj(MAX_IN, vector<int>(MAX_IN));
-vector<int> plan(1000);
 
 namespace Set
 {
@@ -25,7 +23,7 @@ namespace Set
 			return a;
 		return parent[a] = Find(parent[a]);
 	}
-	void Union(int a, int b) {
+	void Merge(int a, int b) {
 		a = Find(a);
 		b = Find(b);
 		if (a > b) parent[a] = b;
@@ -37,59 +35,28 @@ namespace Set
 	}
 
 };
-
-void GetSet(int st, vector<bool>& visit) {
-	queue<int> q;
-	q.push(st);
-	while (!q.empty())
-	{
-		st = q.front();
-		q.pop();
-
-		for (int next=1;next<=N;next++)
-		{
-			if (adj[st][next] && !visit[next] && !Set::isSame(st,next))
-			{
-				Set::Union(st, next);
-				q.push(next);
-				visit[next] = true;
-			}
-		}
-	}
-	visit[st] = true;
-}
-void MergeSets() {
-	vector<bool> visit(N + 1, false);
-	for (int i = 1; i <= N; i++)
-	{
-		if (visit[i]) continue;
-		GetSet(i, visit);
-	}
-}
-
 int main() {
 	FASTIO;
 	cin >> N >> M;
 	for (int u = 1; u <= N; u++)
-	{
 		parent[u] = u;
+	
+	for (int u = 1; u <= N; u++)
+	{
 		for (int v = 1; v <= N; v++)
 		{
 			cin >> adj[u][v];
+			if (adj[u][v])
+				Set::Merge(u, v);
 		}
 	}
-	for (int i = 0; i < M; i++)
+	int dest;
+	cin >> dest;
+	int r = parent[dest];
+	for (int i = 1; i < M; i++)
 	{
-		cin >> plan[i];
-	}
-	//연결 집합 묶기
-	MergeSets();
-	//여행경로 부모와 비교
-
-	int r = parent[plan[0]];
-	for (int i=0;i<M;i++)
-	{
-		if (parent[plan[i]] != r)
+		cin >> dest;
+		if (parent[dest] != r)
 		{
 			cout << "NO\n";
 			return 0;
