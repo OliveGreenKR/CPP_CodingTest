9b0efb883a2f5452682722986dd3e0242bbec60b
jeong98CH <jchul1998@naver.com>
2022-07-01 12:51:13 +0900

    Union_find_pathFinder solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,57 +6,97 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-enum {
-  MAX_IN = 1'000'000 + 1,
+enum
+{
+	MAX_IN = 200 + 1,
 };
 int N, M;
-
 vector<int> parent(MAX_IN);
+vector<vector<int>> adj(MAX_IN, vector<int>(MAX_IN));
+vector<int> plan(1000);
 
-int Find(int a) {
-    if (parent[a] == a)
-        return a;
-    return parent[a] = Find(parent[a]);
+namespace Set
+{
+	int Find(int a) {
+		if (parent[a] == a)
+			return a;
+		return parent[a] = Find(parent[a]);
+	}
+	void Union(int a, int b) {
+		a = Find(a);
+		b = Find(b);
+		if (a > b) parent[a] = b;
+		else
+			parent[b] = a;
+	}
+	bool isSame(int a, int b) {
+		return Find(a) == Find(b);
+	}
+
+};
+
+void GetSet(int st, vector<bool>& visit) {
+	queue<int> q;
+	q.push(st);
+	while (!q.empty())
+	{
+		st = q.front();
+		q.pop();
+
+		for (int next=1;next<=N;next++)
+		{
+			if (adj[st][next] && !visit[next] && !Set::isSame(st,next))
+			{
+				Set::Union(st, next);
+				q.push(next);
+				visit[next] = true;
+			}
+		}
+	}
+	visit[st] = true;
 }
-
-void Union(int a, int b) {
-    a = Find(a);
-    b = Find(b);
-    if (a > b) parent[a] = b;
-    else
-        parent[b] = a;
-}
-
-bool isSame(int a, int b) {
-    return Find(a) == Find(b);
+void MergeSets() {
+	vector<bool> visit(N + 1, false);
+	for (int i = 1; i <= N; i++)
+	{
+		if (visit[i]) continue;
+		GetSet(i, visit);
+	}
 }
 
 int main() {
-  FASTIO;
-  cin >> N >> M;
-  for (int i = 0; i <= N; i++)
-      parent[i] = i;
+	FASTIO;
+	cin >> N >> M;
+	for (int u = 1; u <= N; u++)
+	{
+		parent[u] = u;
+		for (int v = 1; v <= N; v++)
+		{
+			cin >> adj[u][v];
+		}
+	}
+	for (int i = 0; i < M; i++)
+	{
+		cin >> plan[i];
+	}
+	//연결 집합 묶기
+	MergeSets();
+	//여행경로 부모와 비교
 
-  for (int i = 0; i < M; i++) {
-    int op, a, b;
-    cin >> op >> a >> b;
-
-    switch (op) {
-      case 0:
-        Union(a, b);
-        break;
-      case 1:
-        if (isSame(a, b)) {
-          cout << "YES\n";
-        } else
-          cout << "NO\n";
-        break;
-    }
-  }
-
-  return 0;
+	int r = parent[plan[0]];
+	for (int i=0;i<M;i++)
+	{
+		if (parent[plan[i]] != r)
+		{
+			cout << "NO\n";
+			return 0;
+		}
+	}
+	cout << "YES\n";
+	return 0;
 }
 
 #endif 
