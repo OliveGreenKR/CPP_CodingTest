b41eb94176969715382a18a4a19977e81fdd98e4
jeong98CH <jchul1998@naver.com>
2022-07-07 16:34:52 +0900

    <Set,EdgeChoice,3coordinates>::solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,27 +6,26 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cmath>
 #include <queue>
-
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 enum
 {
-	MAX_N = 1'0000+1,
+	MAX_N = 100'000+1,
 	MAX_M = 100'000+1, 
 };
-int N, M;
+int N;
 
 class Set
 {
 public:
-	Set(size_t size) : _size(size+1) {
+	Set(size_t size) : _size(size + 1) {
 		parent.resize(_size);
-		//count.resize(_size);
+
 		for (size_t i = 0; i < _size; i++)
 		{
 			parent[i] = i;
-			//count[i] = 1;
 		}
 	}
 	int Find(size_t a) {
@@ -40,33 +39,84 @@ public:
 		if (a > b)
 		{
 			parent[a] = b;
-			//count[b] += count[a];
-
 		}
 		else
 		{
 			parent[b] = a;
-			//count[a] += count[b];
 		}
 	}
 	bool IsSame(size_t a, size_t b) {
 		return Find(a) == Find(b);
 	}
 	size_t GetSize() { return _size; }
-	//size_t GetSetSize(size_t a) { return count[Find(a)]; }
 private:
 	size_t _size;
 	vector<size_t> parent;
-	//vector<size_t> count; //root [i]'s set size
 };
+enum
+{
+	Z,
+	Y,
+	X,
+};
+vector<vector<pair<int, int>>> coord(3); //[plane][val][idx]
+vector<pair<int, pair<int, int>>> edges; // [cost][[idx1,idx2]]
 
-int main() {
-	FASTIO;
-	
-	return 0;
+inline int getCost(int plane, int idx) {
+	return coord[plane][idx].first - coord[plane][idx + 1].first;
 }
 
+auto getEdge(int plane, int idx) {
+	return
+		make_pair(::abs(getCost(plane, idx)), make_pair( coord[plane][idx].second,coord[plane][idx + 1].second ));
+}
+int main() {
+	FASTIO;
+	cin >> N;
+	Set s(N);
+	for (int i = Z; i <= X; i++)
+	{
+		coord[i].reserve(N);
+	}
+	edges.reserve(3*N);
+
+	for (int idx = 1; idx <= N; idx++)
+	{
+		int x, y, z;
+		cin >> x >> y >> z;
+		coord[Z].push_back({ z,idx });
+		coord[Y].push_back({ y,idx });
+		coord[X].push_back({ x,idx });
+	}
+
+	::sort(coord[Z].begin(), coord[Z].end());
+	::sort(coord[Y].begin(), coord[Y].end());
+	::sort(coord[X].begin(), coord[X].end());
+
+	for (int i = 0; i < N-1; i++)
+	{
+		edges.push_back(getEdge(Z, i));
+		edges.push_back(getEdge(X, i));
+		edges.push_back(getEdge(Y, i));
+	}
+
+	::sort(edges.begin(), edges.end());
+	size_t ret = 0;
+
+	for (auto e :  edges)
+	{
+		int idx1, idx2;
+		idx1 = e.second.first;
+		idx2 = e.second.second;
+		if (!s.IsSame(idx1, idx2))
+		{
+			s.Merge(idx1, idx2);
+			ret += e.first;
+		}
+	}
+
+	cout << ret << "\n";
+	return 0;
+}
 #endif 
-
-
-                                                                                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                                                                   
