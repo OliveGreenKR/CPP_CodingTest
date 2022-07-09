bd077172f87646627556ca8a19fcc59ce40cd240
jeong98CH <jchul1998@naver.com>
2022-07-09 16:22:23 +0900

    dp with tree

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,116 +6,49 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cmath>
-#include <queue>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
 enum
 {
-	MAX_N = 100'000+1,
-	MAX_M = 100'000+1, 
+    MAX_IN = 100000+1,
 };
-int N;
+vector<int> dp(MAX_IN,1);
+vector<bool> visited(MAX_IN, false);
+vector<vector<int>> adj(MAX_IN);
+int N, R, Q;
 
-class Set
-{
-public:
-	Set(size_t size) : _size(size + 1) {
-		parent.resize(_size);
-
-		for (size_t i = 0; i < _size; i++)
-		{
-			parent[i] = i;
-		}
-	}
-	int Find(size_t a) {
-		if (parent[a] == a)
-			return a;
-		return parent[a] = Find(parent[a]);
-	}
-	void Merge(size_t a, size_t b) {
-		a = Find(a);
-		b = Find(b);
-		if (a > b)
-		{
-			parent[a] = b;
-		}
-		else
-		{
-			parent[b] = a;
-		}
-	}
-	bool IsSame(size_t a, size_t b) {
-		return Find(a) == Find(b);
-	}
-	size_t GetSize() { return _size; }
-private:
-	size_t _size;
-	vector<size_t> parent;
-};
-enum
-{
-	Z,
-	Y,
-	X,
-};
-vector<vector<pair<int, int>>> coord(3); //[plane][val][idx]
-vector<pair<int, pair<int, int>>> edges; // [cost][[idx1,idx2]]
-
-inline int getCost(int plane, int idx) {
-	return coord[plane][idx].first - coord[plane][idx + 1].first;
+void dfs(int node, int parent) {
+    visited[node] = true;
+    for (int next : adj[node])
+    {
+        if (visited[next])
+            continue;
+        dfs(next, node);
+    }
+    if (parent != -1)
+    {
+        dp[parent] += dp[node];
+    }
 }
 
-auto getEdge(int plane, int idx) {
-	return
-		make_pair(::abs(getCost(plane, idx)), make_pair( coord[plane][idx].second,coord[plane][idx + 1].second ));
-}
 int main() {
 	FASTIO;
-	cin >> N;
-	Set s(N);
-	for (int i = Z; i <= X; i++)
-	{
-		coord[i].reserve(N);
-	}
-	edges.reserve(3*N);
-
-	for (int idx = 1; idx <= N; idx++)
-	{
-		int x, y, z;
-		cin >> x >> y >> z;
-		coord[Z].push_back({ z,idx });
-		coord[Y].push_back({ y,idx });
-		coord[X].push_back({ x,idx });
-	}
-
-	::sort(coord[Z].begin(), coord[Z].end());
-	::sort(coord[Y].begin(), coord[Y].end());
-	::sort(coord[X].begin(), coord[X].end());
-
-	for (int i = 0; i < N-1; i++)
-	{
-		edges.push_back(getEdge(Z, i));
-		edges.push_back(getEdge(X, i));
-		edges.push_back(getEdge(Y, i));
-	}
-
-	::sort(edges.begin(), edges.end());
-	size_t ret = 0;
-
-	for (auto e :  edges)
-	{
-		int idx1, idx2;
-		idx1 = e.second.first;
-		idx2 = e.second.second;
-		if (!s.IsSame(idx1, idx2))
-		{
-			s.Merge(idx1, idx2);
-			ret += e.first;
-		}
-	}
-
-	cout << ret << "\n";
+    cin >> N >> R >> Q;
+    int a, b;
+    for (int i = 0; i < N - 1; i++)
+    {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    dfs(R, -1);
+    for (int i = 0; i < Q; i++)
+    {
+        int q;
+        cin >> q;
+        cout << dp[q] << '\n';
+    }
 	return 0;
 }
 #endif 
