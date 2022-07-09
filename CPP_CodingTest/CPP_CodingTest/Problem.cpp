8b7f4afb5b8fe11cba0d1419e15a31cb5876254b
jeong98CH <jchul1998@naver.com>
2022-07-09 19:12:14 +0900

    'Maximum Independant Set Size in Tree' problem started. Dp DFS fin.

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -6,49 +6,64 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <stack>
+#include <queue>
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
 enum
 {
-    MAX_IN = 100000+1,
+    MAX_IN = 10'000+1,
 };
-vector<int> dp(MAX_IN,1);
-vector<bool> visited(MAX_IN, false);
-vector<vector<int>> adj(MAX_IN);
-int N, R, Q;
+int N;
 
-void dfs(int node, int parent) {
-    visited[node] = true;
-    for (int next : adj[node])
+vector<int> weight(MAX_IN,0); 
+vector<int> dp(MAX_IN); //i 까지 탐사했을 때 최대 크기
+vector<vector<int>>adj(MAX_IN);
+vector<int>visited(MAX_IN);
+
+void Dfs(int pre, int now, int cost) { //최근 선택, 현재위치, cost
+    dp[now] = ::max(dp[now], cost);
+
+    visited[now] = true;
+
+    for (int next : adj[now])
     {
-        if (visited[next])
-            continue;
-        dfs(next, node);
+        if (visited[next]) continue;
+        if(now != pre)//선택
+            Dfs(next,next, cost+weight[next]);
+        Dfs(pre,next, cost);
     }
-    if (parent != -1)
-    {
-        dp[parent] += dp[node];
-    }
+
+    visited[now] = false;
 }
 
+void getAns(const int root) {
+    adj[0].push_back(root);
+    Dfs(-1, 0, 0);
+
+
+}
 int main() {
 	FASTIO;
-    cin >> N >> R >> Q;
-    int a, b;
-    for (int i = 0; i < N - 1; i++)
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
     {
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+        cin >> weight[i];
+       /* dp[i] = weight[i];*/
     }
-    dfs(R, -1);
-    for (int i = 0; i < Q; i++)
+    for (int i = 0; i < N - 1; i++) //tree
     {
-        int q;
-        cin >> q;
-        cout << dp[q] << '\n';
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
+    //root:1
+    const int root = 1;
+    getAns(root);
+
 	return 0;
 }
 #endif 
