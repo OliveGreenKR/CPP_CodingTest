5e8920ec34cf0c97a43969c007292506b4e8c1c0
jeong98CH <jchul1998@naver.com>
2022-07-09 21:09:55 +0900

    DP Tree solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -9,6 +9,8 @@
 #include <stack>
 #include <queue>
 using namespace std;
+using int64 = long long;
+
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
 enum
@@ -18,31 +20,30 @@ enum
 int N;
 
 vector<int> weight(MAX_IN,0); 
-vector<int> dp(MAX_IN); //i 까지 탐사했을 때 최대 크기
 vector<vector<int>>adj(MAX_IN);
-vector<int>visited(MAX_IN);
+vector<vector<int>>dp(MAX_IN,vector<int>(2)); //[node][state]
+vector<bool> visited(MAX_IN, false);
 
-void Dfs(int pre, int now, int cost) { //최근 선택, 현재위치, cost
-    dp[now] = ::max(dp[now], cost);
+void Dfs(int root) {
+    dp[root][0] = 0;
+    dp[root][1] = weight[root];
 
-    visited[now] = true;
+    visited[root] = true;
 
-    for (int next : adj[now])
+    for (int next : adj[root])
     {
         if (visited[next]) continue;
-        if(now != pre)//선택
-            Dfs(next,next, cost+weight[next]);
-        Dfs(pre,next, cost);
+        Dfs(next);
+
+        dp[root][0] += ::max(dp[next][0], dp[next][1]);
+        dp[root][1] += dp[next][0];
     }
-
-    visited[now] = false;
 }
-
+  
 void getAns(const int root) {
     adj[0].push_back(root);
-    Dfs(-1, 0, 0);
-
-
+    Dfs(0);
+    cout << ::max(dp[0][0], dp[0][1]);
 }
 int main() {
 	FASTIO;
@@ -51,7 +52,6 @@ int main() {
     for (int i = 1; i <= N; i++)
     {
         cin >> weight[i];
-       /* dp[i] = weight[i];*/
     }
     for (int i = 0; i < N - 1; i++) //tree
     {
@@ -60,8 +60,7 @@ int main() {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    //root:1
-    const int root = 1;
+    const int root = 1; //root:1
     getAns(root);
 
 	return 0;
