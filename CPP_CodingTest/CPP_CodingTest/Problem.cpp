ff2bd2bef9301607994b03ee5a83dc8f50f67be2
OliveGreen <olivegreenkr98@gmail.com>
2022-12-24 13:13:12 +0900

    vector Cross product

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -9,91 +9,44 @@
 #include <stack>
 #include <queue>
 using namespace std;
+using float64 = double;
 using int64 = long long;
-
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
 enum
 {
     MAX_IN = 10'000+1,
 };
-int N;
+struct Pos {
+    int64 x;
+    int64 y;
+};
 
-vector<vector<int>>adj(MAX_IN);
-vector<vector<int>>dp(MAX_IN,vector<int>(2)); //[node][state]
-vector<bool> visited(MAX_IN, false);
-vector<int> weight(MAX_IN);
+vector<Pos>positions(MAX_IN);
 
-//1끼리 독립집합만들기, 1끼리 인접못함
-void Dfs(int root) {
-
-    dp[root][0] = 0;
-    dp[root][1] = weight[root];
-
-    visited[root] = true;
-
-    for (int next : adj[root])
-    {
-        if (visited[next]) continue;
-
-        Dfs(next);
-
-        dp[root][0] += ::max(dp[next][1], dp[next][0]);
-        dp[root][1] += dp[next][0];
-    }
+double CCW(Pos p1, Pos p2, Pos p3) {
+	return (p1.x*p2.y + p2.x*p3.y + p3.x*p1.y) - (p1.y*p2.x + p2.y*p3.x + p3.y*p1.x);
 }
-vector<int> path;
-void Track(int state,int now) {
-   
-    visited[now] = true;
 
-    //자손중 큰 상태를 선택해야함 + 상태확인
-	for (int next : adj[now])
-	{
-        if (visited[next]) continue;
-
-        if (state || dp[next][1] < dp[next][0])
-        {
-            Track(0, next);
-        }
-        else if (dp[next][1] > dp[next][0])
-        {
-            path.push_back(next);
-            Track(1, next);
-        }
-	}
+//벡터외적 : 삼각형 넓이는 외적의 절반
+double GetTriangleArea(Pos p1, Pos p2, Pos p3) {
+    return 0.5 * CCW(p1, p2, p3);
 }
-void getAns(const int root) {
-    visited = vector<bool>(MAX_IN, false);
-    adj[0].push_back(root);
 
-    Dfs(0);
-    cout << dp[0][0] << "\n";
-
-    visited = vector<bool>(MAX_IN, false);
-    Track(0, 0);
-
-    ::sort(path.begin(), path.end());
-    for (int n : path)
-        cout << n << " ";
-}
 int main() {
 	FASTIO;
+    int N;
     cin >> N;
-    path.reserve(N);
 
-    for (int i = 1; i <= N; i++)
-        cin >> weight[i];
-
-    for (int i = 0; i < N - 1; i++) //tree
-    {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+    for (int i = 0; i<N; i++) {
+        cin >> positions[i].x >> positions[i].y;
     }
-    const int root = 1; //root:1
-    getAns(root);
+    double area = 0.0;
+    for (int i = 1; i<N-1; i++) {
+        
+        area += GetTriangleArea( positions[0],positions[i],positions[i+1]);
+    }
+    printf("%.1lf", abs(area));
 
 	return 0;
 }
