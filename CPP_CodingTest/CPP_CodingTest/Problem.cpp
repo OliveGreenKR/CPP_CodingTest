a6025f8f608eb2e81cbad89cc8154cf737dd251c
UOS_MIE <UOS@DESKTOP-5LPL2RE>
2023-01-12 15:03:27 +0900

    외판원 순환문제 - dfs+dp :  시간초과

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -11,13 +11,14 @@
 #include <cmath>
 #include <stack>
 #include <queue>
+#include <limits.h>
 
 using namespace std;
 
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define POW(x) (std::pow((x),2))
 
-namespace Bitset {
+namespace bitset {
 	int add(int mask, int x) {
 		mask |= (1 << x);
 		return mask;
@@ -38,44 +39,59 @@ namespace Bitset {
 		}
 		return cnt;
 	}
+
+	int getAll(int x) {
+		return (1<<(x+1))-1;
+	}
 }
 
-vector<vector<int>> cost;
+vector<vector<int>> map;
 vector<int> dp; // dp[mask]= mask상태의 최소 비용
 
+int allmask;
+int lastmask;
 int N;
 
-enum {
-	MAX_M = 10'000
-};
+
+void dfs(int start , int now, int mask) {
+	if (mask == allmask) {
+		if (map[now][start] != 0)
+			dp[lastmask] = ::min(dp[lastmask], dp[allmask]+map[now][start]);
+		return;
+	}
+
+	for (int next = 0; next < N; next++) {
+		if (map[now][next]== 0 || bitset::check(mask,next)) //길이 없거나, 이미 방문
+			continue;
+		int nextmask = bitset::add(mask, next);
+		int nextdp = dp[nextmask];
+		dp[nextmask] = ::min(nextdp, dp[mask]+map[now][next]);
+		dfs(start, next, nextmask); //지금 방문
+		dp[nextmask] = nextdp; //방문 안함
+	}
+}
 
 int main() {
 	FASTIO;
-	
-	cin >> N; //max=20
 
-	cost.resize(N, vector<int>(N)); //job cost
-	dp.resize(1<<N,INT_MAX);
-	dp[0]= 0;
-	
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cin >> cost[i][j];
+	cin >> N; //max=16
+
+	map.resize(N, vector<int>(N));
+	dp.resize(1<<(N+1), INT_MAX);
+
+	for (int i = 0; i< N; i++) {
+		for (int j = 0; j<N; j++) {
+			cin >> map[i][j];
 		}
 	}
+	allmask = bitset::getAll(N-1); //모든 도시 방문
+	lastmask = bitset::getAll(N);  //복귀까지 성공
 
-	const int maxmask = ::pow(2, N);
-	for (int mask = 0; mask < maxmask ; mask++) {
-		int x = Bitset::countBit(mask);
-		for (int j = 0; j < N; j++) {
-			if (!Bitset::check(mask, j)) {
-				int nextmask = Bitset::add(mask, j);
-				dp[nextmask] = ::min(dp[nextmask], dp[mask] + cost[x][j]);
-			}
-		}
-	}
+	int mask = bitset::add(0, 0);
+	dp[mask] = 0;
+	dfs(0, 0,mask);
 
-	cout << dp[dp.size()-1];
+	cout << dp[lastmask];
 
 	return 0;
 }
