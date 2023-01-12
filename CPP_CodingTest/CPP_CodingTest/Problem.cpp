08331b2f664cc181185b0f8cf58b4454617bbc38
UOS_MIE <UOS@DESKTOP-5LPL2RE>
2023-01-12 15:49:14 +0900

    solving

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -17,6 +17,7 @@ using namespace std;
 
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define POW(x) (std::pow((x),2))
+#define INF (1'000'000'000)
 
 namespace bitset {
 	int add(int mask, int x) {
@@ -41,34 +42,31 @@ namespace bitset {
 	}
 
 	int getAll(int x) {
-		return (1<<(x+1))-1;
+		return (1<<(x))-1;
 	}
 }
 
 vector<vector<int>> map;
-vector<int> dp; // dp[mask]= mask상태의 최소 비용
+vector<vector<int>> dp; //dp[now][mask] = 현재now 일때, mask 상태의 최소 비용.
 
 int allmask;
-int lastmask;
 int N;
 
+int dfs(int start , int now, int mask) {
 
-void dfs(int start , int now, int mask) {
 	if (mask == allmask) {
 		if (map[now][start] != 0)
-			dp[lastmask] = ::min(dp[lastmask], dp[allmask]+map[now][start]);
-		return;
+			return map[now][start];
+		return INF;
 	}
 
 	for (int next = 0; next < N; next++) {
 		if (map[now][next]== 0 || bitset::check(mask,next)) //길이 없거나, 이미 방문
 			continue;
 		int nextmask = bitset::add(mask, next);
-		int nextdp = dp[nextmask];
-		dp[nextmask] = ::min(nextdp, dp[mask]+map[now][next]);
-		dfs(start, next, nextmask); //지금 방문
-		dp[nextmask] = nextdp; //방문 안함
+		dp[now][mask] = ::min(dp[now][mask], dfs(start, next, nextmask)+map[now][next]);
 	}
+	return dp[now][mask];
 }
 
 int main() {
@@ -77,21 +75,18 @@ int main() {
 	cin >> N; //max=16
 
 	map.resize(N, vector<int>(N));
-	dp.resize(1<<(N+1), INT_MAX);
+	dp.resize(N,vector<int>(1<<(N), INF));
 
 	for (int i = 0; i< N; i++) {
 		for (int j = 0; j<N; j++) {
 			cin >> map[i][j];
 		}
 	}
-	allmask = bitset::getAll(N-1); //모든 도시 방문
-	lastmask = bitset::getAll(N);  //복귀까지 성공
+	allmask = bitset::getAll(N); //모든 도시 방문
 
 	int mask = bitset::add(0, 0);
-	dp[mask] = 0;
-	dfs(0, 0,mask);
 
-	cout << dp[lastmask];
+	cout << dfs(0, 0, mask);
 
 	return 0;
 }
