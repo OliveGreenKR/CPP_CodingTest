7b20b0aa2b3ed71e1a2056fe294aa4b38c8eb6a9
OliveGreen <olivegreenkr98@gmail.com>
2023-01-19 16:05:34 +0900

    kmp(find pattern in Text)  solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -19,11 +19,64 @@ using namespace std;
 
 #define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
+vector<int> makePatternTable(const string& pattern) {
+	
+	const int size = pattern.size();
+	vector<int> out = vector<int>(size, 0);
+    int k = 0;
+    for (int i = 1; i < size; i++) {
+		while (k > 0 && pattern[i] != pattern[k])
+            k = out[k - 1];
+
+        if (pattern[k] == pattern[i]) 
+            out[i] = ++k;
+    }
+    return out;
+}
+
+void kmp(const string& text, const string& pattern) {
+    vector<int> ptable = makePatternTable(pattern);
+
+    int j = 0;
+    int count = 0;
+    vector<int> foundIndex;
+    const int last = pattern.size()-1;
+
+    for (int i = 0; i< text.size(); i++) {
+        auto& now = text[i];
+        while (j >0 && now != pattern[j])
+            j = ptable[j-1];
+
+        if (now == pattern[j]) {
+            if (j == last) {
+                count++;
+                int index = (i - last +1);
+                foundIndex.push_back(index);
+                j = 0;
+            }
+            else
+                j++;
+        }
+    }
+
+    //Ãâ·Â
+    cout << count << "\n";
+    for (auto& idx : foundIndex)
+        cout << idx << ' ';
+
+    return;
+}
 
 int main() {
 	FASTIO;
 
+    string text, pattern;
+
+    getline(cin, text);
+    getline(cin, pattern);
+
+    kmp(text, pattern);
+
 	return 0;
 }
 #endif 
-//tooo :  algorithm read
