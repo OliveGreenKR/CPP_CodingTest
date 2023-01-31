aca61b42390fd9d721a48b66ee5eb5aa68a64dfb
OliveGreen <olivegreenkr98@gmail.com>
2023-01-31 13:41:12 +0900

    baekjoon 1766 solved (maxHeap + TopologySort)

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -17,118 +17,72 @@
 #include <map>
 #include <limits.h>
 
+#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
 using namespace std;
 using int64 = long long;
 
-#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+template<typename Ty>
+using minHeap = priority_queue<Ty, vector<Ty>, greater<Ty>>;
 
 struct Node {
-	vector<int> adj;
+	vector<int> _adj;
 };
 
+void HeapTopologySort(vector<Node>& nodes, vector<int>& indegree, vector<int>& result) {
 
-void TopologySort(vector<Node>& nodes,vector<int>& indegree, vector<int>& result) {
+	minHeap<int> pq; //min Heap
 
-	int maxN = nodes.size(); //node max count
-	queue<int> q;
-	result.resize(maxN);
+	const int N = indegree.size();
+	
+	result.resize(N);
 
-	//push zero indgree node to Q
-
-	for (int now = 0; now < maxN; now++) {
-
-		if (indegree[now]==0) {
-			q.push(now);
+	for (int i = 0; i < N; i++) {
+		if (indegree[i]==0) {
+			pq.push(i);
 		}
 	}
 
-	if (q.size()>1) {
-		cout << "?\n";
-		return;
-	}
+	for (int i = 0; i < N; i++) {
 
-	//topology sort
-	for (int now = 0; now < maxN; now++) {
-		if (q.empty()) {
-			cout << "IMPOSSIBLE\n";
-			return; //cyclic graph
-		}
-
-		int next = q.front();
-		q.pop();
-
-		result[now] = next;
-
-		//push zero indgree node to Q
-		for (auto n : nodes[next].adj) {
-			if (--indegree[n] == 0)
-				q.push(n);
-		}
-
-		if (q.size()>1) {
-			cout << "?\n";
+		if (pq.empty())
 			return;
+		int now = pq.top();
+		pq.pop();
+
+		result[i] = now;
+
+		for (auto& n : nodes[now]._adj) {
+			if (--indegree[n] == 0){
+				pq.push(n);
+			}
 		}
 	}
-
-	for (auto& out : result)
-		cout << (out+1) << " ";
-	cout << "\n";
 
 	return;
 }
 
-
-void makeIndegree(auto& matrix, auto& ranks) {
-	int indeg = 0;
-	for (auto& r : ranks) {
-		matrix[r-1] = indeg++;
-	}
-	return;
-}
-
-void makeNodes(auto& nodes, auto& ranks) {
-	for (int rank = 0; rank < ranks.size(); rank++) {
-		auto& node = ranks[rank];
-		for (int r = rank+1; r < ranks.size(); r++) {
-			auto& tonode = ranks[r];
-			nodes[node-1].adj.push_back(tonode-1);
-		}
-	}
-}
 
 int main() {
 	FASTIO;
-	int T, N, M;
-	cin >> T;
+	int N,M;
+	cin >> N >> M;
 
-	for (int i = 0; i<T; i++) {
-		cin >> N;
-		vector<int> preranks(N);
-		for (auto& r : preranks)
-			cin >> r;
-		vector<int> indegrees(N);
-		vector<Node> nodes(N);
-		makeIndegree(indegrees, preranks);
+	vector<int> indegrees(N);
+	vector<Node> nodes(N);
+	vector<int> result;
 
-		//todo: make node to array
-		makeNodes(nodes, preranks);
-
-		cin >> M;
-
-
-		for (int j = 0; j<M; j++) {
-			int A, B;
-			cin >> A >> B;
-			indegrees[--A]--;
-			indegrees[--B]++;
-		}
-
-		vector<int> result(N);
-
-		TopologySort(nodes, indegrees, result);
-
+	for (int i = 0; i< M; i++) {
+		int A, B;
+		cin >> A >> B;
+		indegrees[--B]++;
+		nodes[--A]._adj.push_back(B);
 	}
+
+	HeapTopologySort(nodes, indegrees, result);
+
+	for (int& n : result)
+		cout << ++n << " ";
 
 	return 0;
 }
