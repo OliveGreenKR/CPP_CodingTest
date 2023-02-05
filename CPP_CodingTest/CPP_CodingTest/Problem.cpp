a9aca623c2290bb65117c25a46d9659ae5573d41
OliveGreen <olivegreenkr98@gmail.com>
2023-02-05 19:51:37 +0900

    LCA  Tree solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -23,70 +23,120 @@
 using namespace std;
 using int64 = long long;
 
-template<typename Ty>
-using minHeap = priority_queue<Ty, vector<Ty>, greater<Ty>>;
 
-template<typename Ty> //default heap
-using maxHeap = priority_queue<Ty, vector<Ty>, less<Ty>>;
-
-struct Node {
-	vector<int> _adj;
-};
-
-void HeapTopologySort(const vector<Node>& nodes, vector<int> indegree, OUT vector<int>& result) {
-
-	minHeap<int> pq; //min Heap
-
-	const int N = indegree.size();
-	
-	result.resize(N);
-
-	for (int i = 0; i < N; i++) {
-		if (indegree[i]==0) {
-			pq.push(i);
-		}
+class SparseTable {
+public:
+	SparseTable(int n, int x) {
+		_logk = (int)log2(n);
+		_lenx = x;
+		_parent.resize(_lenx+1, vector<int>(_logk+1));
+		_depth.resize(_lenx+1, -1);
+		_adj.resize(_lenx+1);
 	}
 
-	for (int i = 0; i < N; i++) {
+	void addNode(int a, int b) {
+		_adj[a].push_back(b);
+		_adj[b].push_back(a);
+	}
 
-		if (pq.empty())
-			return;
-		int now = pq.top();
-		pq.pop();
 
-		result[i] = now;
+	void tableInit() {
 
-		for (auto& n : nodes[now]._adj) {
-			if (--indegree[n] == 0){
-				pq.push(n);
+		initDepth();
+
+		for (int k = 1; k < _logk+1; k++) {
+			for (int i = 2; i < _lenx+1; i++) {
+				if (_parent[i][k-1]!=0)
+					_parent[i][k] = _parent[_parent[i][k-1]][k-1];
 			}
 		}
+
+		return;
+	}
+	//노드 x에서 n만큼 위로
+	int query(int n, int x) {
+
+		if (n <= 0)
+			return x;
+
+		for (int k = log2(n); k >= 0; k--) {
+			if ((n & (1<< k)) != 0)
+				x = _parent[x][k];
+		}
+		return x;
 	}
 
-	return;
-}
 
+	int findLCA(int a, int b) {
+		int ha = _depth[a];
+		int hb = _depth[b];
+		int h = ::abs(ha -  hb);
+
+		(ha > hb) ? a = query(h, a) : b = query(h, b); //now, a and b are same height
+
+
+		if (a!=b) {
+			for (int k = _logk; k >= 0; k--) {
+				if (_parent[a][k]!= 0  && _parent[a][k] != _parent[b][k]) {
+					a = _parent[a][k];
+					b = _parent[b][k];
+				}
+			}
+		}
+
+		return _parent[a][0];
+	}
+
+private:
+
+	void initDepth(int parent = 0, int now = 1, int depth = 0) {
+
+		int& dep = _depth[now];
+		int& par = _parent[now][0];
+
+		if (dep != -1 || now < 1)
+			return;
+
+		par = parent;
+		dep = depth;
+		for (auto next : _adj[now]) {
+			if(now != next)
+				initDepth(now, next, depth+1);
+		}
+	}
+
+private:
+	int _logk;
+	int _lenx;
+	vector<vector<int>> _parent;
+	vector<int> _depth;
+	vector<vector<int>> _adj;
+};
 
 int main() {
 	FASTIO;
-	int N,M;
-	cin >> N >> M;
+	int N, M;
 
-	vector<int> indegrees(N);
-	vector<Node> nodes(N);
-	vector<int> result;
+	cin >> N;
 
-	for (int i = 0; i< M; i++) {
-		int A, B;
-		cin >> A >> B;
-		indegrees[--B]++;
-		nodes[--A]._adj.push_back(B);
+	SparseTable st(N, N);
+
+	//input tree
+	for (int i = 1; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+		st.addNode(a, b);
 	}
+	st.tableInit();
 
-	HeapTopologySort(nodes, indegrees, result);
+	cin >> M;
 
-	for (int& n : result)
-		cout << ++n << " ";
+	//input query
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << "ans : " << st.findLCA(a, b) << "\n";
+	}
 
 	return 0;
 }
