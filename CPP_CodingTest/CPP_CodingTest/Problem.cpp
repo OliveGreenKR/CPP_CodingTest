e1667e3a6eaca8c5580c3a1cea7e79343463bc6d
OliveGreen <olivegreenkr98@gmail.com>
2023-02-07 14:59:32 +0900

    solving.. need to debug specifig case

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -25,18 +25,29 @@ using int64 = long long;
 
 
 class SparseTable {
+
+	struct Road {
+		Road(int b, int c) : dest(b), cost(c) {
+		}
+		int dest = -1;
+		int cost = -1;
+	};
+
 public:
 	SparseTable(int n, int x) {
 		_logk = (int)log2(n);
 		_lenx = x;
 		_parent.resize(_logk+1, vector<int>(_lenx+1));
+		_maxcost.resize(_logk+1, vector<int>(_lenx+1));
+		_mincost.resize(_logk+1, vector<int>(_lenx+1));
+
 		_depth.resize(_lenx+1, -1);
 		_adj.resize(_lenx+1);
 	}
 
-	void addNode(int a, int b) {
-		_adj[a].push_back(b);
-		_adj[b].push_back(a);
+	void addNode(int a, int b, int c) {
+		_adj[a].push_back(Road(b, c));
+		_adj[b].push_back(Road(a, c));
 	}
 
 
@@ -46,8 +57,13 @@ public:
 
 		for (int k = 1; k < _logk+1; k++) {
 			for (int i = 2; i < _lenx+1; i++) {
-				if (_parent[k-1][i]!=0)
+				if (_parent[k-1][i]!=0) {
 					_parent[k][i] = _parent[k-1][_parent[k-1][i]];
+					_mincost[k][i] = ::min(_mincost[k][i],_mincost[k-1][_parent[k-1][i]]);
+					_maxcost[k][i] = ::max(_maxcost[k][i],_maxcost[k-1][_parent[k-1][i]]);
+		
+				}
+					
 			}
 		}
 
@@ -66,10 +82,28 @@ public:
 		return x;
 	}
 
+	int query(int n, int x, int& maxcost, int& mincost) {
 
-	int findLCA(int a, int b) {
+		if (n <= 0)
+			return x;
+
+		for (int k = log2(n); k >= 0; k--) {
+			if ((n & (1<< k)) != 0) {
+				x = _parent[k][x];
+				maxcost = ::max(maxcost, _maxcost[k][x]);
+				mincost = ::min(mincost, _mincost[k][x]);
+			}
+		}
+		return x;
+	}
+
+
+	void solveQuery(int a, int b) {
 		int ha = _depth[a];
 		int hb = _depth[b];
+		int max = ::max( _maxcost[0][a], _maxcost[0][b]);
+		int min = ::min( _mincost[0][a], _mincost[0][b]);
+
 		int h = ::abs(ha -  hb);
 
 		(ha > hb) ? a = query(h, a) : b = query(h, b); //now, a and b are same height
@@ -80,29 +114,41 @@ public:
 				if (_parent[k][a]!= 0  && _parent[k][a] != _parent[k][b]) {
 					a = _parent[k][a];
 					b = _parent[k][b];
+					max = ::max(max, ::max(_maxcost[k][a], _maxcost[k][b]));
+					min = ::min(max, ::min(_mincost[k][a], _mincost[k][b]));
 				}
 			}
 			a=_parent[0][a];
+			max = ::max(max, _maxcost[0][a]);
+			min = ::max(min, _mincost[0][a]);
 		}
 
-		return a;
+		//cout ans
+		cout << min << " " << max << "\n";
+
+		return;
 	}
 
 private:
 
-	void initDepth(int parent = 0, int now = 1, int depth = 0) {
+	void initDepth(int parent = 0, int now = 1, int depth = 0, int cost = 0) {
 
 		int& dep = _depth[now];
 		int& par = _parent[0][now];
+		int& min = _mincost[0][now];
+		int& max = _maxcost[0][now];
 
 		if (dep != -1 || now < 1)
 			return;
 
 		par = parent;
 		dep = depth;
-		for (auto next : _adj[now]) {
+		max = cost;
+		min = cost;
+		for (auto road : _adj[now]) {
+			int next = road.dest;
 			if(now != next)
-				initDepth(now, next, depth+1);
+				initDepth(now, next, depth+1, road.cost);
 		}
 	}
 
@@ -110,8 +156,11 @@ private:
 	int _logk;
 	int _lenx;
 	vector<vector<int>> _parent;
+	vector<vector<int>> _maxcost;
+	vector<vector<int>> _mincost;
+
 	vector<int> _depth;
-	vector<vector<int>> _adj;
+	vector<vector<Road>> _adj;
 };
 
 int main() {
@@ -123,10 +172,10 @@ int main() {
 	SparseTable st(N, N);
 
 	//input tree
-	for (int i = 1; i < N; i++) {
-		int a, b;
-		cin >> a >> b;
-		st.addNode(a, b);
+	for (int i = 0; i < N-1; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		st.addNode(a, b, c);
 	}
 	st.tableInit();
 
@@ -136,7 +185,7 @@ int main() {
 	for (int i = 0; i < M; i++) {
 		int a, b;
 		cin >> a >> b;
-		cout << st.findLCA(a, b) << "\n";
+		st.solveQuery(a,b);
 	}
 
 	return 0;
