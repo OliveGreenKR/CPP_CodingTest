18114a83d663471308c235e0c106f12ba2568350
OliveGreen <olivegreenkr98@gmail.com>
2023-02-09 16:15:55 +0900

    baekjoon 13511 solved

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -40,7 +40,7 @@ public:
 		_lenx = x;
 		_parent.resize(_logk+1, vector<int>(_lenx+1,0));
 
-		_cost.resize(_logk+1, vector<int>(_lenx+1,MINV));
+		_cost.resize(_lenx+1,MINV);
 
 		_depth.resize(_lenx+1, 0);
 		_adj.resize(_lenx+1);
@@ -63,78 +63,60 @@ public:
 		initDepth();
 
 		for (int k = 1; k < _logk+1; k++) {
-			for (int i = 2; i < _lenx+1; i++) {
-				if (_parent[k-1][i]!=0) {
-					_cost[k][i] = _cost[k-1][i] + _cost[k-1][_parent[k-1][i]];
-					_parent[k][i] = _parent[k-1][_parent[k-1][i]];
-
-				}
-					
+			for (int i = 1; i < _lenx+1; i++) {
+				_parent[k][i] = _parent[k-1][_parent[k-1][i]];
 			}
 		}
 
 		return;
 	}
-	//노드 x에서 n만큼 위로
 	int gohigh(int n, int x) {
 
-		if (n <= 0)
-			return x;
-
-		for (int k = log2(n); k >= 0; k--) {
-			if ((n & (1<< k)) != 0)
+		for (int k = 0; n; k++) {
+			if (n & 1)
 				x = _parent[k][x];
+			n >>= 1;
 		}
 		return x;
 	}
 
-	int gohigh(int n, int x, OUT int& cost, OUT vector<int>& path ) {
-
-		if (n <= 0)
-			return x;
-
-		for (int k = (int)log2(n); k >= 0; k--) {
-			if ((n & (1<< k)) != 0) {
-				cost += _cost[k][x];
-				x = _parent[k][x];
-				path.push_back(x);
-			}
-		}
-		return x;
-	}
-	
-	pair<int,vector<int>> solveQeury(int a, int b) {
+	int getLCA(int a, int b) {
 		int ha = _depth[a];
 		int hb = _depth[b];
-		int cost = MINV;
-		vector<int> apath;
-		vector<int> bpath;
-
-		apath.push_back(a);
-		bpath.push_back(b);
-
 		int h = ::abs(ha -  hb);
 
-		(ha > hb) ? a = gohigh(h, a, cost, apath) : b = gohigh(h, b, cost, bpath); //now, a and b are same height
+		(ha > hb) ? a = gohigh(h, a) : b = gohigh(h, b); //now, a and b are same height
 
 		if (a!=b) {
 			for (int k = _logk; k >= 0; k--) {
-				if (_parent[k][a]!= 0  && _parent[k][a] != _parent[k][b]) {
-					cost += _cost[k][a] + _cost[k][b];
+				if (_parent[k][a] != _parent[k][b]) {
 					a = _parent[k][a];
 					b = _parent[k][b];
-					apath.push_back(a);
-					bpath.push_back(b);
 				}
 			}
-			cost += _cost[0][a] + _cost[0][b];
-			a = _parent[0][a];//LCA 
-			apath.push_back(a);
+			a = _parent[0][a];
 		}
-		for (int i = (bpath.size()-1); i >=0; i--) {
-			apath.push_back(bpath[i]);
+
+		return a;
+	}
+
+	int getKth(int a, int b, int k) {
+		int lca = getLCA(a, b);
+		int delta = _depth[a] - _depth[lca];
+		k--;
+		if (delta >= k) {
+			k = gohigh(k, a);
 		}
-		return ::make_pair(cost,apath);
+		else {
+			k = _depth[b] - _depth[lca] - k + delta;
+			k = gohigh(k, b);
+		}
+		return k;
+	}
+
+	int getCost(int a, int b) {
+		int lca = getLCA(a, b);
+		return _cost[a]+_cost[b] -2*_cost[lca];
 	}
 
 	int Query() {
@@ -147,34 +129,33 @@ public:
 		switch(q) {
 		case 1:
 			cin >> a >> b;
-			ret = solveQeury(a, b).first;
+			ret = getCost(a, b);
 			break;
 		case 2:
 			cin >> a >> b >> k;
-			ret = solveQeury(a, b).second[k-1];
+			ret = getKth(a, b, k);
 			break;
 		default:
 			break;
 		}
-
 		return ret;
 	}
 
 private:
 
-	void initDepth(int parent = 0, int now = 1, int depth = 0, int cost = 0) {
+	void initDepth(int parent = 0, int now = 1, int depth = 0, int64 cost = 0) {
 
 		int& dep = _depth[now];
 		int& par = _parent[0][now];
-		int& mcost = _cost[0][now];
+		int64& mcost = _cost[now];
 
 		par = parent;
 		dep = depth;
 		mcost = cost;
-		for (auto road : _adj[now]) {
-			int next = road.dest;
+		for (auto edge : _adj[now]) {
+			int next = edge.dest;
 			if(parent != next)
-				initDepth(now, next, depth+1, road.cost);
+				initDepth(now, next, depth+1, edge.cost+cost);
 		}
 		return;
 	}
@@ -183,7 +164,7 @@ private:
 	int _logk;
 	int _lenx;
 	vector<vector<int>> _parent;
-	vector<vector<int>> _cost;
+	vector<int64> _cost;
 
 	vector<int> _depth;
 	vector<vector<Edge>> _adj;
@@ -210,3 +191,112 @@ int main() {
 }
 #endif 
 //de bug case  : 1 이 들어갈 때, 현재 root=1이라고 한 상태임.
+#if 0
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <limits.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> p;
+
+vector<p> g[101010];
+ll dp[101010][22];
+ll dist[101010];
+ll dep[101010];
+ll n;
+bool chk[101010];
+
+void dfs(ll now, ll d, ll c) {
+	chk[now] = 1;
+	dep[now] = d;
+	dist[now] = c;
+	for (auto i : g[now]) {
+		ll nxt = i.first;
+		if (chk[nxt]) continue;
+		ll nxtCost = i.second + c;
+		dp[nxt][0] = now;
+		dfs(nxt, d+1, nxtCost);
+	}
+}
+
+void make() {
+	for (int j = 1; j<22; j++) {
+		for (int i = 1; i<=n; i++) {
+			dp[i][j] = dp[dp[i][j-1]][j-1];
+		}
+	}
+}
+
+ll getLca(ll u, ll v) {
+	if (dep[u] > dep[v]) swap(u, v);
+	ll diff = dep[v] - dep[u];
+	for (int i = 0; diff; i++) {
+		if (diff & 1) v = dp[v][i];
+		diff >>= 1;
+	}
+	if (u == v) return u;
+	for (int i = 21; i>=0; i--) {
+		if (dp[u][i] != dp[v][i]) u = dp[u][i], v = dp[v][i];
+	}
+	return dp[u][0];
+}
+
+ll getDist(ll u, ll v) {
+	ll lca = getLca(u, v);
+	return dist[u] + dist[v] - 2*dist[lca];
+}
+
+ll getkth(ll u, ll v, ll k) {
+	ll lca = getLca(u, v);
+	ll diff = dep[u] - dep[lca];
+	k--;
+	if (diff >= k) {
+		for (int i = 0; k; i++) {
+			if (k & 1) u = dp[u][i];
+			k >>= 1;
+		}
+		return u;
+	}
+
+	k = dep[v] - dep[lca] + diff - k;
+	for (int i = 0; k; i++) {
+		if (k & 1) v = dp[v][i];
+		k >>= 1;
+	}
+	return v;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> n;
+	for (int i = 1; i<n; i++) {
+		ll s, e, x; cin >> s >> e >> x;
+		g[s].push_back({e, x});
+		g[e].push_back({s, x});
+	}
+	dfs(1, 1, 0); make();
+
+	int m; cin >> m;
+	while (m--) {
+		int op; cin >> op;
+		if (op == 1) {
+			ll a, b; cin >> a >> b;
+			cout << getDist(a, b) << "\n";
+		}
+		else {
+			ll a, b, c; cin >> a >> b >> c;
+			cout << getkth(a, b, c) << "\n";
+
+		}
+	}
+}
+#endif
