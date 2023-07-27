84e84d87333068d13e5cbb58ff99a340719daf62
OliveGreen <olivegreenkr98@gmail.com>
2023-07-27 17:37:52 +0900

    solving.,,

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -13,58 +13,43 @@
 using namespace std;
 const int INF = 10001;
 const int MAX_CNT = 150 +1;
+
 int max_alp = 0, max_cop = 0;
+vector<vector<int>> dp(MAX_CNT, vector<int>(MAX_CNT, INF)); //dp[alp][cop]의 최단시간.
 
-using cost_type = int;
-
-vector<vector<cost_type>> dp(MAX_CNT, vector<cost_type>(MAX_CNT, INF)); //dp[alp][cop]의 최단시간.
-
-//problem :  [alp_req, cop_req, alp_rwd, cop_rwd, cost]
-int find_min_cost(const int alp, const int cop, cost_type cost, const vector<vector<int>>& problems) {
-
-	static vector<bool> pass_problem(problems.size(), false);
-
-	if (alp >= max_alp && cop >= max_cop)
-		return dp[max_alp][max_cop] = ::min(cost, dp[max_alp][max_cop]);
-
-	if (dp[alp][cop] <= cost)
-		return INF;
-
-	dp[alp][cop] = cost;
-
-	for (auto it = problems.begin(); it != problems.end(); ++it) {
-		if (pass_problem[it - problems.begin()] != true) {
-			cost_type next_cost = cost;
-			//필요치 까지 공부하기
-			next_cost += (*it)[0] - alp > 0 ? (*it)[0] - alp : 0;
-			next_cost += (*it)[1] - cop > 0 ? (*it)[1] - cop : 0;
-			int next_alp = ::max((*it)[0], alp);
-			int next_cop = ::max((*it)[1], cop);
-			//문제 안풀기
-			pass_problem[it - problems.begin()] = true;
-			dp[next_alp][next_cop] = ::min(find_min_cost(next_alp, next_cop, next_cost, problems),
-				dp[next_alp][next_cop]);
-			pass_problem[it - problems.begin()] = false;
-			//문제 풀기
-			next_alp = ::min(next_alp + (*it)[2], max_alp);
-			next_cop = ::min(next_cop + (*it)[3], max_cop);
-			next_cost += (*it)[4];
-			dp[next_alp][next_cop] = ::min(find_min_cost(next_alp, next_cop, next_cost, problems),
-				dp[next_alp][next_cop]);
+void SolvingProblems(int alp, int cop, const vector<vector<int>>& problems) {
+	//problem[alp_req, cop_req, alp_rwd, cop_rwd. cost]
+	for (auto& problem : problems) {
+		int al_req = problem[0]; int co_req = problem[1];;
+		int pre_alp = alp - problem[2];
+		int pre_cop = cop = problem[3];
+		int cost = problem[4];
+		if (pre_alp >= al_req && pre_cop >= co_req) {
+	/*		alp = ::min(alp, max_alp);
+			cop = ::min(cop, max_cop)*/;
+			dp[alp][cop] = ::min(dp[alp][cop], dp[pre_alp][pre_cop] + cost);
 		}
 	}
 }
-
 int solution(int alp, int cop, vector<vector<int>> problems) {
-
-	::sort(problems.begin(), problems.end(), [](auto& A, auto& B) { return A[0] + A[1] < B[0] + B[1]; });
-
 	max_alp = (*::max_element(problems.begin(), problems.end(), [](auto& A, auto& B) {
 		return A[0] < B[0];  }))[0];
 	max_cop = (*::max_element(problems.begin(), problems.end(), [](auto& A, auto& B) {
 		return A[1] < B[1];  }))[1];
 
-	find_min_cost(alp, cop, 0, problems);
+	dp[alp][cop] = 0;
+
+	for (int a = alp; a <= max_alp ; ++a) {
+		for (int b = cop; b <= max_cop ; ++b) {
+			//공부하기
+			if (a >= 1)
+				dp[a][b] = ::min(dp[a-1][b] + 1, dp[a][b]);
+			if (b >= 1)
+				dp[a][b] = ::min(dp[a][b-1] + 1, dp[a][b]);
+			//문제풀기
+			SolvingProblems(a, b, problems);
+		}
+	}
 
 	return dp[max_alp][max_cop];
 }
