d467886594332bbd91ae211286cefade6a34fa1a
OliveGreenKR <jeongchul0098@gmail.com>
2023-08-03 10:54:35 +0900

    solving 23kakao intership no4 - need to other logic

--- CPP_CodingTest/CPP_CodingTest/Problem.cpp
+++ CPP_CodingTest/CPP_CodingTest/Problem.cpp
@@ -8,60 +8,84 @@
 #ifdef BACK
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
+#include <map>
 
 using namespace std;
-const int INF = 1000'000'000;
-const int MAX_CNT = 150 +1;
-int max_alp = 0, max_cop = 0;
+using path = vector<int>; //{i,j,w}
 
-using cost_type = int;
+const int INF = 10'000'001;
 
-vector<vector<cost_type>> dp(MAX_CNT, vector<cost_type>(MAX_CNT, INF)); 
+void choose_path(const path& path, vector<bool>& avail) {
+    int i = path[0], j = path[1], cost = path[2];
 
-
-int find_min_cost(int alp, int cop, const vector<vector<int>>& problems) {
-
-	if (alp >= max_alp && cop >= max_cop)
-		return 0;
-
-	alp = ::min(max_alp, alp);
-	cop = ::min(max_cop, cop);
-
-	if (dp[alp][cop] != INF)
-		return dp[alp][cop];
-
-	dp[alp][cop] = INF + 1;
-
-	
-	dp[alp][cop] = ::min(dp[alp][cop], find_min_cost(alp+1, cop, problems) + 1);
-	dp[alp][cop] = ::min(dp[alp][cop], find_min_cost(alp, cop+1, problems) + 1);
-
-	//problem :  [alp_req, cop_req, alp_rwd, cop_rwd, cost]
-	for (auto it = problems.begin(); it != problems.end(); ++it) {
-		if ((*it)[0] <= alp && (*it)[1] <= cop)
-			dp[alp][cop] = ::min( dp[alp][cop], find_min_cost(alp + (*it)[2], cop + (*it)[3], problems) + (*it)[4]);
-	}
-
-	return dp[alp][cop];
 }
 
-int solution(int alp, int cop, vector<vector<int>> problems) {
+vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
+ 
+    //인접 리스트 형태로 정리 adj[i] = 'i 와 인접한 노드들의 vector'
+    //paths => prioirty_queue 사용
+    //선택된 집합 vs 선택된 집합과 인접한 정점
+    static vector<bool> available(n+1, false);
+    //static map<int,vector<int>> adj;
+    static vector<vector<int>> adj(n+1);
 
-	max_alp = (*::max_element(problems.begin(), problems.end(), [](auto& A, auto& B) {
-		return A[0] < B[0];  }))[0];
-	max_cop = (*::max_element(problems.begin(), problems.end(), [](auto& A, auto& B) {
-		return A[1] < B[1];  }))[1];
+    auto greater_path = [](const path& A, const path& B)->bool{ return A[2] > B[2]; };
+    static priority_queue<path,vector<path>, decltype(greater_path)> pq(greater_path);
 
-	return find_min_cost(alp, cop, problems);
+
+    /*
+    1. pq 만들기
+    2. 선택됨 정점과 인접한 정점 검색
+    3.
+    */
+    for (path& p : paths) {
+        pq.push(p);
+        adj[p[0]].push_back(p[1]);
+        adj[p[1]].push_back(p[0]);
+    }
+
+    path now;
+    int cost;
+    bool isGate = false , isSummit = false;
+
+    while (!pq.empty()) {
+        now = pq.top();
+        pq.pop();
+
+        cost = now[2];
+
+
+
+    }
+
+    /*
+    now  = pq.pop();
+    if( now == 현재 인접 정점이 아님)
+        continue;
+    
+    chosen.push(now);
+    cost =  now.cost;
+
+    if ( now  in summits )
+        summit = ::min(summit, now)
+    if ( now in gates)
+        gate = true
+
+    if( gate == true && summit != INF )
+        return {summit, cost}
+
+    */
+    return { 0,0 };
 }
-
 #endif 
 
 #include <iostream>
 
 int main() {
-	//vector<vector<int>> problems = { {10,15,2,1,2},{20,20,3,3,4} };
-	vector<vector<int>> problems = { {0, 0, 2, 1, 2},{4, 5, 3, 1, 2},{4, 11, 4, 0, 2},{10, 4, 0, 4, 2} };
-	cout << solution(0, 0, problems);
+    vector<vector<int>> paths = { {3, 4, 4} ,{4, 5, 3},{4, 6, 1},{5, 6, 1} };
+    vector<int> gates = { 1,3 };
+    vector<int> summits = { 5 };
+
+    solution(6, paths, gates, summits);
 }
