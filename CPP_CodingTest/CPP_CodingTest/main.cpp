ac1dcdf652bc8e2a24b8f2d7f2bf90918bcf62bf
OliveGreenKR <jeongchul0098@gmail.com>
2024-10-21 15:10:39 +0900

    HackerRank-  1 Week Preparation Kit Day4 - Brides counting

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -1,11 +1,39 @@
-#pragma warning(disable: 4996)//printf, scanf
-#include <fstream>
-#include <iostream>
-#define M_PI (3.14159265358979323846)
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <cctype>
+#include <locale>
+#include <unordered_map>
 
+using namespace std;
+
+void minimumBribes(vector<int> q) {
+    int n = q.size();
+    int brideSum = 0;
+    for (int now = n - 1; now >= 0; now--)
+    {
+        int expectedPos = q[now];
+        int initPos = now + 1;
+
+        if (expectedPos - initPos > 2)
+        {
+            cout << "Too chaotic" << endl;
+            return;
+        }
+
+        for (int possiblePos = std::max(0, expectedPos - 2); possiblePos < now; possiblePos++)
+        {
+            //through me
+            if (q[possiblePos] > expectedPos)
+                brideSum++;
+        }
+    }
+    cout << brideSum << endl;
+}
 
 int main() {
+
     
 }
