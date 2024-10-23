cab34a71e6cc8d082c40deab6fcf335fbaec3fec
OliveGreenKR <jeongchul0098@gmail.com>
2024-10-23 13:57:50 +0900

    ..

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -4,28 +4,31 @@
 #include <algorithm>
 #include <functional>
 #include <cctype>
+#include <set>
 #include <locale>
 #include <unordered_map>
 
 using namespace std;
 
-
-string twoArrays(int k, vector<int> A, vector<int> B) {
-    
-    std::sort(A.begin(), A.end());
-    std::sort(B.begin(), B.end(), std::greater<int>());
-
-    for (int i = 0; i < A.size(); ++i)
-    {
-        if (A[i] + B[i] < k)
-            return "NO";
-    }
-
-    return "YES";
+int circularRange(int start, int end, int value) {
+    int range = end - start + 1;
+    return start + (value - start) % range;
 }
 
+int maxMin(int k, vector<int> arr) {
+    //k개를 원소로 가지는 부분배열에서 최대-최소값의 차이의 최솟값.
+    set<int> dtas(arr);
+    if(dtas.size() > 2)
+        return dtas[0] - dtas[1];
+    else
+    {
+        return 0;
+    }
+}
 
 int main() {
 
-    
+    //1...2,3,,4,5,,,6,7,,,en
+    // 1...2
+    // 1..2,3,, 4
 }
