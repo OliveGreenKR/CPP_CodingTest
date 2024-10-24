e2c94b323e12b275166a92f5114320a1e0238c74
OliveGreenKR <jeongchul0098@gmail.com>
2024-10-24 14:15:10 +0900

    dense ranking

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -5,6 +5,7 @@
 #include <functional>
 #include <cctype>
 #include <set>
+#include <map>
 #include <locale>
 #include <unordered_map>
 
@@ -17,37 +18,30 @@ int circularRange(int start, int end, int value) {
 }
 
 // Function to check if a string is valid by removing at most one character
-string isValid(string s) {
-    int freq[26] = {0};
-    for (char c : s) {
-        freq[c - 'a']++;
+vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
+
+    ranked.erase(std::unique(ranked.begin(), ranked.end()), ranked.end());
+
+    std::vector<int> result;
+    int n = ranked.size();
+
+    for (int p : player)
+    {
+        auto it = ::lower_bound(ranked.begin(), ranked.end(),p,::greater<int>());
+
+        int rank = it - ranked.begin() + 1;
+        result.push_back(it -ranked.begin() +1);
     }
 
-    unordered_map<int, int> freqCount;
-    for (int i = 0; i < 26; i++) {
-        if (freq[i] > 0) {
-            freqCount[freq[i]]++;
-        }
-    }
-
-    if (freqCount.size() == 1) {
-        return "YES";
-    } else if (freqCount.size() == 2) {
-        auto it = freqCount.begin();
-        int f1 = it->first, c1 = it->second;
-        it++;
-        int f2 = it->first, c2 = it->second;
-
-        if ((c1 == 1 && (f1 - 1 == f2 || f1 - 1 == 0)) || (c2 == 1 && (f2 - 1 == f1 || f2 - 1 == 0))) {
-            return "YES";
-        }
-    }
-
-    return "NO";
+    return result;
 }
 
+
+
 int main() {
-    string s;
-    cin >> s;
-    cout << isValid(s) << endl;
+    vector<int> ranked = { 100,90,90,80 };
+    vector<int> player = { 70,80,105};
+
+    climbingLeaderboard(ranked, player);
+
 }
