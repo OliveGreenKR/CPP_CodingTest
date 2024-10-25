5e16f8f6a0d6fc82a72bd0ef9a4e9ebf5779d600
OliveGreenKR <jeongchul0098@gmail.com>
2024-10-25 11:00:17 +0900

    Equal Stacks - less memory

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -35,26 +35,38 @@ unordered_set<int> getPartialSumSet(vector<int>& target)
 }
 
 
-int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
+#include <vector>
+#include <numeric> // for accumulate
 
-    //n번까지의 부분합
-    auto set1 = getPartialSumSet(h1);
-    auto set2 = getPartialSumSet(h2);
-    auto set3 = getPartialSumSet(h3);
+// Function to find the maximum possible height where all stacks have equal height
+int equalStacks(std::vector<int> h1, std::vector<int> h2, std::vector<int> h3) {
+    // Calculate the total height of each stack
+    int height1 = std::accumulate(h1.begin(), h1.end(), 0);
+    int height2 = std::accumulate(h2.begin(), h2.end(), 0);
+    int height3 = std::accumulate(h3.begin(), h3.end(), 0);
 
-    //3 집합의 공통값중 가장 큰 것 찾기.
-    int largestCommon = -1; // Assuming all elements are positive
+    int idx1 = 0, idx2 = 0, idx3 = 0;
 
-    for (const int& elem : set1)
+    // While the heights of the stacks are not equal, keep removing cylinders from the tallest stack
+    while (!(height1 == height2 && height2 == height3))
     {
-        if (set2.find(elem) != set2.end() && set3.find(elem) != set3.end())
+        if (height1 >= height2 && height1 >= height3)
         {
-            largestCommon = max(largestCommon, elem);
+            height1 -= h1[idx1++];
+        }
+        else if (height2 >= height1 && height2 >= height3)
+        {
+            height2 -= h2[idx2++];
+        }
+        else
+        {
+            height3 -= h3[idx3++];
         }
     }
 
-    return largestCommon < 0 ? 0 : largestCommon;
+    return height1; // or height2 or height3, they should all be equal at this point
 }
+
 
 
 
