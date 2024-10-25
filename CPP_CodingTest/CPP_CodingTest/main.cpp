61f275f2e5d9eabd28be2157d00a3e69f39eb005
OliveGreenKR <jeongchul0098@gmail.com>
2024-10-25 10:57:17 +0900

    Equal Stacks : 누적합

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -8,8 +8,12 @@
 #include <map>
 #include <locale>
 #include <unordered_map>
+#include <unordered_set>
+#include <numeric>
 
 using namespace std;
+
+#define OUT
 
 // Function to calculate circular range
 int circularRange(int start, int end, int value) {
@@ -17,31 +21,48 @@ int circularRange(int start, int end, int value) {
     return start + (value - start) % range;
 }
 
-// Function to check if a string is valid by removing at most one character
-vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
-
-    ranked.erase(std::unique(ranked.begin(), ranked.end()), ranked.end());
-
-    std::vector<int> result;
-    int n = ranked.size();
-
-    for (int p : player)
+unordered_set<int> getPartialSumSet(vector<int>& target)
+{
+    unordered_set<int> result;
+    int sum = 0;
+    for(auto it = target.rbegin(); it != target.rend(); ++it)
     {
-        auto it = ::lower_bound(ranked.begin(), ranked.end(),p,::greater<int>());
-
-        int rank = it - ranked.begin() + 1;
-        result.push_back(it -ranked.begin() +1);
+        sum += *it;
+        result.insert(sum);
     }
 
     return result;
 }
 
 
+int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
+
+    //n번까지의 부분합
+    auto set1 = getPartialSumSet(h1);
+    auto set2 = getPartialSumSet(h2);
+    auto set3 = getPartialSumSet(h3);
+
+    //3 집합의 공통값중 가장 큰 것 찾기.
+    int largestCommon = -1; // Assuming all elements are positive
+
+    for (const int& elem : set1)
+    {
+        if (set2.find(elem) != set2.end() && set3.find(elem) != set3.end())
+        {
+            largestCommon = max(largestCommon, elem);
+        }
+    }
+
+    return largestCommon < 0 ? 0 : largestCommon;
+}
+
+
 
 int main() {
-    vector<int> ranked = { 100,90,90,80 };
-    vector<int> player = { 70,80,105};
 
-    climbingLeaderboard(ranked, player);
+    vector<int> h1 = { 3,2,1,1,1 };
+    vector<int> h2 = { 4,3,2 };
+    vector<int> h3 = { 1,1,4,1};
 
+    cout << equalStacks(h1, h2, h3);
 }
