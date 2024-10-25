dd3c9a63460897de05ceb500fd25651693ff3ff4
OliveGreenKR <jeongchul0098@gmail.com>
2024-10-25 14:45:42 +0900

    Dp- Lego Blocks

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -15,66 +15,78 @@ using namespace std;
 
 #define OUT
 
-// Function to calculate circular range
-int circularRange(int start, int end, int value) {
-    int range = end - start + 1;
-    return start + (value - start) % range;
+constexpr const int MOD = (1e9 + 7);
+
+// Function to calculate the number of ways to build one row of width m
+long long countWaysToBuildRow(int width, std::vector<long long>& memo) {
+
+    //Base State
+    if (width == 0) return 1;
+    if (width < 0) return 0;
+
+    // Return the stored result if already calculated
+    if (memo[width] != -1)
+        return memo[width];
+
+    // Recursive case: try placing bricks of width 1, 2, 3, or 4
+    long long ways = countWaysToBuildRow(width - 1, memo) % MOD +
+        countWaysToBuildRow(width - 2, memo) % MOD +
+        countWaysToBuildRow(width - 3, memo) % MOD +
+        countWaysToBuildRow(width - 4, memo) % MOD;
+    ways %= MOD;
+
+    // Store Results
+    memo[width] = ways;
+    return ways;
 }
 
-unordered_set<int> getPartialSumSet(vector<int>& target)
-{
-    unordered_set<int> result;
-    int sum = 0;
-    for(auto it = target.rbegin(); it != target.rend(); ++it)
+int modExp(long long base, int exp, int mod) {
+    long long result = 1;
+    while (exp > 0)
     {
-        sum += *it;
-        result.insert(sum);
+        if (exp % 2 == 1)
+        {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp /= 2;
     }
-
     return result;
 }
 
+int legoBlocks(int n, int m) {
 
-#include <vector>
-#include <numeric> // for accumulate
+    vector<long long> OneRow(m + 1, -1);
+    countWaysToBuildRow(m, OneRow);
 
-// Function to find the maximum possible height where all stacks have equal height
-int equalStacks(std::vector<int> h1, std::vector<int> h2, std::vector<int> h3) {
-    // Calculate the total height of each stack
-    int height1 = std::accumulate(h1.begin(), h1.end(), 0);
-    int height2 = std::accumulate(h2.begin(), h2.end(), 0);
-    int height3 = std::accumulate(h3.begin(), h3.end(), 0);
+    vector<long long> NRows(m + 1, 0);
 
-    int idx1 = 0, idx2 = 0, idx3 = 0;
-
-    // While the heights of the stacks are not equal, keep removing cylinders from the tallest stack
-    while (!(height1 == height2 && height2 == height3))
+    //Count all ways in height 'n'
+    for (int i = 1; i <= m; i++)
     {
-        if (height1 >= height2 && height1 >= height3)
+        NRows[i] = modExp(OneRow[i], n, MOD);
+    }
+
+    //Selection Valid Wall
+    vector<long long> validWalls(m + 1, 0);
+    validWalls[0] = 1;
+    for (int i = 1; i <= m; i++)
+    {
+        validWalls[i] = NRows[i];
+        for (int j = 1; j < i; j++)
         {
-            height1 -= h1[idx1++];
-        }
-        else if (height2 >= height1 && height2 >= height3)
-        {
-            height2 -= h2[idx2++];
-        }
-        else
-        {
-            height3 -= h3[idx3++];
+            validWalls[i] = (validWalls[i] - (validWalls[j] * NRows[i - j]) % MOD + MOD) % MOD;
         }
     }
 
-    return height1; // or height2 or height3, they should all be equal at this point
+    return validWalls[m];
+
 }
-
-
-
-
 int main() {
 
-    vector<int> h1 = { 3,2,1,1,1 };
-    vector<int> h2 = { 4,3,2 };
-    vector<int> h3 = { 1,1,4,1};
+    cout << legoBlocks(2, 2) << endl;
+    cout << legoBlocks(3, 2) << endl;
+    cout << legoBlocks(4, 4) << endl;
 
-    cout << equalStacks(h1, h2, h3);
+ 
 }
