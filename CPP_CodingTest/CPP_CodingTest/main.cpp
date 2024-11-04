c4ac08cb0f9e19ef58a6254e2e8a9fc0f38399d2
OliveGreenKR <jeongchul0098@gmail.com>
2024-11-04 10:57:13 +0900

    3D Surface Area

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -20,37 +20,57 @@
 
 using namespace std;
 
-string gridSearch(vector<string> G, vector<string> P) {
-    int R = G.size();    // Grid rows
-    int C = G[0].size(); // Grid columns
-    int r = P.size();    // Pattern rows
-    int c = P[0].size(); // Pattern columns
+int surfaceArea(vector<vector<int>> A) {
+	//입체 평면의 표면적 계산하기
 
-    //2D sliding window 
+	//3D 좌표는 (i,j,A[[i][j])
+	
+	//최소길이는 1 이므로
+	//전체 도형의 2면적의 표면적은 고정 (H*W)
+		//측면 표면적만 계산
+			//각셀의 측면을 인접셀의 높이와 비하여 노출된 부분만 계산.
 
-    //Find the possible starting point in the grid.
-    for (int i = 0; i <= R - r; i++)
-    {
-        for (int j = 0; j <= C - c; j++)
+    const int H = A.size();
+    const int W = A[0].size();
+
+    int totalSurfaceArea = 0;
+
+    //Upper, Lower Side
+    totalSurfaceArea += 2 * H * W;
+
+    auto GetCellHeight = [&A, H, W](const int i, const int j)
         {
-
-            bool match = true;
-            //check Pattern matches with  at grid[i][j]
-            for (int x = 0; x < r; x++)
+            if (i < 0 || i >= H || j < 0 || j >= W)
             {
-                if (G[i + x].substr(j, c) != P[x])
-                {
-                    match = false;
-                    break;
-                }
+                return 0;
             }
-            //whole pattern found
-            if (match)
-                return "YES";
+            else
+            {
+                return A[i][j];
+            }
+        };
+
+    //check the Side sufaces.
+    for (int i = 0; i < H; ++i)
+    {
+        for (int j = 0; j < W; ++j)
+        {
+            const int current = GetCellHeight(i, j);
+            //North 
+            totalSurfaceArea += ::max(0, current - GetCellHeight(i - 1, j));
+            //East 
+            totalSurfaceArea += ::max(0, current - GetCellHeight(i, j + 1));
+            //South
+            totalSurfaceArea += ::max(0, current - GetCellHeight(i + 1, j));
+            //West 
+            totalSurfaceArea += ::max(0, current - GetCellHeight(i, j - 1));
         }
     }
-    return "NO";
+
+    return totalSurfaceArea;
+
 }
+
 int main() {
 
 }
