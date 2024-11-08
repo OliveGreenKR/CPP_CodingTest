67cc2507c9f4b1c006bc01fc269b0b1059ec036e
OliveGreenKR <jeongchul0098@gmail.com>
2024-11-08 14:34:29 +0900

    Maximum Palindrome but time Limits some cases.

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -20,56 +20,84 @@
 
 using namespace std;
 
-string highestValuePalindrome(string& s, int n, int k) {
-    //make largest palindrome in k changes.
+constexpr int MOD = 1e9 + 7;
+constexpr int MAXN = 1e5;
 
-    vector<int> needed(n, 0); //needed chages to make Largest palindrome
+std::vector<long long> factorial(MAXN + 1);
+std::vector<long long> invFactorial(MAXN + 1);
 
-    //odd string, center record
-    if (n % 2 == 1)
-        needed[n / 2] = 1;
-
-    //needed record
-    for (int i = 0; i < n / 2; ++i)
+// Function to calculate x^y % MOD in O(log y)
+long long modPow(long long x, long long y, int mod) {
+    long long result = 1;
+    while (y > 0)
     {
-        int first = s[i] - '0';
-        int second = s[n - i - 1] - '0';
-
-        //already pair
-        if (first == second)
+        if (y % 2 == 1)
         {
-            needed[i] = 2;
+            result = (result * x) % mod;
         }
-        else //not pair
-        {
-            s[i] = s[n - i - 1] = max(first + '0', second + '0');
-            --k;
-            needed[i] = 1;
-        }
+        x = (x * x) % mod;
+        y /= 2;
     }
-
-    if (k < 0)
-    {
-        return "-1";
-    }
-
-    //to make large, greedy choice
-    int i = 0;
-    while (k > 0 && i <= n / 2)
-    {
-        if (k >= needed[i] && s[i] != '9')
-        {
-            k -= needed[i];
-            s[i] = s[n - 1 - i] = '9';
-        }
-        ++i;
-    }
-    return s;
-
+    return result;
 }
-int main() {
 
-    string s = "abcdefghhgfedecba";
-    cout << isValid(s);
-    return 0;
+// Function to precompute factorials and inverse factorials
+void precomputeFactorials() {
+    factorial[0] = 1;
+    factorial[1] = 1;
+    for (int i = 1; i <= MAXN; ++i)
+    {
+        factorial[i] = (factorial[i - 1] * i) % MOD;
+    }
+    invFactorial[MAXN] = modPow(factorial[MAXN], MOD - 2, MOD);
+    for (int i = MAXN - 1; i >= 0; --i)
+    {
+        invFactorial[i] = (invFactorial[i + 1] * (i + 1)) % MOD;
+    }
+}
+
+int answerQuery(const string s, int l, int r) {
+    // Return the answer for this query modulo 1000000007.
+    // This function is called once before all queries.
+    //get modulo the number of 'maximum-length palindromes' in substring(s[l,r])
+
+    //count alphabet
+    vector<int> freq(26, 0);
+
+    for (int i = l - 1; i < r; i++)
+    {
+        freq[s[i] - 'a']++;
+    }
+    int Odd = 0;
+    int possibles = 0;
+    //count Odd freq
+    for (const int count : freq)
+    {
+        Odd += count % 2;
+        possibles += count / 2;
+    }
+
+    //count the number of cases in MAX_LENGTH.
+    long long cases;
+    cases = factorial[possibles];
+
+    for (const int count : freq)
+    {
+        if (count / 2 > 1)
+        {
+            cases = (cases * invFactorial[count / 2]) % MOD;
+        }
+    }
+
+    if (Odd > 0)
+        cases = (cases * Odd) % MOD;
+
+    return cases;
+}
+
+
+int main() {
+    precomputeFactorials();
+    string s = "cool";
+    cout << answerQuery(s, 1, 4);
 }
