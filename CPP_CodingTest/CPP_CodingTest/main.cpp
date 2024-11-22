eecbb97cbc2a647e6bd2327279a451b3ec9f4615
OliveGreenKR <jeongchul0098@gmail.com>
2024-11-22 14:11:38 +0900

    Beaturiful Quarduraples solved

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -30,57 +30,60 @@ using namespace std;
 #include <string>
 using namespace std;
 
-
-//toto 첫번째 조건을 위해서는
-    // not (모두 같은 수 ||  2개씩 같은 수) 이어야함...
-        //3 번째 까지는 일반적인 오름차순 선택
-            //4번째에서 2쌍 수를 제거해야함.
-
-//정상적인 오름차순 -  [모두 같은 수 = A ]  - [2쌍]
-
-//2쌍수 는 오름차순이기에
-    //AACC, AABB만 가능
-//즉 제거해야하는 경우
-
-int solution(int n, int m, vector<vector<int>>& dp, vector<int>&p)
-{
-    if (n < 0 || n > 3 || m < 1) 
-        return 0;
-
-    if (m > p[n])
-        dp[n][m] = 0;
-
-    if (dp[n][m] != -1)
-        return dp[n][m];
-
-    dp[n][m] = 0;
-
-    for (int i = 1; i <= m; ++i)
-    {
-        dp[n][m] += solution(n - 1, i, dp,p);
-    }
-
-    return dp[n][m];
-}
-
-int beautifulQuadruples(int a, int b, int c, int d) {
+long long beautifulQuadruples(int a, int b, int c, int d) {
     vector<int> p = { a,b,c,d };
     sort(p.begin(), p.end());
 
-    vector<vector<int>> dp(4, vector<int>(3000 + 1, -1));
-    for (int i = 1; i <= p[0]; ++i)
+    const int MAX = 4096;
+    vector<int> cnt(MAX, 0);
+    long long allCount = 0;
+
+    for (int z = 1; z <= p[2]; ++z)
     {
-        dp[0][i] = 1;
+        for (int w = z; w <= p[3]; ++w)
+        {
+            ++cnt[z ^ w];
+            ++allCount;
+        }
     }
 
-    int result = 0;
-    for (int m = 1; m <= p[3]; ++m)
-        result += solution(3, m, dp, p);
+    long long result = 0;
 
-    //for BitWise condition
-    //[AA][CC]  , when [C] is lower than 'C'
-    return result - p[0]*p[2];
+    for (int y = 1; y <= p[1]; ++y)
+    {
+        for (int x = 1; x <= min(p[0], y); ++x)
+        {
+            result += allCount - cnt[x ^ y];
+        }
+        for (int w = y; w <= p[3]; ++w)
+        {
+            --cnt[y ^ w];
+            --allCount;
+        }
+    }
 
+    return result;
+}
+
+long long countAllPermutations(int a, int b, int c, int d) {
+    vector<int> p = { a,b,c,d };
+    sort(p.begin(), p.end());
+
+    long long allCount = 0;
+
+    for (int x = 1; x <= p[0]; x++)
+    {
+        for (int y = x; y <= p[1]; y++)
+        {
+            int minZ = y;   
+            int maxZ = p[2];   
+
+            allCount += ((long long)(maxZ - minZ + 1) * (p[3] - minZ + 1) -
+                      ((long long)(maxZ - minZ + 1) * (maxZ - minZ)) / 2);
+        }
+    }
+
+    return allCount;
 }
 
 int main() {
@@ -99,8 +102,15 @@ ofstream outputFile("./output.txt"); // 출력 파일 열기
     }
 #pragma endregion
 
+
+    // 마지막 위치(3)부터 시작
+
+    //cout << beautifulQuadruples(360, 896, 1927, 2047);
+    cout << beautifulQuadruples(360, 896, 1927, 2047) << endl;
     cout << beautifulQuadruples(1, 2, 3, 4);
-    /*Do Something*/
+
+    //2047 1927 360 896    =>   301271960180
+    // 전체 종류 수 : 301438994250   // 301438300530
 
 #pragma region Close
     inputFile.close(); // 입력 파일 닫기
