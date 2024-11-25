0bf2fcc79847dae1282b1a6053147429ec2044a7
OliveGreenKR <jeongchul0098@gmail.com>
2024-11-25 10:27:33 +0900

    Red Knight's shortest Path

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -7,7 +7,6 @@
 #include <set>
 #include <map>
 #include <locale>
-#include <queue>
 #include <unordered_map>
 #include <unordered_set>
 #include <numeric>
@@ -18,80 +17,80 @@
 #include <fstream> // 파일 입출력을 위한 헤더 추가
 #include <bitset>
 #include <sstream>
+#include <stack>
+#include <queue>
 
 #define OUT
 
 using namespace std;
 
+void printShortestPath(const int n, int i_start, int j_start, int i_end, int j_end) {
 
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <string>
-using namespace std;
+    using Vector2 = pair<int, int>;
+    const vector<Vector2> directions = { {-2,-1},{-2,1},{0,2},{2,1},{2,-1},{0,-2} };
+    const vector<string>  directionStrings = { "UL", "UR", "R", "LR", "LL", "L" };
 
-long long beautifulQuadruples(int a, int b, int c, int d) {
-    vector<int> p = { a,b,c,d };
-    sort(p.begin(), p.end());
+    vector<vector<int>> dirRecord(n, vector<int>(n, -1)); //record direction's index
 
-    const int MAX = 4096;
-    vector<int> cnt(MAX, 0);
+    queue<Vector2> q;
 
-    //the number of {ZW}
-    long long countZW = 0;
+    q.push({ i_start, j_start });
 
-
-    //all possible {ZW} and recore Z^W
-    for (int z = 1; z <= p[2]; ++z)
+    while (!q.empty())
     {
-        for (int w = z; w <= p[3]; ++w)
+        auto [x, y] = q.front();
+        q.pop();
+
+        if (x == i_end && y == j_end)
         {
-            ++cnt[z ^ w];
-            ++countZW;
+            break;
+        }
+
+        for (int i = 0; i < directions.size(); ++i)
+        {
+            int nx = x + directions[i].first;
+            int ny = y + directions[i].second;
+
+            //visit check
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && dirRecord[nx][ny] == -1)
+            {
+                q.push({ nx, ny });
+                dirRecord[nx][ny] = i;  //record directions
+            }
         }
     }
 
-    long long result = 0;
-
-    for (int y = 1; y <= p[1]; ++y)
+    if (dirRecord[i_end][j_end] == -1)
     {
-        for (int x = 1; x <= min(p[0], y); ++x)
-        {
-            //answer += {ZW} - {ZW | Z^W==X^Y}
-            result += countZW - cnt[x ^ y];
-        }
-        //update possible{ZW} for next y
-            // Delete pairs that were possible with the current y
-        for (int w = y; w <= p[3]; ++w)
-        {
-            --cnt[y ^ w];
-            --countZW;
-        }
+        cout << "Impossible" << endl;
+        return;
+    }
+       
+	
+    Vector2 now(i_end, j_end);
+    Vector2 start(i_start, j_start);
+    stack<string> shortest_path;
+    //backtrack
+    int cost = 0;
+    while (now != start)
+    {
+        cost++;
+        int direcIdx = dirRecord[now.first][now.second];
+        shortest_path.push(directionStrings[direcIdx]);
+        now.first -= directions[direcIdx].first;
+        now.second -= directions[direcIdx].second;
     }
 
-    return result;
+
+    cout << cost << "\n";
+    while (!shortest_path.empty())
+    {
+        cout << shortest_path.top() << " ";
+        shortest_path.pop();
+    }
+    cout << endl;
 }
 
-long long countAllPermutations(int a, int b, int c, int d) {
-    vector<int> p = { a,b,c,d };
-    sort(p.begin(), p.end());
-
-    long long allCount = 0;
-
-    for (int x = 1; x <= p[0]; x++)
-    {
-        for (int y = x; y <= p[1]; y++)
-        {
-            int minZ = y;   
-            int maxZ = p[2];   
-
-            allCount += ((long long)(maxZ - minZ + 1) * (p[3] - minZ + 1) -
-                      ((long long)(maxZ - minZ + 1) * (maxZ - minZ)) / 2);
-        }
-    }
-
-    return allCount;
-}
 
 int main() {
  ifstream inputFile("./input.txt"); // 입력 파일 열기
@@ -109,15 +108,10 @@ ofstream outputFile("./output.txt"); // 출력 파일 열기
     }
 #pragma endregion
 
-
-    // 마지막 위치(3)부터 시작
-
-    //cout << beautifulQuadruples(360, 896, 1927, 2047);
-    cout << beautifulQuadruples(360, 896, 1927, 2047) << endl;
-    cout << beautifulQuadruples(1, 2, 3, 4);
-
-    //2047 1927 360 896    =>   301271960180
-    // 전체 종류 수 : 301438994250   // 301438300530
+    printShortestPath(7, 6, 6, 0, 1); //UL UL UL L
+    printShortestPath(6, 5, 1, 0, 5); //impossible
+    printShortestPath(7, 0, 3, 4, 3); //LR LL
+    
 
 #pragma region Close
     inputFile.close(); // 입력 파일 닫기
