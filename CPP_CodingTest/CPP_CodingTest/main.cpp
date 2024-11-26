0710e2c6be0f273bec703649c7f9a4aaf176d226
OliveGreenKR <jeongchul0098@gmail.com>
2024-11-26 11:12:51 +0900

    Synchronous Shopping solved : Dijikstra + Bitmask + dp

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -25,27 +25,25 @@
 using namespace std;
 
 int shop(int n, int k, vector<string> centers, vector<vector<int>> roads) {
-    const int INF = INT32_MAX;
+    const int INF = 1e9;
     vector<vector<pair<int, int>>> graph(n + 1);
     vector<int> fish_at_node(n + 1, 0);
 
-    // Bitmask for fishState
+    //fishState for Bitmask
     for (int i = 1; i <= n; i++)
     {
         stringstream ss(centers[i - 1]);
         int num_types;
-        ss >> num_types;  // # of fish Type 
-
-        // fish Types
+        ss >> num_types;
         for (int j = 0; j < num_types; j++)
         {
             int fish;
             ss >> fish;
-            fish_at_node[i] |= (1 << (fish - 1));  // 0-based Bitmask
+            fish_at_node[i] |= (1 << (fish - 1));
         }
     }
 
-    // Construct Graph - adj map
+    // Construct grpah : adj map
     for (const auto& road : roads)
     {
         int u = road[0], v = road[1], w = road[2];
@@ -56,60 +54,54 @@ int shop(int n, int k, vector<string> centers, vector<vector<int>> roads) {
     // dp[node][fish_mask] = minimum time to reach node with fish_mask
     vector<vector<int>> dp(n + 1, vector<int>(1 << k, INF));
 
-    // start point
+    //Dijikstra - {cost {node,fishState}}
+    priority_queue<pair<int, pair<int, int>>,
+        vector<pair<int, pair<int, int>>>,
+        greater<>> pq;
+
+    // start Point
     dp[1][fish_at_node[1]] = 0;
+    pq.push({ 0, {1, fish_at_node[1]} });  // {cost {node,fishState}}
 
-    //BFS
-    queue<int> nodesToVisit;
-    nodesToVisit.push(1);
 
-    // minumum time for each node in all possible fish State
-    while (!nodesToVisit.empty())
+    while (!pq.empty())
     {
-        int curr = nodesToVisit.front();
-        nodesToVisit.pop();
+        auto [curr_time, p] = pq.top();
+        auto [curr, mask] = p;
+        pq.pop();
 
-        //current node's  all FishState 
-        for (int mask = 0; mask < (1 << k); mask++)
+        if (curr_time > dp[curr][mask]) continue;
+
+        for (auto [next, time] : graph[curr])
         {
-            if (dp[curr][mask] == INF) continue;
+            int new_mask = mask | fish_at_node[next];
+            int new_time = curr_time + time;
 
-            //find Next
-            for (auto [next, time] : graph[curr])
+            if (dp[next][new_mask] > new_time)
             {
-                //visit
-                int new_mask = mask | fish_at_node[next];
-
-                // update minimum time for Next
-                if (dp[next][new_mask] > dp[curr][mask] + time)
-                {
-                    dp[next][new_mask] = dp[curr][mask] + time;
-                    nodesToVisit.push(next);
-                }
+                dp[next][new_mask] = new_time;
+                pq.push({ new_time, {next, new_mask} });
             }
         }
     }
 
-    // Find all Possible fish State pairs in 'n'
-    int answer = INF;
-    int all_fish = (1 << k) - 1;  //target fish state
+    //target State
+    int all_fish = (1 << k) - 1;
 
+    //find minimum pairs for target State
+    int answer = INF;
     for (int mask1 = 0; mask1 < (1 << k); mask1++)
     {
         if (dp[n][mask1] == INF) continue;
-
         for (int mask2 = 0; mask2 < (1 << k); mask2++)
         {
             if (dp[n][mask2] == INF) continue;
-
-            //check all fish state
             if ((mask1 | mask2) == all_fish)
             {
                 answer = min(answer, max(dp[n][mask1], dp[n][mask2]));
             }
         }
     }
-
     return answer == INF ? -1 : answer;
 }
 
