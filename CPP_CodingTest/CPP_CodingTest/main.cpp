16bf4d06dd9ca34fa329a67136eeb9e99b0c847e
OliveGreenKR <jeongchul0098@gmail.com>
2024-11-27 09:46:21 +0900

    BFS :  ShortestPath solved

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -24,85 +24,57 @@
 
 using namespace std;
 
-int shop(int n, int k, vector<string> centers, vector<vector<int>> roads) {
-    const int INF = 1e9;
-    vector<vector<pair<int, int>>> graph(n + 1);
-    vector<int> fish_at_node(n + 1, 0);
-
-    //fishState for Bitmask
-    for (int i = 1; i <= n; i++)
+vector<int> bfs(const int n, const int m, const vector<vector<int>>& edges, const int s) {
+    //build graph - make adj map
+    vector<vector<int>> adj(n);
+    for (auto& edge : edges)
     {
-        stringstream ss(centers[i - 1]);
-        int num_types;
-        ss >> num_types;
-        for (int j = 0; j < num_types; j++)
-        {
-            int fish;
-            ss >> fish;
-            fish_at_node[i] |= (1 << (fish - 1));
-        }
+        adj[edge[0] - 1].push_back(edge[1] - 1); //zero based index
+        adj[edge[1] - 1].push_back(edge[0] - 1);
     }
 
-    // Construct grpah : adj map
-    for (const auto& road : roads)
+    //BFS search for all nodes
+    vector<int> dist(n, -1); //shortest distance from s
+    queue<pair<int, int>> q; //{node,dist}
+    //start point
+    dist[s - 1] = 0;
+    q.push({ s - 1,0 });
+
+    while (!q.empty())
     {
-        int u = road[0], v = road[1], w = road[2];
-        graph[u].push_back({ v, w });
-        graph[v].push_back({ u, w });
-    }
+        auto [now, cost] = q.front();
+        q.pop();
 
-    // dp[node][fish_mask] = minimum time to reach node with fish_mask
-    vector<vector<int>> dp(n + 1, vector<int>(1 << k, INF));
-
-    //Dijikstra - {cost {node,fishState}}
-    priority_queue<pair<int, pair<int, int>>,
-        vector<pair<int, pair<int, int>>>,
-        greater<>> pq;
-
-    // start Point
-    dp[1][fish_at_node[1]] = 0;
-    pq.push({ 0, {1, fish_at_node[1]} });  // {cost {node,fishState}}
-
-
-    while (!pq.empty())
-    {
-        auto [curr_time, p] = pq.top();
-        auto [curr, mask] = p;
-        pq.pop();
-
-        if (curr_time > dp[curr][mask]) continue;
-
-        for (auto [next, time] : graph[curr])
+        for (const int next : adj[now])
         {
-            int new_mask = mask | fish_at_node[next];
-            int new_time = curr_time + time;
-
-            if (dp[next][new_mask] > new_time)
+            //find unvisited next
+            if (dist[next] == -1)
             {
-                dp[next][new_mask] = new_time;
-                pq.push({ new_time, {next, new_mask} });
+                dist[next] = cost + 6;
+                q.push({ next,cost + 6 });
             }
         }
     }
 
-    //target State
-    int all_fish = (1 << k) - 1;
-
-    //find minimum pairs for target State
-    int answer = INF;
-    for (int mask1 = 0; mask1 < (1 << k); mask1++)
+    //return result
+        //remove start
+    vector<int> result(n - 1);
+    for (int i = 0; i < n; ++i)
     {
-        if (dp[n][mask1] == INF) continue;
-        for (int mask2 = 0; mask2 < (1 << k); mask2++)
+        if (i == s - 1)
         {
-            if (dp[n][mask2] == INF) continue;
-            if ((mask1 | mask2) == all_fish)
-            {
-                answer = min(answer, max(dp[n][mask1], dp[n][mask2]));
-            }
+            continue;
+        }
+        else if (i < s - 1)
+        {
+            result[i] = dist[i];
+        }
+        else
+        {
+            result[i - 1] = dist[i];
         }
     }
-    return answer == INF ? -1 : answer;
+    return result;
 }
 
 
