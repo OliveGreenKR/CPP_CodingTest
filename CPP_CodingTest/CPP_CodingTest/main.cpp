5ad86740503844c13bb76657e7e6c2898b6b0acc
OliveGreenKR <jeongchul0098@gmail.com>
2024-11-28 17:37:20 +0900

    solving...

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -21,161 +21,158 @@
 #include <queue>
 #include <bitSet>
 
-using namespace std;
 #define OUT
 
-constexpr int MAXN = 1e5;
+using namespace std;
+using GuessMap = vector<unordered_set<int>>;
 
-struct Node
+int findCorrect(int root, vector<bool>& visit, const vector<vector<int>>& adj, const GuessMap& guess)
 {
-    vector<int> children;
-    bitset<MAXN> subTree;
-};
+	visit[root] = true;
+	int correct = 0;
 
-bitset<MAXN> recordSubTree(int root, vector<Node*>& nodes)
-{
-    Node* now = nodes[root];
-    now->subTree.set(root);
+	for (int next : adj[root])
+	{
+		if (visit[next])
+			continue;
 
-    for (const int child : now->children)
-    {
-        now->subTree |= recordSubTree(child, nodes);
-    }
-    int cnt = now->subTree.count();
-    return now->subTree;
-    
+		if (guess[root].count(next)) //guess true
+		{
+			correct++;
+		}
+
+		correct += findCorrect(next, visit, adj, guess);
+	}
+
+	return correct;
 }
 
-void buildTreewithDAG(int root, vector<Node*> nodes, const vector<vector<int>>& adj)
+void recordCorrect(int root, int record, vector<bool>& visit, vector<int>& correct, const vector<vector<int>>& adj, const GuessMap& guess)
 {
-    Node* now = nodes[root];
-    for (const int child : adj[root])
-    {
-        now->children.push_back(child);
-        buildTreewithDAG(child, nodes, adj);
-    }
+	correct[root] = record;
+	visit[root] = true;
+
+	for (int next : adj[root])
+	{
+		if (visit[next])
+			continue;
+
+		int delta = 0;
+		if (guess[root].count(next))
+			delta = -1;
+		if (guess[next].count(root)) //inversion guess exists
+			delta =  1;
+
+		recordCorrect(next, record + delta, visit, correct, adj, guess);
+	}
+	return;
 }
 
-string storyOfATree(const int n, const vector<vector<int>>& edges, const int k, const vector<vector<int>>& guesses) {
-
-    vector<vector<int>> adj(n);
-
-    //build dag
-    for (const auto& edge : edges)
-    {
-        adj[edge[0] - 1].push_back(edge[1] - 1);
-        adj[edge[1] - 1].push_back(edge[0] - 1);
-    }
 
 
+string storyOfATree(const int n, const vector<vector<int>>& edges, const int k, const vector<vector<int>>& guesses)
+{
+
+	//make adjlist
+	vector<vector<int>> adj(n + 1);
+	for (const auto& edge : edges)
+	{
+		const int u = edge[0];
+		const int v = edge[1];
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	//make guessMap
+	GuessMap guessMap(n + 1);
+	for (const auto& guess : guesses)
+	{
+		guessMap[guess[0]].insert(guess[1]);
+	}
 
 
-    //build tree
-    vector<Node*> nodes(n);
-    for (int i = 0; i < n; ++i)
-    {
-        nodes[i] = new Node();
-    }
+	vector<bool> visit(n + 1, false);//1-based index
+	vector<int> correct(n + 1, 0);
 
-    buildTreewithDAG(edges[0][0] - 1, nodes, adj);
-    recordSubTree(edges[0][0]-1, nodes);
-    //for (int i = 0; i < n ; i++)
-    //{
-    //    Node* now = nodes[i];
-    //    now->subTree.reset(i); 
-    //}
-    
-    const int g = guesses.size();
-    vector<int> record(n,g); //record the number possible guess when [root == i]
+	//find correctness in root == 1
+	correct[1] = findCorrect(1, visit, adj, guessMap);
 
-    //Number of predictions correct when root = k on all nodes write to record[k] -> O(N*k)
-    for (const auto& guess : guesses)
-    {
-        int parent = guess[0]-1;  //u-v connection always guaranted.
-        int child = guess[1]-1;
+	fill(visit.begin(), visit.end(), false);
+	recordCorrect(1, correct[1], visit, correct, adj, guessMap);
 
-        for (int r = 0; r < n; ++r)
-        {
-            if (r == parent) continue;
-            //The new route belongs to parent's sub And
-                // parent's child belongs to the new root's subtree -> inversion
-            int inversion =
-                nodes[parent]->subTree.test(r) && nodes[r]->subTree.test(child);
-            record[r] -= inversion;
-        }
-    }
-    
-    //count p
-    int p = 0;
-    int q = n;
-    for (int result : record)
-    {
-        p += (result >= k);
-    }
+	//get p for wins
+	int numerator = ::count_if(correct.begin(), correct.end(), [&k](const int x) {return x >= k; });
+	int denominator = n;
 
-    int gcdPq = std::gcd(p, q);
-    p /= gcdPq;
-    q /= gcdPq;
+	int gcd = std::gcd(numerator, denominator);
+	numerator /= gcd;
+	denominator /= gcd;
 
-    char buffer[50];
-    sprintf_s(buffer, "%d/%d", p,q);
-    return string(buffer);
+	return to_string(numerator) + "/" + to_string(denominator);
 }
-
+ 
 int main() {
 	ifstream fin("./input.txt"); // 입력 파일 열기
 	ofstream fout("./output.txt"); // 출력 파일 열기
 #pragma region OpenFile
-    if (!fin)
-    {
-        cerr << "Unable to open input file";
-        return 1; // 입력 파일을 열 수 없으면 프로그램 종료
-    }
-    if (!fout)
-    {
-        cerr << "Unable to open output file";
-        return 1; // 출력 파일을 열 수 없으면 프로그램 종료
-    }
+	if (!fin)
+	{
+		cerr << "Unable to open input file";
+		return 1; // 입력 파일을 열 수 없으면 프로그램 종료
+	}
+	if (!fout)
+	{
+		cerr << "Unable to open output file";
+		return 1; // 출력 파일을 열 수 없으면 프로그램 종료
+	}
 #pragma endregion
-    
-    int q;
-    fin >> q;
+	
+	int q;
+	fin >> q;
 
-    for (int i = 0; i < q; ++i)
-    {
-        int n;
-        
+	for (int i = 0; i < q; ++i)
+	{
+		int n;
+		
 
-        fin >> n;
-        vector<vector<int>> edges(n - 1, vector<int>(2));
-        for (int j = 0; j < n-1; ++j)
-        {
-            fin >> edges[j][0] >> edges[j][1];
-        }
+		fin >> n;
+		vector<vector<int>> edges(n - 1, vector<int>(2));
+		for (int j = 0; j < n-1; ++j)
+		{
+			fin >> edges[j][0] >> edges[j][1];
+		}
 
-        int g, k;
-        fin >> g >> k;
-        vector<vector<int>> guesses(g, vector<int>(2));
-        for (int k = 0; k < g; ++k)
-        {
-            fin >> guesses[k][0] >> guesses[k][1];
-        }
-        
+		int g, k;
+		fin >> g >> k;
+		vector<vector<int>> guesses(g, vector<int>(2));
+		for (int k = 0; k < g; ++k)
+		{
+			fin >> guesses[k][0] >> guesses[k][1];
+		}
+		
 
-        cout << storyOfATree(n, edges, k, guesses) << "\n";
-    }
+		cout << storyOfATree(n, edges, k, guesses) << "\n";
+	}
    
 
-    /*
-    19/50
-    1/1
-    217/250
-    0/1
-    67/1000
-*/
+	/*
+	67/100
+	21/100
+	1/1
+	0/1
+	87/100
+	*/
+
+	/* [input2]
+	19/50
+	1/1
+	217/250
+	0/1
+	67/1000
+	*/
 #pragma region Close
-    fin.close(); // 입력 파일 닫기
-    fout.close(); // 출력 파일 닫기
+	fin.close(); // 입력 파일 닫기
+	fout.close(); // 출력 파일 닫기
 #pragma endregion
-    return 0;
+	return 0;
 }
