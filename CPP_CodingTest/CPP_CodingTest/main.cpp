96d771fc7368b5ddb24480f9f9c175e1b44509e2
OliveGreenKR <jeongchul0098@gmail.com>
2024-12-05 15:45:37 +0900

    solve : transport gold and silver

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -26,27 +26,68 @@
 using namespace std;
 	
 
-long long solution(int a, int b, const vector<int>& g, const vector<int>& s, const vector<int>& w, const vector<int>& t) {
-    // i 도시 자원 -> 금 , 은 , 트럭
-    //각 도시 트럭은 '새 도시 건설 장소' 와 i도시만 왕복 가능
-    //왕복은 무한대로 가능
-        //편도 소요 시간 t[i] , 최대 운반 가능 무기 w[i] - 금,은 혼합 이동 가능
 
-    //모든 트럭은 최적운행 -> 현재 부족한 광물을 항상 최대의 무게로 운반.
-        //본인 도시의 광물이 떨어지기 전까지 무조건 운반
+// Check if it's possible to transport required amount of gold and silver within given time
+bool isPossible(const long long time, int gold, int silver, const vector<int>& g,
+                const vector<int>& s, const vector<int>& w, const vector<int>& t) {
+    const int n = g.size();
 
-    //가장 오래 운행하는 트럭의 운행시간을 확인해야함.
+    //금, 은 모두 가능한 최대를 옮긴다고 가정
+       // 모두가 금위주로 할 때, 모두가 은 위주로 할 때 
 
+    long long totalGold = 0;    // Total transportable gold
+    long long totalSilver = 0;  // Total transportable silver
+    long long totalMineral = 0; // Total transportable minerals (gold + silver)
 
-    // 운반 소요 시간 = t + 2(운반 횟수-1)t  
+    for (int i = 0; i < n; i++)
+    {
+        //운반 시간 =  t + (운반횟수-1) * 2t  (횟수 >=1)
+        //운반횟수 =  floor((totalT - t)/2t + 1)
+        long long rounds = 0;
+        if (time >= t[i])
+        {
+            rounds = (time - t[i]) / (2 * t[i]) + 1;
+        }
+
+        const long long maxGold = min(1LL * w[i] * rounds, 1LL * g[i]);
+        const long long maxSilver = min(1LL * w[i] * rounds, 1LL * s[i]);
+        long long maxWeight = min(1LL * w[i] * rounds, 1LL * g[i] + s[i]);
+
+        totalGold += maxGold;
+        totalSilver += maxSilver;
+        totalMineral += maxWeight;
+    }
+
+    // 두가지 상태의 금, 은 무게 가 요구사항을 넘고, 운반 화물양이 모든 요구 화물양을 넘으면 만족
+    return  (totalGold >= gold) && (totalSilver >= silver) && (totalMineral >= ((long long)gold + silver));
+}
+
+long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) {
     //도시 개수 N <= 1e5 -> NlogN 이하로
 
-    // 두 가지 자원을 모두 만족해야함.
-        //한 가지 자원이라면 
+    //get lower_bound possible time.
+    long long left = 0;
+    long long right = 1e9 * 4LL * 1e5; //한계 운반 시간
 
+    //binary search
+    while (left < right)
+    {
+        long long mid = (left + right) / 2;
+        if (isPossible(mid, a, b, g, s, w, t))
+        {
+            //smaller mid
+            right = mid;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
 
-
+    return left;
 }
+
+
 int main() {
 	string infile = "./input.txt";
 	string outfile = "./output.txt";
@@ -64,7 +105,7 @@ int main() {
 		return 1; // 출력 파일을 열 수 없으면 프로그램 종료
 	}
 #pragma endregion
-	
+    cout << solution(10, 10, { 100 }, { 100 }, { 7 }, { 10 });
 
 #pragma region CloseFile 
 	fin.close(); // 입력 파일 닫기
