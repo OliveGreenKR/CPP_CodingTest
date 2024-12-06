9d1eb879ab473cc22b29882cfaea15476cf0e192
OliveGreenKR <jeongchul0098@gmail.com>
2024-12-06 13:06:08 +0900

    정수 삼각형 최대 누적합 경로 찾기

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -25,68 +25,36 @@
 
 using namespace std;
 	
+int solution(vector<vector<int>> triangle) {
 
+    vector<vector<int>> dp(triangle);
+    //dp[i][j] = tri[i][j]까지 경로의 최대 누적합
+    //dp[i][j] = ::max(dp[i-1][j],dp[i-1][j-1]) + tri[i][j]
 
-// Check if it's possible to transport required amount of gold and silver within given time
-bool isPossible(const long long time, int gold, int silver, const vector<int>& g,
-                const vector<int>& s, const vector<int>& w, const vector<int>& t) {
-    const int n = g.size();
-
-    //금, 은 모두 가능한 최대를 옮긴다고 가정
-       // 모두가 금위주로 할 때, 모두가 은 위주로 할 때 
-
-    long long totalGold = 0;    // Total transportable gold
-    long long totalSilver = 0;  // Total transportable silver
-    long long totalMineral = 0; // Total transportable minerals (gold + silver)
-
-    for (int i = 0; i < n; i++)
+    // Fill DP table
+    for (int i = 1; i < triangle.size(); i++)
     {
-        //운반 시간 =  t + (운반횟수-1) * 2t  (횟수 >=1)
-        //운반횟수 =  floor((totalT - t)/2t + 1)
-        long long rounds = 0;
-        if (time >= t[i])
+        for (int j = 0; j <= i; j++)
         {
-            rounds = (time - t[i]) / (2 * t[i]) + 1;
-        }
-
-        const long long maxGold = min(1LL * w[i] * rounds, 1LL * g[i]);
-        const long long maxSilver = min(1LL * w[i] * rounds, 1LL * s[i]);
-        long long maxWeight = min(1LL * w[i] * rounds, 1LL * g[i] + s[i]);
-
-        totalGold += maxGold;
-        totalSilver += maxSilver;
-        totalMineral += maxWeight;
-    }
-
-    // 두가지 상태의 금, 은 무게 가 요구사항을 넘고, 운반 화물양이 모든 요구 화물양을 넘으면 만족
-    return  (totalGold >= gold) && (totalSilver >= silver) && (totalMineral >= ((long long)gold + silver));
-}
-
-long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) {
-    //도시 개수 N <= 1e5 -> NlogN 이하로
-
-    //get lower_bound possible time.
-    long long left = 0;
-    long long right = 1e9 * 4LL * 1e5; //한계 운반 시간
-
-    //binary search
-    while (left < right)
-    {
-        long long mid = (left + right) / 2;
-        if (isPossible(mid, a, b, g, s, w, t))
-        {
-            //smaller mid
-            right = mid;
-        }
-        else
-        {
-            left = mid + 1;
+            if (j == 0)
+            {  // Leftmost edge - can only come from above
+                dp[i][j] = dp[i - 1][j] + triangle[i][j];
+            }
+            else if (j == i)
+            {  // Rightmost edge - can only come from left-above
+                dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+            }
+            else
+            {  // Middle positions - can come from either above or left-above
+                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+            }
         }
     }
 
-    return left;
+    // Find maximum value in the last row
+    return *max_element(dp[triangle.size() - 1].begin(),
+                        dp[triangle.size() - 1].end());
 }
-
 
 int main() {
 	string infile = "./input.txt";
@@ -105,7 +73,7 @@ int main() {
 		return 1; // 출력 파일을 열 수 없으면 프로그램 종료
 	}
 #pragma endregion
-    cout << solution(10, 10, { 100 }, { 100 }, { 7 }, { 10 });
+
 
 #pragma region CloseFile 
 	fin.close(); // 입력 파일 닫기
