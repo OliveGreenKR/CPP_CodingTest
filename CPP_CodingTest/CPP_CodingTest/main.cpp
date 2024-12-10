a7ada5dc228c26de1f5cb3d6446fde28493d007c
OliveGreenKR <jeongchul0098@gmail.com>
2024-12-10 10:59:11 +0900

    Ricochet Robot solved with BFS

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -25,21 +25,91 @@
 
 using namespace std;
 
-//hanoi Path
-void getPath(const int n, int curr, int target, int empty, vector<vector<int>>& result)
+struct State
 {
-	if (n < 1)
-		return;
+	State() {};
+	State(const State& other) : move(other.move), x(other.x), y(other.y) {};
 
-	getPath(n - 1, curr, empty, target, result);
-	result.push_back({ {curr,target} });
-	getPath(n - 1, empty, target, curr, result);
+	int move = 0;
+	int x;
+	int y;
 
-}
-vector<vector<int>> solution(int n) {
-	vector<vector<int>> result;
-	getPath(n, 1, 3, 2, result);
-	return result;
+	bool operator >(const State& B)
+	{
+		return this->move > B.move;
+	}
+};
+
+int solution(vector<string> board) {
+
+	const vector<int> dirX = { 1,0,0,-1 };
+	const vector<int> dirY = { 0,1,-1,0 };
+
+	const int row = board.size();
+	const int col = board[0].size();
+
+	auto isBlocked = [&board, &row, &col](const int x, const int y)
+		{
+			return (x >= row || x < 0 || y >= col || y < 0 || board[x][y] == 'D');
+		};
+
+	//find start and dest
+	State start;
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			if (board[i][j] == 'R')
+			{
+				start.x = i;
+				start.y = j;
+			}
+		}
+	}
+
+	priority_queue<State, vector<State>, greater<>> pq;
+	State now;
+
+	now.x = start.x;
+	now.y = start.y;
+	now.move = 0;
+	board[now.x][now.y] = '*';
+	pq.push(now);
+
+	while (!pq.empty())
+	{
+		now = pq.top();
+		pq.pop();
+		
+
+		//find next
+		for (int i = 0; i < 4; ++i)
+		{
+			State next(now);
+			while (!isBlocked(next.x, next.y))
+			{
+				next.x += dirX[i];
+				next.y += dirY[i];
+			}
+			next.x -= dirX[i];
+			next.y -= dirY[i];
+			++next.move;
+
+			if (board[next.x][next.y] == 'G')
+				return next.move;
+
+			if (board[next.x][next.y] != '*')
+			{
+				board[next.x][next.y] = '*';
+				pq.push(next);
+			}
+		 
+		}
+		
+	}
+
+	//impossible
+	return -1;
 }
 int main() {
 	string infile = "./input.txt";
@@ -58,7 +128,7 @@ int main() {
 		return 1; // 출력 파일을 열 수 없으면 프로그램 종료
 	}
 #pragma endregion
-
+	cout << solution({ "...D..R", ".D.G...", "....D.D", "D....D.", "..D...." });
 
 #pragma region CloseFile 
 	fin.close(); // 입력 파일 닫기
