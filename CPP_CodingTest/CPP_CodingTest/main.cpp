98ad4401be22deb65c5193aef4a7d6a992bdb159
OliveGreenKR <jeongchul0098@gmail.com>
2024-12-11 13:51:13 +0900

    parentheses conversion

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -25,92 +25,67 @@
 
 using namespace std;
 
-struct State
-{
-	State() {};
-	State(const State& other) : move(other.move), x(other.x), y(other.y) {};
+//check if the string is rightful  : O(N)
+bool isRight(const string& str) {
+    int count = 0;
 
-	int move = 0;
-	int x;
-	int y;
+    for (char c : str)
+    {
+        if (c == '(')
+            ++count;
+        else
+            --count;
 
-	bool operator >(const State& B)
-	{
-		return this->move > B.move;
-	}
-};
-
-int solution(vector<string> board) {
-
-	const vector<int> dirX = { 1,0,0,-1 };
-	const vector<int> dirY = { 0,1,-1,0 };
-
-	const int row = board.size();
-	const int col = board[0].size();
-
-	auto isBlocked = [&board, &row, &col](const int x, const int y)
-		{
-			return (x >= row || x < 0 || y >= col || y < 0 || board[x][y] == 'D');
-		};
-
-	//find start and dest
-	State start;
-	for (int i = 0; i < row; ++i)
-	{
-		for (int j = 0; j < col; ++j)
-		{
-			if (board[i][j] == 'R')
-			{
-				start.x = i;
-				start.y = j;
-			}
-		}
-	}
-
-	priority_queue<State, vector<State>, greater<>> pq;
-	State now;
-
-	now.x = start.x;
-	now.y = start.y;
-	now.move = 0;
-	board[now.x][now.y] = '*';
-	pq.push(now);
-
-	while (!pq.empty())
-	{
-		now = pq.top();
-		pq.pop();
-		
-
-		//find next
-		for (int i = 0; i < 4; ++i)
-		{
-			State next(now);
-			while (!isBlocked(next.x, next.y))
-			{
-				next.x += dirX[i];
-				next.y += dirY[i];
-			}
-			next.x -= dirX[i];
-			next.y -= dirY[i];
-			++next.move;
-
-			if (board[next.x][next.y] == 'G')
-				return next.move;
-
-			if (board[next.x][next.y] != '*')
-			{
-				board[next.x][next.y] = '*';
-				pq.push(next);
-			}
-		 
-		}
-		
-	}
-
-	//impossible
-	return -1;
+        if (count < 0)
+            return false;
+    }
+    return count == 0;
 }
+
+//find smallest balaned str idx
+pair<string, string> splitBalanced(const string& str)
+{
+    int count = 0;
+    string u;
+
+    for (int i = 0; i < str.size(); ++i)
+    {
+        u += str[i];
+        if (str[i] == '(')
+            ++count;
+        else
+            --count;
+        if (count == 0)
+            return { u,str.substr(i + 1) };
+    }
+    return { u,"" };
+}
+
+string solution(string p) {
+    const int n = p.size();
+
+    if (n < 1)
+        return p;
+
+    //find smallest balaned str
+    auto pairs = splitBalanced(p);
+    string u = pairs.first;
+    string v = pairs.second;
+
+    if (isRight(u))
+    {
+        return u + solution(v);
+    }
+
+    string result = "(" + solution(v) + ")";
+    for (int i = 0; i < u.size(); ++i)
+    {
+        u[i] = u[i] == '(' ? ')' : '(';
+    }
+
+    return u + result;
+}
+
 int main() {
 	string infile = "./input.txt";
 	string outfile = "./output.txt";
@@ -128,7 +103,7 @@ int main() {
 		return 1; // 출력 파일을 열 수 없으면 프로그램 종료
 	}
 #pragma endregion
-	cout << solution({ "...D..R", ".D.G...", "....D.D", "D....D.", "..D...." });
+	cout << solution({ ")(" });
 
 #pragma region CloseFile 
 	fin.close(); // 입력 파일 닫기
