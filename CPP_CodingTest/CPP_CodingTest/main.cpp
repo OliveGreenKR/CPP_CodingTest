e0062db68d14ac0318dddb87f6f4d4ab633a65a0
OliveGreenKR <jeongchul0098@gmail.com>
2024-12-16 11:27:59 +0900

    word modifying solved

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -24,31 +24,77 @@
 #define OUT
 
 using namespace std;
+int solution(string begin, string target, vector<string> words) {
 
-//find all possible values in dfs.
-int dfs(long long current, int remainingPlus)
-{
+	//add start
+	words.push_back(begin);
 	
-	if (current < 3 || current < pow(3, remainingPlus / 2))
-		return 0;
+	const int n = words.size();
+	const int m = target.size();
 
-	if (current == 3 && remainingPlus == 2)
-		return 1;
+	unordered_map<string, vector<int>> patternMap;
+	//[pattern][word idx]
 
-	int result = 0;
+	bool bHasTarget = false;
+	//build pattern map
+	for (int i = 0; i < n; ++i)
+	{
+		if (words[i] == target)
+			bHasTarget = true;
 
-	if (current % 3 == 0 && remainingPlus >= 2)
-		result += dfs(current/3, remainingPlus-2);
+		for (int j = 0; j < m; ++j)
+		{
+			string p(words[i]);
+			p[j] = '*';
+			patternMap[p].push_back(i);
+		}
+	}
 
+	if (bHasTarget == false)
+		return false;
 
-	result += dfs(current - 1, remainingPlus + 1);
+	vector<unordered_set<int>> adj(n); //[idx][adj set]
+	//build adj list
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			string p(words[i]);
+			p[j] = '*';
 
-	return result;
-}
+			const vector<int>& pv = patternMap[p];
+			if(!pv.empty())
+				adj[i].insert(pv.begin(), pv.end());
+		}
+		adj[i].erase(i);
+	}
 
-int solution(int n) {
+	//bfs
+	queue<pair<int, int>> q; //[idx][cost]
 
-	return dfs(n, 0);
+	//start
+	vector<bool> visit(n, false);
+	q.push({ n-1 ,0 }); 
+
+	while (!q.empty())
+	{
+		auto [idx, cost] = q.front();
+		q.pop();
+
+		if (words[idx] == target)
+			return cost;
+
+		for (const int& next : adj[idx])
+		{
+			if (visit[next])
+				continue;
+			visit[next] = true;
+			q.push({ next,cost + 1 });
+		}
+
+	}
+
+	return 0;
 }
 
 int main() {
@@ -68,7 +114,8 @@ int main() {
 		return 1; // 출력 파일을 열 수 없으면 프로그램 종료
 	}
 #pragma endregion
-
+	cout << solution("hit", "cog", { "hot", "dot", "dog", "lot", "log", "cog" });
+	cout << solution("hit", "cog", { "hot", "dot", "dog", "lot", "log" });
 #pragma region CloseFile 
 	fin.close(); // 입력 파일 닫기
 	fout.close(); // 출력 파일 닫기
