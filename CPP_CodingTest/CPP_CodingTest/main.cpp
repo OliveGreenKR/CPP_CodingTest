55342d122b432b79f10970534d282909a694e2f0
OliveGreenKR <jeongchul0098@gmail.com>
2024-12-18 14:12:00 +0900

    Count of correct parentheses strings in n parentheses pairs.

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -25,54 +25,29 @@
 
 using namespace std;
 
-unordered_map<int, vector<int>> children;
+//sym, asym
+pair<int, int> dfs(int n)
+{
+	if (n < 1)
+		return { 0,0 };
 
-//{not_attend, attend}
-pair<int, int> dfs(int now , const vector<int>& sales) {
+	if (n == 1)
+		return { 0,0 };
+	if (n == 2)
+		return { 1,0 };
 
-	//leaf
-	if (children.find(now) == children.end()) 
-		return { 0, sales[now - 1] }; 
+	auto [sym1, asym1] = dfs(n - 1);
+	auto [sym2, asym2] = dfs(n - 2);
 
-	int sum = 0, min_diff = INT32_MAX;
-	bool flag = false;
-
-	for (auto next : children[now])
-	{
-		const auto [not_attend, attend] = dfs(next, sales);
-
-		//case 1 : attend now
-			//sum of child [attend or not]
-		sum += min(not_attend, attend);
-
-		//case 2 : other team attend
-		if (not_attend >= attend) 
-			flag = true;
-
-		//case 3 :  no one attend
-			//forcely make child attend
-		min_diff = min(min_diff, attend - not_attend);
-	}
-
-	if (flag) 
-		return { sum, sales[now - 1] + sum };
-	return { sum + min_diff, sales[now - 1] + sum };
-   
+	return { (1 + sym1 + sym2) , (sym1 * 2 + asym1 + (asym1 - sym2) * 2) };
 }
 
-int solution(vector<int> sales, vector<vector<int>> links) {
+int solution(int n) {
 
-	for (auto link : links)
-	{
-		children[link[0]].push_back(link[1]);
-	}
-
-	const auto [not_attend, attend] = dfs(1, sales);
-
-	return min(not_attend, attend);
+	auto res = dfs(n);
+	return res.first + res.second + 1;
 }
-//without dp : 
-//with dp : 20 : 304,  19 : 235
+
 
 int main() {
 	string infile = "./input.txt";
@@ -91,7 +66,7 @@ int main() {
 		return 1; // 출력 파일을 열 수 없으면 프로그램 종료
 	}
 #pragma endregion
-
+	cout << solution(3);
 #pragma region CloseFile 
 	fin.close(); // 입력 파일 닫기
 	fout.close(); // 출력 파일 닫기
