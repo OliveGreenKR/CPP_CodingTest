a5c1c56a153f8cbe80d82f15a8ca7fe50f658e2a
OliveGreenKR <jeongchul0098@gmail.com>
2024-12-20 11:32:24 +0900

    solved  Bulid BST with x,y coord

--- CPP_CodingTest/CPP_CodingTest/main.cpp
+++ CPP_CodingTest/CPP_CodingTest/main.cpp
@@ -25,51 +25,150 @@
 
 using namespace std;
 
-//sym, asym
-pair<int, int> dfs(int n)
+struct Node
 {
-	if (n < 1)
-		return { 0,0 };
+    int x, y;
+    Node* left;
+    Node* right;
 
-	if (n == 1)
-		return { 0,0 };
-	if (n == 2)
-		return { 1,0 };
+    Node(int x_, int y_) : x(x_), y(y_), left(nullptr), right(nullptr) {}
+};
 
-	auto [sym1, asym1] = dfs(n - 1);
-	auto [sym2, asym2] = dfs(n - 2);
+inline long long CoordHash (int x, int y)
+{
+    return x + y * 1e6;
+};
 
-	return { (1 + sym1 + sym2) , (sym1 * 2 + asym1 + (asym1 - sym2) * 2) };
+using NodeMap = unordered_map<long long, int>;
+
+// Helper function to insert a node into the tree
+Node* insertNode(Node* root, Node* newNode) {
+    // empty tree
+    if (!root) 
+        return newNode;
+
+    //parent of root
+    if (newNode->y > root->y)
+    {
+        // Determine if current root should be left or right child
+        if (root->x < newNode->x)
+        {
+            newNode->left = root;
+        }
+        else
+        {
+            newNode->right = root;
+        }
+        return newNode;
+    }
+
+    // child of root
+    if (newNode->x < root->x)
+    {
+        // Insert into left subtree
+        root->left = insertNode(root->left, newNode);
+    }
+    else if (newNode->x > root->x)
+    {
+        // Insert into right subtree
+        root->right = insertNode(root->right, newNode);
+    }
+
+    return root;
 }
 
-int solution(int n) {
+Node* constructBinaryTree(const vector<vector<int>>& nodeInfo) {
+    
+    // Sort nodes by y-coordinate (level) in descending order
+    vector<vector<int>> sortedNodes = nodeInfo;
+    sort(sortedNodes.begin(), sortedNodes.end(),
+         [](const vector<int>& a, const vector<int>& b) {
+             return a[1] > b[1];  // Sort by y-coordinate
+         });
 
-	auto res = dfs(n);
-	return res.first + res.second + 1;
+    Node* root = nullptr;
+
+    // Process each node and insert it into the tree
+    for (const auto& node : sortedNodes)
+    {
+        Node* newNode = new Node(node[0], node[1]);
+        root = insertNode(root, newNode);
+    }
+
+    return root;
 }
 
+
+void preOrder(const Node* root, vector<int>& record, const NodeMap& nodeMap)
+{
+    if (root == nullptr)
+        return;
+
+    long long coord = CoordHash(root->x, root->y);
+
+    record.push_back(nodeMap.at(coord));
+    preOrder(root->left, record, nodeMap);
+    preOrder(root->right, record, nodeMap);
+}
+
+void postOrder(const Node* root, vector<int>& record, const NodeMap& nodeMap)
+{
+    if (root == nullptr)
+        return;
+
+    preOrder(root->left, record, nodeMap);
+    preOrder(root->right, record, nodeMap);
+    long long coord = CoordHash(root->x, root->y);
+    record.push_back(nodeMap.at(coord));
+}
+vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
+    
+    const int n = nodeinfo.size();
+
+    NodeMap nodeMap; //nodeMap [coord][nodeidx]
+    for (int i = 0; i < n; ++i)
+    {
+        const auto& node = nodeinfo[i];
+        long long coord = CoordHash(node[0], node[1]);
+        nodeMap[coord] = i+1;
+    }
+
+
+    //buildTree
+    Node* root = constructBinaryTree(nodeinfo);
+
+
+    vector<vector<int>> answer(2);
+
+    preOrder(root, answer[0], nodeMap);
+    postOrder(root, answer[1], nodeMap);
+
+    return answer;
+}
 
 int main() {
-	string infile = "./input.txt";
-	string outfile = "./output.txt";
+    string infile = "./input.txt";
+    string outfile = "./output.txt";
 #pragma region OpenFile
-	ifstream fin(infile); 
-	ofstream fout(outfile); 
-	if (!fin)
-	{
-		cerr << "Unable to open input file";
-		return 1; // 입력 파일을 열 수 없으면 프로그램 종료
-	}
-	if (!fout)
-	{
-		cerr << "Unable to open output file";
-		return 1; // 출력 파일을 열 수 없으면 프로그램 종료
-	}
+    ifstream fin(infile);
+    ofstream fout(outfile);
+    if (!fin)
+    {
+        cerr << "Unable to open input file";
+        return 1; // 입력 파일을 열 수 없으면 프로그램 종료
+    }
+    if (!fout)
+    {
+        cerr << "Unable to open output file";
+        return 1; // 출력 파일을 열 수 없으면 프로그램 종료
+    }
 #pragma endregion
-	cout << solution(3);
+
+    vector<vector<int>> nodeinfo = {{ 5, 3 }, { 11, 5 }, { 13, 3 }, { 3, 5 }, { 6, 1 }, { 1, 3 }, { 8, 6 }, { 7, 2 }, { 2, 2 }};
+    solution(nodeinfo);
 #pragma region CloseFile 
-	fin.close(); // 입력 파일 닫기
-	fout.close(); // 출력 파일 닫기
+    fin.close(); // 입력 파일 닫기
+    fout.close(); // 출력 파일 닫기
 #pragma endregion
-	return 0;
+    return 0;
 }
